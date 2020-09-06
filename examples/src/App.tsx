933114aa922d3c8b25e3714e62abcc8f281c31c3
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-09-06 17:20:31 +0200

    adds example code

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Provider, atom, useAtom } from 'jotai'
 
+import PrismCode from 'react-prism'
+import 'prismjs'
+import 'prismjs/components/prism-jsx.min'
+import 'prismjs/themes/prism-okaidia.css'
 const textAtom = atom<string>('hello')
 const textLenAtom = atom((get) => get(textAtom).length)
 const uppercaseAtom = atom((get) => get(textAtom).toUpperCase())
 
 const Input = () => {
   const [text, setText] = useAtom(textAtom)
-  return <input value={text} onChange={(e) => setText(e.target.value)} />
+  return (
+    <input
+      className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none leading-normal"
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+    />
+  )
 }
 
 const CharCount = () => {
@@ -20,23 +30,41 @@ const Uppercase = () => {
   return <div>Uppercase: {uppercase}</div>
 }
 
+const code = `const Input = () => {
+  const [text, setText] = useAtom(textAtom)
+  return <input 
+    value={text} 
+    onChange={(e) => setText(e.target.value)} 
+  />
+}
+
+const CharCount = () => {
+  const [len] = useAtom(textLenAtom)
+  return <div>Length: {len}</div>
+}
+
+const Uppercase = () => {
+  const [uppercase] = useAtom(uppercaseAtom)
+  return <div>Uppercase: {uppercase}</div>
+}`
+
 const App = () => (
-  <div className="container mx-auto px-8">
-    <header className="mt-12 mb-12">
-      <h1 className="text-6xl font-bold">Jōtai</h1>
-      <h2
-        className="
-        text-3xl font-weight-500 text-gray-400 
-        lg:flex justify-between">
-        <div>Primitive and flexible state management for React.</div>
-        <div className="text-xl font-weight-300 text-gray-900">状態 </div>
-      </h2>
-    </header>
-    <Provider>
-      <Input />
-      <CharCount />
-      <Uppercase />
-    </Provider>
+  <div
+    className="
+      lg:flex
+      space-x-5
+    ">
+    <div className="w-1/2">
+      <Provider>
+        <Input />
+        <CharCount />
+        <Uppercase />
+      </Provider>
+    </div>
+
+    <div className="w-1/2">
+      <PrismCode component="pre" className="language-jsx" children={code} />
+    </div>
   </div>
 )
 
--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -3,4 +3,20 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './style.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <div className="container mx-auto px-8">
+    <header className="mt-12 mb-12">
+      <h1 className="text-6xl font-bold">Jōtai</h1>
+      <h2
+        className="
+        text-3xl font-weight-500 text-gray-400 
+        lg:flex justify-between items-center
+        ">
+        <div>Primitive and flexible state management for React.</div>
+        <div className="text-xl font-weight-300 text-gray-900">状態 </div>
+      </h2>
+    </header>
+    <App />
+  </div>,
+  document.getElementById('root')
+)
