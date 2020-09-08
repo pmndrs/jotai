2dfa1235faa9a3de162888ad2ccab32fd1f47450
Daishi Kato <dai-shi@users.noreply.github.com>
2020-09-08 21:48:44 +0900

    Example (#12)
    * style draft
    
    * adds example code
    
    * example fixes
    
    * finetuning
    
    * minor tweaks
    
    * fixes font size
    
    * edit style
    
    * edit subtitle
    
    * add inter font
    
    * format
    
    * disable select
    
    * add github link
    
    Co-authored-by: Gianmarco Simone <gianmarcosimone89@gmail.com>
    Co-authored-by: iinfin <48106228+iinfin@users.noreply.github.com>

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Provider, atom, useAtom } from 'jotai'
 
-const textAtom = atom('hello')
+import PrismCode from 'react-prism'
+import 'prismjs'
+import 'prismjs/components/prism-jsx.min'
+
+const textAtom = atom<string>('hello')
 const textLenAtom = atom((get) => get(textAtom).length)
 const uppercaseAtom = atom((get) => get(textAtom).toUpperCase())
 
 const Input = () => {
   const [text, setText] = useAtom(textAtom)
-  return <input value={text} onChange={(e) => setText(e.target.value)} />
+  return (
+    <input
+      className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none leading-normal"
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+    />
+  )
 }
 
 const CharCount = () => {
@@ -17,15 +27,64 @@ const CharCount = () => {
 
 const Uppercase = () => {
   const [uppercase] = useAtom(uppercaseAtom)
+  return uppercase
+}
+
+const code = `import { Provider, atom, useAtom } from 'jotai'
+
+// 1️⃣ create your atoms
+const textAtom = atom('hello')
+const uppercaseAtom = atom((get) => get(textAtom).toUpperCase())
+
+// 2️⃣ use them anywhere in your app
+const Input = () => {
+  const [text, setText] = useAtom(textAtom)
+  return <input value={text} onChange={(e) => setText(e.target.value)} />
+}
+
+const Uppercase = () => {
+  const [uppercase] = useAtom(uppercaseAtom)
   return <div>Uppercase: {uppercase}</div>
 }
 
-const App = () => (
+// 3️⃣ Wrap your components in the Jotai provider
+const MyApp = () => (
   <Provider>
     <Input />
-    <CharCount />
     <Uppercase />
   </Provider>
+)
+`
+
+const App = () => (
+  <div>
+    <p className="">A simple example:</p>
+    <div
+      className="
+      ">
+      <div className="py-8 text-sm">
+        <Provider>
+          <div className="relative">
+            <Input />
+            <div className="absolute top-0 right-0 h-full flex items-center mr-4 font-bold">
+              <Uppercase />
+            </div>
+          </div>
+        </Provider>
+      </div>
+
+      <div className="">
+        <PrismCode component="pre" className="language-jsx" children={code} />
+      </div>
+
+      <div className="src">
+        <a href="http://github.com/react-spring/jotai" target="_blank">
+          <img src="https://img.icons8.com/windows/22/github.png" alt="" />
+          <p>GitHub ↗</p>
+        </a>
+      </div>
+    </div>
+  </div>
 )
 
 export default App
--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -1,6 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import './prism.css'
 import './style.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <div className="max-w-4xl mx-auto px-8 pb-8">
+    <header className="mt-12 mb-12">
+      <h1 className="text-6xl font-bold">Jōtai</h1>
+      <h2
+        className="
+        text-2xl font-regular text-gray-400 
+        lg:flex justify-between items-center
+        ">
+        <div>Primitive and flexible state management for React.</div>
+        <div className="text-xl font-regular text-gray-400">状態 </div>
+      </h2>
+    </header>
+    <App />
+  </div>,
+  document.getElementById('root')
+)
