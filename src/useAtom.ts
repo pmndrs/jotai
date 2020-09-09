f7f8dfc574d85ef166fea903642b63b4f9fde09d
daishi <daishi@axlight.com>
2020-09-10 01:33:36 +0900

    fix suspense behavior for async get

--- src/useAtom.ts
+++ src/useAtom.ts
@@ -2,7 +2,7 @@ import { useCallback } from 'react'
 import { useContext, useContextSelector } from 'use-context-selector'
 
 import { StateContext, ActionsContext, AtomState } from './Provider'
-import { Atom, WritableAtom, AnyWritableAtom } from './types'
+import { Atom, WritableAtom, AnyWritableAtom, NonPromise } from './types'
 import { useIsoLayoutEffect } from './utils'
 
 const isWritable = <Value, Update>(
@@ -12,9 +12,9 @@ const isWritable = <Value, Update>(
 
 export function useAtom<Value, Update>(
   atom: WritableAtom<Value, Update>
-): [Value, (update: Update) => void]
+): [NonPromise<Value>, (update: Update) => void]
 
-export function useAtom<Value>(atom: Atom<Value>): [Value, never]
+export function useAtom<Value>(atom: Atom<Value>): [NonPromise<Value>, never]
 
 export function useAtom<Value, Update>(
   atom: Atom<Value> | WritableAtom<Value, Update>
@@ -25,9 +25,15 @@ export function useAtom<Value, Update>(
     useCallback(
       (state) => {
         const atomState = state.get(atom) as AtomState<Value> | undefined
-        if (!atomState) return atom.initialValue
-        if (atomState.promise) return atomState.promise
-        return atomState.value
+        if (atomState) {
+          return atomState.promise || atomState.value
+        }
+        if (atom.initialValue instanceof Promise) {
+          atom.initialValue.then(() => {
+            actions.init(null, atom)
+          })
+        }
+        return atom.initialValue
       },
       [atom]
     )
