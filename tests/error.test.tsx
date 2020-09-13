05ea5b22b90fa2d3071c7dbbaf4e2fb64c17aa43
daishi <daishi@axlight.com>
2020-09-13 21:11:20 +0900

    add chained error test

--- tests/error.test.tsx
+++ tests/error.test.tsx
@@ -97,3 +97,95 @@ it('can throw an error in read function', async () => {
   fireEvent.click(getByText('button'))
   await findByText('errored')
 })
+
+it('can throw an initial chained error in read function', async () => {
+  console.error = jest.fn()
+
+  const errorAtom = atom(() => {
+    throw new Error()
+  })
+  const derivedAtom = atom((get) => get(errorAtom))
+
+  class ErrorBoundary extends React.Component<{}, { hasError: boolean }> {
+    constructor(props: {}) {
+      super(props)
+      this.state = { hasError: false }
+    }
+    static getDerivedStateFromError() {
+      return { hasError: true }
+    }
+    render() {
+      return this.state.hasError ? <div>errored</div> : this.props.children
+    }
+  }
+
+  const Counter: React.FC = () => {
+    useAtom(derivedAtom)
+    return (
+      <>
+        <div>no error</div>
+      </>
+    )
+  }
+
+  const { findByText } = render(
+    <Provider>
+      <ErrorBoundary>
+        <Counter />
+      </ErrorBoundary>
+    </Provider>
+  )
+
+  await findByText('errored')
+})
+
+it('can throw a chained error in read function', async () => {
+  console.error = jest.fn()
+
+  const countAtom = atom(0)
+  const errorAtom = atom((get) => {
+    if (get(countAtom) === 0) {
+      return 0
+    }
+    throw new Error()
+  })
+  const derivedAtom = atom((get) => get(errorAtom))
+
+  class ErrorBoundary extends React.Component<{}, { hasError: boolean }> {
+    constructor(props: {}) {
+      super(props)
+      this.state = { hasError: false }
+    }
+    static getDerivedStateFromError() {
+      return { hasError: true }
+    }
+    render() {
+      return this.state.hasError ? <div>errored</div> : this.props.children
+    }
+  }
+
+  const Counter: React.FC = () => {
+    const [, setCount] = useAtom(countAtom)
+    const [count] = useAtom(derivedAtom)
+    return (
+      <>
+        <div>count: {count}</div>
+        <div>no error</div>
+        <button onClick={() => setCount((c) => c + 1)}>button</button>
+      </>
+    )
+  }
+
+  const { getByText, findByText } = render(
+    <Provider>
+      <ErrorBoundary>
+        <Counter />
+      </ErrorBoundary>
+    </Provider>
+  )
+
+  await findByText('no error')
+
+  fireEvent.click(getByText('button'))
+  await findByText('errored')
+})
