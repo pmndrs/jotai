d0d32d9c83e966640d4970d678b61fdd975b6ab7
daishi <daishi@axlight.com>
2020-09-13 22:30:04 +0900

    reducerAtom

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,9 @@
 import { useMemo } from 'react'
 import { atom, WritableAtom, useAtom } from 'jotai'
 
+type NonPromise<T> = T extends Promise<unknown> ? never : T
+type NonFunction<T> = T extends Function ? never : T
+
 export const useUpdateAtom = <Value, Update>(
   anAtom: WritableAtom<Value, Update>
 ) => {
@@ -9,4 +12,14 @@ export const useUpdateAtom = <Value, Update>(
     [anAtom]
   )
   return useAtom(writeOnlyAtom)[1]
+}
+
+export const reducerAtom = <Value, Action>(
+  initialValue: NonFunction<NonPromise<Value>>,
+  reducer: (v: Value, a: Action) => Value
+) => {
+  const anAtom: any = atom<Value, Action>(initialValue, (get, set, action) =>
+    set(anAtom, reducer(get(anAtom), action))
+  )
+  return anAtom as WritableAtom<Value, Action>
 }
