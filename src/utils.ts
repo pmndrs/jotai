c8df4e7cd9100f94d5c4d8d84ae67072d1b0a583
daishi <daishi@axlight.com>
2020-09-15 15:29:11 +0900

    atom family util

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,12 @@
 import { useCallback, useMemo } from 'react'
-import { atom, WritableAtom, useAtom } from 'jotai'
+import { atom, useAtom, Atom, WritableAtom, PrimitiveAtom } from 'jotai'
 
 import type {
   NonPromise,
   NonFunction,
   SetStateAction,
-  PrimitiveAtom,
+  Getter,
+  Setter,
 } from './types'
 
 export const useUpdateAtom = <Value, Update>(
@@ -71,4 +72,81 @@ export const atomWithReducer = <Value, Action>(
     set(anAtom, reducer(get(anAtom), action))
   )
   return anAtom as WritableAtom<Value, Action>
+}
+
+type AtomFamilyReturnType<Param, AtomType> = {
+  (param: Param): AtomType
+  remove(atom: AtomType): void
+}
+
+type AtomFamily = {
+  // writable derived atom
+  <Param, Value, Update>(
+    initializeRead: (param?: Param) => (get: Getter) => NonPromise<Value>,
+    initializeWrite: (
+      param?: Param
+    ) => (get: Getter, set: Setter, update: Update) => void | Promise<void>,
+    areEqual?: (a: Param, b: Param) => boolean
+  ): AtomFamilyReturnType<Param, WritableAtom<Value, Update>>
+  // write-only derived atom
+  <Param, Value, Update>(
+    initializeRead: (param?: Param) => NonFunction<NonPromise<Value>>,
+    initializeWrite: (
+      param?: Param
+    ) => (get: Getter, set: Setter, update: Update) => void | Promise<void>,
+    areEqual?: (a: Param, b: Param) => boolean
+  ): AtomFamilyReturnType<Param, WritableAtom<Value, Update>>
+  // async-read writable derived atom
+  <Param, Value, Update>(
+    initializeRead: (param?: Param) => (get: Getter) => Promise<Value>,
+    initializeWrite: (
+      param?: Param
+    ) => (get: Getter, set: Setter, update: Update) => void | Promise<void>,
+    areEqual?: (a: Param, b: Param) => boolean
+  ): AtomFamilyReturnType<Param, WritableAtom<Value | Promise<Value>, Update>>
+  // read-only derived atom
+  <Param, Value>(
+    initializeRead: (param?: Param) => (get: Getter) => NonPromise<Value>,
+    initializeWrite?: null,
+    areEqual?: (a: Param, b: Param) => boolean
+  ): AtomFamilyReturnType<Param, Atom<Value>>
+  // async-read read-only derived atom
+  <Param, Value>(
+    initializeRead: (param?: Param) => (get: Getter) => Promise<Value>,
+    initializeWrite?: null,
+    areEqual?: (a: Param, b: Param) => boolean
+  ): AtomFamilyReturnType<Param, Atom<Value | Promise<Value>>>
+  // primitive atom
+  <Param, Value>(
+    initializeRead: (param?: Param) => NonFunction<NonPromise<Value>>,
+    initializeWrite?: null,
+    areEqual?: (a: Param, b: Param) => boolean
+  ): AtomFamilyReturnType<Param, PrimitiveAtom<Value>>
+}
+
+export const atomFamily: AtomFamily = <Param, AtomType>(
+  initializeRead: (param?: Param) => any,
+  initializeWrite?: null | ((param?: Param) => any),
+  areEqual: (a: Param, b: Param) => boolean = Object.is
+) => {
+  const atoms: [Param, AtomType][] = []
+  const createAtom = (param: Param) => {
+    const found = atoms.find((x) => areEqual(x[0], param))
+    if (found) {
+      return found[1]
+    }
+    const newAtom = (atom(
+      initializeRead(param),
+      initializeWrite && initializeWrite(param)
+    ) as unknown) as AtomType
+    atoms.unshift([param, newAtom])
+    return newAtom
+  }
+  createAtom.remove = (a: AtomType) => {
+    const index = atoms.findIndex((x) => x[1] === a)
+    if (index >= 0) {
+      atoms.splice(index, 1)
+    }
+  }
+  return createAtom
 }
