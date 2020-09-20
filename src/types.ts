430d6ff6cdaa5304e53bd5e1e306374726108650
daishi <daishi@axlight.com>
2020-09-20 20:20:35 +0900

    optional update for write types

--- src/types.ts
+++ src/types.ts
@@ -13,8 +13,14 @@ export type Atom<Value> = {
   read: (get: Getter) => Value | Promise<Value>
 }
 
+type Write<Update> = [Update] extends [never]
+  ? (get: Getter, set: Setter) => void | Promise<void>
+  : undefined extends Update
+  ? (get: Getter, set: Setter, update?: Update) => void | Promise<void>
+  : (get: Getter, set: Setter, update: Update) => void | Promise<void>
+
 export type WritableAtom<Value, Update> = Atom<Value> & {
-  write: (get: Getter, set: Setter, update: Update) => void | Promise<void>
+  write: Write<Update>
 }
 
 export type PrimitiveAtom<Value> = WritableAtom<Value, SetStateAction<Value>>
