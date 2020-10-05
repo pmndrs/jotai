81d4574931755acd303e73ba37f8df52855bca5e
Meris Bahtijaragic <meris.bahti@gmail.com>
2020-10-06 01:01:39 +0200

    add useReducerAtom.test (#99)

---
+++ tests/utils/useReducerAtom.test.tsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import { fireEvent, render } from '@testing-library/react'
+import { Provider, useAtom, atom } from '../../src/index'
+import { useReducerAtom } from '../../src/utils'
+
+it('useReducerAtom with no action argument', async () => {
+  const countAtom = atom(0)
+  const reducer = (state: number) => state + 2
+
+  const Parent: React.FC = () => {
+    const [count, dispatch] = useReducerAtom(countAtom, reducer)
+    return (
+      <>
+        <div>count: {count}</div>
+        <button onClick={() => dispatch()}>dispatch</button>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <Provider>
+      <Parent />
+    </Provider>
+  )
+
+  await findByText('count: 0')
+
+  fireEvent.click(getByText('dispatch'))
+  await findByText('count: 2')
+
+  fireEvent.click(getByText('dispatch'))
+  await findByText('count: 4')
+})
+
+it('useReducerAtom with optional action argument', async () => {
+  const countAtom = atom(0)
+  const reducer = (state: number, action?: 'INCREASE' | 'DECREASE') => {
+    switch (action) {
+      case 'INCREASE':
+        return state + 1
+      case 'DECREASE':
+        return state - 1
+      case undefined:
+        return state
+    }
+  }
+
+  const Parent: React.FC = () => {
+    const [count, dispatch] = useReducerAtom(countAtom, reducer)
+    return (
+      <>
+        <div>count: {count}</div>
+        <button onClick={() => dispatch('INCREASE')}>dispatch INCREASE</button>
+        <button onClick={() => dispatch('DECREASE')}>dispatch DECREASE</button>
+        <button onClick={() => dispatch()}>dispatch empty</button>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <Provider>
+      <Parent />
+    </Provider>
+  )
+
+  await findByText('count: 0')
+
+  fireEvent.click(getByText('dispatch INCREASE'))
+  await findByText('count: 1')
+
+  fireEvent.click(getByText('dispatch empty'))
+  await findByText('count: 1')
+
+  fireEvent.click(getByText('dispatch DECREASE'))
+  await findByText('count: 0')
+})
+
+it('useReducerAtom with non-optional action argument', async () => {
+  const countAtom = atom(0)
+  const reducer = (state: number, action: 'INCREASE' | 'DECREASE') => {
+    switch (action) {
+      case 'INCREASE':
+        return state + 1
+      case 'DECREASE':
+        return state - 1
+    }
+  }
+
+  const Parent: React.FC = () => {
+    const [count, dispatch] = useReducerAtom(countAtom, reducer)
+    return (
+      <>
+        <div>count: {count}</div>
+        <button onClick={() => dispatch('INCREASE')}>dispatch INCREASE</button>
+        <button onClick={() => dispatch('DECREASE')}>dispatch DECREASE</button>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <Provider>
+      <Parent />
+    </Provider>
+  )
+
+  await findByText('count: 0')
+
+  fireEvent.click(getByText('dispatch INCREASE'))
+  await findByText('count: 1')
+
+  fireEvent.click(getByText('dispatch DECREASE'))
+  await findByText('count: 0')
+})
