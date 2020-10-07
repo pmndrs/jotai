6226516cf6a2004923481a075b785dc38f94156b
daishi <daishi@axlight.com>
2020-10-08 07:59:36 +0900

    fix: useBridge with updated use-context-selector

--- src/core/Bridge.ts
+++ src/core/Bridge.ts
@@ -1,11 +1,15 @@
 import React, { createElement, useMemo } from 'react'
-import { useContext, BridgeProvider } from 'use-context-selector'
+import {
+  useContext,
+  BridgeProvider,
+  useBridgeValue,
+} from 'use-context-selector'
 
 import { StateContext, ActionsContext } from './Provider'
 
 export const useBridge = () => {
-  const actions = useContext(ActionsContext)
-  const state = useContext(StateContext)
+  const actions = useBridgeValue(ActionsContext)
+  const state = useBridgeValue(StateContext)
   return useMemo(() => [actions, state], [actions, state]) as [
     typeof actions,
     typeof state
