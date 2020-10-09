e7813611be6d48ec79b0ff8ce82fe8d986da3441
M. Bagher Abiat <zorofight94@gmail.com>
2020-10-09 16:24:27 +0330

    withImmer better types

--- src/immer/withImmer.ts
+++ src/immer/withImmer.ts
@@ -2,12 +2,12 @@
 import { produce, Draft } from 'immer'
 import { atom, WritableAtom } from 'jotai'
 
-export const withImmer = <Value>(anAtom: WritableAtom<Value, Value>) =>
+export const withImmer = <Value, Update>(anAtom: WritableAtom<Value, Update>) =>
   atom(
     (get) => get(anAtom),
     (get, set, fn: (draft: Draft<Value>) => void) =>
       set(
         anAtom,
-        produce(get(anAtom), (draft) => fn(draft))
+        (produce(get(anAtom), (draft) => fn(draft)) as unknown) as Update
       )
   )
