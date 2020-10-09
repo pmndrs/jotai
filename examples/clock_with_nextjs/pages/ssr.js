fbf930cac3b2f9cfdd819e6eacc21619ced2fbd5
LuckyPigeon <lucky90322@gmail.com>
2020-10-09 08:33:30 +0800

    Remove click with nextjs from examples

--- examples/clock_with_nextjs/pages/ssr.js
+++
@@ -1,9 +0,0 @@
-import Index from "./index";
-
-export default function SSR() {
-  return <Index />;
-}
-
-export function getServerSideProps() {
-  return { props: { initialState: { light: false, lastUpdate: Date.now() } } };
-}
