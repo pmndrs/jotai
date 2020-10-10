8578a23af9c5d97ee7fd57e8b91237a306c1d9ab
M. Bagher Abiat <zorofight94@gmail.com>
2020-10-10 17:56:16 +0330

    atomWithImmer.ts test

---
+++ tests/immer/atomWithImmer.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { fireEvent, render } from '@testing-library/react'
+import { Provider, useAtom } from '../../src/index'
+import { atomWithImmer } from '../../src/immer'
+
+it('atomWithImmer with useAtom', async () => {
+  const countAtom = atomWithImmer(0)
+
+  const Parent: React.FC = () => {
+    const [count, setCount] = useAtom(countAtom)
+    return (
+      <>
+        <div>count: {count}</div>
+        <button onClick={() => setCount((draft) => (draft = draft + 1))}>
+          Increase
+        </button>
+        <button onClick={() => setCount((draft) => (draft = draft - 1))}>
+          Decrease
+        </button>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <Provider>
+      <Parent />
+    </Provider>
+  )
+
+  await findByText('count: 0')
+
+  fireEvent.click(getByText('Increase'))
+  await findByText('count: 1')
+
+  fireEvent.click(getByText('Decrease'))
+  await findByText('count: 0')
+})
