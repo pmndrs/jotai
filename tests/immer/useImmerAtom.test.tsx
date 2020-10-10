1766b0952b1539e9e9b747497b44cd24ed2c2ed7
M. Bagher Abiat <zorofight94@gmail.com>
2020-10-11 02:53:04 +0330

    Immer tests (#123)
    * atomWithImmer.ts test
    
    * useImmerAtom test
    
    * withImmer.ts test
    
    * remove unused vars

---
+++ tests/immer/useImmerAtom.test.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import { fireEvent, render } from '@testing-library/react'
+import { Provider, atom } from '../../src/index'
+import { atomWithImmer, useImmerAtom, withImmer } from '../../src/immer'
+
+it('useImmerAtom with regular atom', async () => {
+  const countAtom = atom(0)
+
+  const Parent: React.FC = () => {
+    const [count, setCount] = useImmerAtom(countAtom)
+    return (
+      <>
+        <div>count: {count}</div>
+        <button onClick={() => setCount((draft) => (draft = draft + 1))}>
+          Increase
+        </button>
+        <button onClick={() => setCount((draft) => (draft = draft - 1))}>
+          Decrease
+        </button>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <Provider>
+      <Parent />
+    </Provider>
+  )
+
+  await findByText('count: 0')
+
+  fireEvent.click(getByText('Increase'))
+  await findByText('count: 1')
+
+  fireEvent.click(getByText('Decrease'))
+  await findByText('count: 0')
+})
+
+it('useImmerAtom with immer atom', async () => {
+  const countAtom = atomWithImmer(0)
+
+  const Parent: React.FC = () => {
+    const [count, setCount] = useImmerAtom(countAtom)
+    return (
+      <>
+        <div>count: {count}</div>
+        <button onClick={() => setCount((draft) => (draft = draft + 1))}>
+          Increase
+        </button>
+        <button onClick={() => setCount((draft) => (draft = draft - 1))}>
+          Decrease
+        </button>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <Provider>
+      <Parent />
+    </Provider>
+  )
+
+  await findByText('count: 0')
+
+  fireEvent.click(getByText('Increase'))
+  await findByText('count: 1')
+
+  fireEvent.click(getByText('Decrease'))
+  await findByText('count: 0')
+})
+
+it('useImmerAtom with derived immer atom', async () => {
+  const regularCountAtom = atom(0)
+  const countAtom = withImmer(regularCountAtom)
+
+  const Parent: React.FC = () => {
+    const [count, setCount] = useImmerAtom(countAtom)
+    return (
+      <>
+        <div>count: {count}</div>
+        <button onClick={() => setCount((draft) => (draft = draft + 1))}>
+          Increase
+        </button>
+        <button onClick={() => setCount((draft) => (draft = draft - 1))}>
+          Decrease
+        </button>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <Provider>
+      <Parent />
+    </Provider>
+  )
+
+  await findByText('count: 0')
+
+  fireEvent.click(getByText('Increase'))
+  await findByText('count: 1')
+
+  fireEvent.click(getByText('Decrease'))
+  await findByText('count: 0')
+})
