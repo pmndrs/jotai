cd953aeb6e948c8fff3801fca6c2f10293b18712
daishi <daishi@axlight.com>
2020-10-12 20:28:44 +0900

    add failing test

--- tests/dependency.test.tsx
+++ tests/dependency.test.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { Suspense, useEffect, useRef } from 'react'
 import { fireEvent, cleanup, render } from '@testing-library/react'
 import { Provider, atom, useAtom } from '../src/index'
 
@@ -42,4 +42,44 @@ it('works with 2 level dependencies', async () => {
 
   fireEvent.click(getByText('button'))
   await findByText('commits: 2, count: 2, doubled: 4, tripled: 12')
+})
+
+it('works a primitive atom and a dependent async atom', async () => {
+  const countAtom = atom(1)
+  const doubledAtom = atom(async (get) => {
+    await new Promise((r) => setTimeout(r, 10))
+    return get(countAtom) * 2
+  })
+
+  const Counter: React.FC = () => {
+    const [count, setCount] = useAtom(countAtom)
+    const [doubledCount] = useAtom(doubledAtom)
+    return (
+      <>
+        <div>
+          count: {count}, doubled: {doubledCount}
+        </div>
+        <button onClick={() => setCount((c) => c + 1)}>button</button>
+      </>
+    )
+  }
+
+  const { getByText, findByText } = render(
+    <Provider>
+      <Suspense fallback="loading">
+        <Counter />
+      </Suspense>
+    </Provider>
+  )
+
+  await findByText('loading')
+  await findByText('count: 1, doubled: 2')
+
+  fireEvent.click(getByText('button'))
+  await findByText('loading')
+  await findByText('count: 2, doubled: 4')
+
+  fireEvent.click(getByText('button'))
+  await findByText('loading')
+  await findByText('count: 3, doubled: 6')
 })
