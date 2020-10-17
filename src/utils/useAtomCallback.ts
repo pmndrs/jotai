0241f0a73caf8d737a69625976999bbf157cc596
daishi <daishi@axlight.com>
2020-10-17 10:18:35 +0900

    new util useAtomCallback

---
+++ src/utils/useAtomCallback.ts
@@ -0,0 +1,39 @@
+import { useCallback, useMemo } from 'react'
+import { atom, useAtom } from 'jotai'
+
+import type { Getter, Setter } from '../core/types'
+
+export function useAtomCallback<Arg, Result>(
+  callback: (get: Getter, set: Setter, arg: Arg) => Result
+) {
+  const anAtom = useMemo(
+    () =>
+      atom(
+        null,
+        (
+          get,
+          set,
+          [arg, resolve, reject]: [
+            Arg,
+            (result: Result) => void,
+            (reason: any) => void
+          ]
+        ) => {
+          try {
+            resolve(callback(get, set, arg))
+          } catch (e) {
+            reject(e)
+          }
+        }
+      ),
+    [callback]
+  )
+  const [, invoke] = useAtom(anAtom)
+  return useCallback(
+    (arg: Arg) =>
+      new Promise<Result>((resolve, reject) => {
+        invoke([arg, resolve, reject])
+      }),
+    [invoke]
+  )
+}
