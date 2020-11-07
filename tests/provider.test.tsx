ac0c3122539e195f5e7a54d12daf9f986c7fa224
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2020-11-07 11:54:33 +0000

    test: provider initialValues (#170)

---
+++ tests/provider.test.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { render, waitFor } from '@testing-library/react'
+import { Provider, atom, useAtom } from '../src/index'
+
+it('uses initial values from provider', async () => {
+  const countAtom = atom(1)
+  const petAtom = atom('cat')
+
+  const Display: React.FC = () => {
+    const [count] = useAtom(countAtom)
+    const [pet] = useAtom(petAtom)
+
+    return (
+      <>
+        <p>count: {count}</p>
+        <p>pet: {pet}</p>
+      </>
+    )
+  }
+
+  const { getByText } = render(
+    <Provider
+      initialValues={[
+        [countAtom, 0],
+        [petAtom, 'dog'],
+      ]}>
+      <Display />
+    </Provider>
+  )
+
+  await waitFor(() => {
+    getByText('count: 0')
+    getByText('pet: dog')
+  })
+})
+
+it('only uses initial value from provider for specific atom', async () => {
+  const countAtom = atom(1)
+  const petAtom = atom('cat')
+
+  const Display: React.FC = () => {
+    const [count] = useAtom(countAtom)
+    const [pet] = useAtom(petAtom)
+
+    return (
+      <>
+        <p>count: {count}</p>
+        <p>pet: {pet}</p>
+      </>
+    )
+  }
+
+  const { getByText } = render(
+    <Provider initialValues={[[petAtom, 'dog']]}>
+      <Display />
+    </Provider>
+  )
+
+  await waitFor(() => {
+    getByText('count: 1')
+    getByText('pet: dog')
+  })
+})
