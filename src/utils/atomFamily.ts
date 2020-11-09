cf8ce98893ef1639bb6f2fa6ef8d389df427cd08
Victor Malov <victor.malov@breeffy.com>
2020-11-09 18:04:30 +0300

    fixes #177: use areEqual in atom remove

--- src/utils/atomFamily.ts
+++ src/utils/atomFamily.ts
@@ -67,6 +67,10 @@ export function atomFamily<Param, Value, Update>(
   let shouldRemove: ShouldRemove<Param> | null = null
   const atoms: Map<Param, [AtomType, CreatedAt]> = new Map()
   const createAtom = (param: Param) => {
+    if (param === undefined) {
+      throw new Error("param can't be undefined")
+    }
+
     let item: [AtomType, CreatedAt] | undefined
     if (areEqual === undefined) {
       item = atoms.get(param)
@@ -96,8 +100,24 @@ export function atomFamily<Param, Value, Update>(
     return newAtom
   }
 
-  createAtom.remove = (p: Param) => {
-    atoms.delete(p)
+  createAtom.remove = (param: Param) => {
+    let atomKey: Param | undefined
+    if (areEqual === undefined) {
+      atomKey = param
+    } else {
+      for (let [key, _] of atoms) {
+        if (areEqual(key, param)) {
+          atomKey = key
+          break
+        }
+      }
+    }
+
+    // We can miss case when param is undefined
+    // Do not allow undefined key in Map
+    if (atomKey !== undefined) {
+      atoms.delete(atomKey)
+    }
   }
 
   createAtom.setShouldRemove = (fn: ShouldRemove<Param> | null) => {
