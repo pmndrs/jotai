be57b8fa81e46110c3f3f7c5a854dc751df959f1
daishi <daishi@axlight.com>
2020-11-10 22:39:40 +0900

    feat: freeze atom util

---
+++ src/utils/freezeAtom.ts
@@ -0,0 +1,29 @@
+import { atom, Atom } from 'jotai'
+
+import type { Getter } from '../core/types'
+
+const deepFreeze = (obj: unknown) => {
+  if (typeof obj !== 'object' || obj === null) return
+  Object.freeze(obj)
+  const propNames = Object.getOwnPropertyNames(obj)
+  for (const name of propNames) {
+    const value = (obj as any)[name]
+    deepFreeze(value)
+  }
+}
+
+export function freezeAtom<T extends Atom<unknown>>(atomToFreeze: T) {
+  const origRead = atomToFreeze.read
+  atomToFreeze.read = (get: Getter) => {
+    const value = origRead(get)
+    deepFreeze(value)
+    return value
+  }
+  return atomToFreeze
+}
+
+const atomFrozen: typeof atom = ((read: any, write: any) =>
+  freezeAtom(atom(read, write))) as any
+
+export const atomFrozenInDev =
+  process.env.NODE_ENV === 'development' ? atomFrozen : atom
