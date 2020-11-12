6f855e9ba27200524a93b9b520f7156bd703ffc8
daishi <daishi@axlight.com>
2020-11-12 09:31:19 +0900

    make freezeAtom pure func

--- src/utils/freezeAtom.ts
+++ src/utils/freezeAtom.ts
@@ -2,28 +2,29 @@ import { atom, Atom } from 'jotai'
 
 import type { Getter } from '../core/types'
 
-const deepFreeze = (obj: unknown) => {
+const deepFreeze = (obj: any) => {
   if (typeof obj !== 'object' || obj === null) return
   Object.freeze(obj)
   const propNames = Object.getOwnPropertyNames(obj)
   for (const name of propNames) {
-    const value = (obj as any)[name]
+    const value = obj[name]
     deepFreeze(value)
   }
+  return obj
 }
 
-export function freezeAtom<T extends Atom<unknown>>(atomToFreeze: T) {
-  const origRead = atomToFreeze.read
-  atomToFreeze.read = (get: Getter) => {
-    const value = origRead(get)
-    deepFreeze(value)
-    return value
-  }
+export function freezeAtom<T extends Atom<any>>(anAtom: T) {
+  const frozenAtom: any = atom(
+    (get) => deepFreeze(get(anAtom)),
+    (_get, set, arg) => set(anAtom as any, arg)
+  )
+  return frozenAtom as T
 }
 
 const atomFrozen: typeof atom = ((read: any, write: any) => {
   const anAtom = atom(read, write)
-  freezeAtom(anAtom)
+  const origRead = anAtom.read
+  anAtom.read = (get: Getter) => deepFreeze(origRead(get))
   return anAtom
 }) as any
 
