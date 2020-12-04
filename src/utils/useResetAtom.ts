43a3cc23dde09af8bdc99f6fa3b4b8549fd22bb2
Yaroslav Lapin <jlarky@gmail.com>
2020-12-03 23:32:25 -0800

    we can now split atomWithReset and useResetAtom (#228)
    * it wasn't possible before since RESET wasn't exported

--- src/utils/useResetAtom.ts
+++ src/utils/useResetAtom.ts
@@ -1,26 +1,6 @@
 import { useMemo } from 'react'
 import { atom, useAtom, WritableAtom } from 'jotai'
-
-import type { SetStateAction } from '../core/types'
-
-export const RESET = Symbol()
-
-export function atomWithReset<Value>(initialValue: Value) {
-  type Update = SetStateAction<Value> | typeof RESET
-  const anAtom: any = atom<Value, Update>(initialValue, (get, set, update) => {
-    if (update === RESET) {
-      set(anAtom, initialValue)
-    } else {
-      set(
-        anAtom,
-        typeof update === 'function'
-          ? (update as (prev: Value) => Value)(get(anAtom))
-          : update
-      )
-    }
-  })
-  return anAtom as WritableAtom<Value, Update>
-}
+import { RESET } from './atomWithReset'
 
 export function useResetAtom<Value>(anAtom: WritableAtom<Value, typeof RESET>) {
   const writeOnlyAtom = useMemo(
