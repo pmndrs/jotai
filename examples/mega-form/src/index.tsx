9bbeef5c6e91993db70b971cf1ef5d5c09463b93
Meris Bahtijaragic <meris.bahti@gmail.com>
2021-02-06 14:49:29 +0100

    fix(examples): add example with mega form

---
+++ examples/mega-form/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+import './style.css'
+
+const rootElement = document.getElementById('root')
+render(<App />, rootElement)
