438e752e0b9bb755366d7277329706885cf4eaf2
M. Bagher Abiat <zorofight94@gmail.com>
2021-02-17 13:32:57 +0330

    new impl test

--- tests/query/atomWithQuery.test.tsx
+++ tests/query/atomWithQuery.test.tsx
@@ -35,7 +35,37 @@ it('query basic test', async () => {
   await findByText('loading')
   await findByText('count: 0')
 })
+it('query basic test with object instead of function', async () => {
+  const countAtom = atomWithQuery({
+    queryKey: 'count',
+    queryFn: async () => {
+      return await fakeFetch({ count: 0 })
+    },
+  })
+  const Counter: React.FC = () => {
+    const [
+      {
+        response: { count },
+      },
+    ] = useAtom(countAtom)
+    return (
+      <>
+        <div>count: {count}</div>
+      </>
+    )
+  }
 
+  const { findByText } = render(
+    <Provider>
+      <React.Suspense fallback="loading">
+        <Counter />
+      </React.Suspense>
+    </Provider>
+  )
+
+  await findByText('loading')
+  await findByText('count: 0')
+})
 it('query refetch', async () => {
   let count = 0
   const mockFetch = jest.fn(fakeFetch)
