91be0a34d55fe75f4460416530612ee937c8f301
aslemammad <zorofight94@gmail.com>
2021-03-15 13:30:50 +0330

    test

---
+++ tests/optimization.test.tsx
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react'
+import { fireEvent, render, waitFor } from '@testing-library/react'
+import { atom, useAtom } from '../src/index'
+
+it('useless re-renders with static atoms', async () => {
+  // check out https://codesandbox.io/s/setatom-bail-failure-forked-m82r5?file=/src/App.tsx to see the expected re-renders
+  const countAtom = atom(0)
+  const unrelatedAtom = atom(0)
+
+  const Counter: React.FC = () => {
+    const [count, setCount] = useAtom(countAtom)
+    useAtom(unrelatedAtom)
+    const commits = useRef(0)
+    ++commits.current
+
+    return (
+      <>
+        <div>
+          <p>commits: {commits.current}</p>
+          <p>count: {count}</p>
+        </div>
+        <button onClick={() => setCount((c) => c + 1)}>button</button>
+      </>
+    )
+  }
+
+  const { getByText, findByText } = render(<Counter />)
+
+  await waitFor(() => {
+    getByText('count: 0')
+    getByText('commits: 1')
+  })
+  fireEvent.click(getByText('button'))
+  await waitFor(() => {
+    getByText('count: 1')
+    getByText('commits: 2')
+  })
+  fireEvent.click(getByText('button'))
+  await waitFor(() => {
+    getByText('count: 2')
+    getByText('commits: 3')
+  })
+})
