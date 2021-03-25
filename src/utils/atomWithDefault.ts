ec2efef3d8458af9dda47983ecfd8dabb19f5ef7
daishi <daishi@axlight.com>
2021-03-25 21:35:51 +0900

    atom with default with two atoms

--- src/utils/atomWithDefault.ts
+++ src/utils/atomWithDefault.ts
@@ -1,27 +1,26 @@
 import { atom, PrimitiveAtom } from 'jotai'
 import type { Read } from '../core/types'
 
-// This depends on some internal undocumented behaviors.
-// It's not recommended to use this pattern in application code.
-// If one needs something similar in application code,
-// the recommendation instead is to use three atoms.
-// Refer: https://github.com/pmndrs/jotai/issues/352#issuecomment-797829152
-
 export function atomWithDefault<Value>(
   getDefault: Read<Value>
 ): PrimitiveAtom<Value> {
-  const EMPTY = Symbol()
-  const anAtom: any = atom(
+  const overwrittenAtom = atom(false)
+  const anAtom: PrimitiveAtom<Value> = atom(
     (get) => {
-      const current = get(anAtom)
-      if (current !== EMPTY) {
-        return current
+      if (get(overwrittenAtom)) {
+        return get(anAtom)
       }
       return getDefault(get)
     },
-    (get, set, update) =>
-      set(anAtom, typeof update === 'function' ? update(get(anAtom)) : update)
+    (get, set, update) => {
+      set(overwrittenAtom, true)
+      set(
+        anAtom,
+        typeof update === 'function'
+          ? (update as (prev: Value) => Value)(get(anAtom))
+          : update
+      )
+    }
   )
-  anAtom.init = EMPTY
   return anAtom
 }
