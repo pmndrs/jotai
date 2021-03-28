7fcabcfd34dfb827d9052e9216efa4eabe5ad65e
Mathis Møller <thisen-dk@hotmail.com>
2021-03-28 11:22:52 +0200

    Feature: waitFor utilities (#358)
    * wip: waitForAll
    
    * feat: waitForAll
    
    * wip returning a record from waitForAll
    
    * feat: use weak cache to keep waitForAll atom reference
    
    * Add possibly for records in waitFor
    
    * Cleanup types
    
    * fix types, improve tests
    
    * fix: do not cache atoms with named
    
    * Add error handling test
    
    * Fix test and timers
    
    * Add docs
    
    * Fix header
    
    * chore remove unused import
    
    * Return promise.all
    
    * Update size snapshot
    
    * chore: remove unused/uneffective atom cache
    
    * Update docs with csb
    
    Co-authored-by: daishi <daishi@axlight.com>
    Co-authored-by: Mathis Møller <mmm@cryosinternational.com>

---
+++ src/utils/waitForAll.ts
@@ -0,0 +1,53 @@
+import { Atom, atom } from 'jotai'
+
+export function waitForAll<Values extends Record<string, unknown>>(
+  atoms: { [K in keyof Values]: Atom<Values[K]> }
+): Atom<Values>
+
+export function waitForAll<Values extends unknown[]>(
+  atoms: { [K in keyof Values]: Atom<Values[K]> }
+): Atom<Values>
+
+export function waitForAll<Values extends Record<string, unknown> | unknown[]>(
+  atoms: { [K in keyof Values]: Atom<Values[K]> }
+) {
+  const derivedAtom = atom((get) => {
+    const promises: Promise<unknown>[] = []
+    const values = unwrapAtoms(atoms).map((anAtom, index) => {
+      try {
+        return get(anAtom)
+      } catch (e) {
+        if (e instanceof Promise) {
+          promises[index] = e
+        } else {
+          throw e
+        }
+      }
+    })
+    if (promises.length) {
+      return Promise.all(promises)
+    }
+    return wrapResults(atoms, values)
+  })
+  return derivedAtom
+}
+
+const unwrapAtoms = <Values extends Record<string, unknown> | unknown[]>(
+  atoms: { [K in keyof Values]: Atom<Values[K]> }
+): Atom<unknown>[] =>
+  Array.isArray(atoms)
+    ? atoms
+    : Object.getOwnPropertyNames(atoms).map(
+        (key) => (atoms as Record<string, Atom<unknown>>)[key]
+      )
+
+const wrapResults = <Values extends Record<string, unknown> | unknown[]>(
+  atoms: { [K in keyof Values]: Atom<Values[K]> },
+  results: unknown[]
+): unknown[] | Record<string, unknown> =>
+  Array.isArray(atoms)
+    ? results
+    : Object.getOwnPropertyNames(atoms).reduce(
+        (out, key, idx) => ({ ...out, [key]: results[idx] }),
+        {}
+      )
