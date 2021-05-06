84221540813b3c3ae5aa99db67a1dd943e65000b
daishi <daishi@axlight.com>
2021-05-06 22:46:31 +0900

    wip: urql/atomWithQuery (#427)

--- src/urql/atomWithQuery.ts
+++ src/urql/atomWithQuery.ts
@@ -1,12 +1,5 @@
 import { DocumentNode } from 'graphql'
-import { pipe, subscribe } from 'wonka'
-import {
-  Client,
-  TypedDocumentNode,
-  OperationContext,
-  OperationResult,
-  RequestPolicy,
-} from '@urql/core'
+import { TypedDocumentNode, OperationContext, RequestPolicy } from '@urql/core'
 import { WritableAtom, atom } from 'jotai'
 import type { Getter } from '../core/types'
 import { getClient } from './clientAtom'
@@ -36,64 +29,34 @@ export function atomWithQuery<Data, Variables extends object>(
   >(
     (get) => {
       const args = createQueryArgs(get)
-      const observerAtom = atom(
-        null,
-        (
-          get,
-          set,
-          action:
-            | { type: 'init'; initializer: (client: Client) => void }
-            | { type: 'result'; result: OperationResult }
-        ) => {
-          if (action.type === 'init') {
-            set(
-              dataAtom,
-              new Promise<Data>(() => {}) // new fetch
-            )
-            if (!args.pause) {
-              action.initializer(getClient(get, set))
-            }
-          } else if (action.type === 'result') {
-            const data = get(dataAtom)
-            if (data === null && action.result.data !== undefined) {
-              set(dataAtom, action.result.data)
-            }
-          }
-        }
-      )
-      observerAtom.onMount = (dispatch) => {
-        let unsub: (() => void) | undefined | false
-        const initializer = (client: Client) => {
-          const subscription = pipe(
-            client.query(args.query, args.variables, {
+      const initAtom = atom(null, (get, set) => {
+        set(
+          dataAtom,
+          new Promise<Data>(() => {}) // new fetch
+        )
+        if (!args.pause) {
+          const client = getClient(get, set)
+          client
+            .query(args.query, args.variables, {
               requestPolicy: args.requestPolicy,
               ...args.context,
-            }),
-            subscribe((result) => {
-              dispatch({ type: 'result', result })
             })
-          )
-          if (unsub === false) {
-            subscription.unsubscribe()
-          } else {
-            unsub = () => {
-              subscription.unsubscribe()
-            }
-          }
+            .toPromise()
+            .then((result) => {
+              if (result.data !== undefined) {
+                set(dataAtom, result.data)
+              }
+            })
         }
-        dispatch({ type: 'init', initializer })
-        return () => {
-          if (unsub) {
-            unsub()
-          }
-          unsub = false
-        }
+      })
+      initAtom.onMount = (init) => {
+        init()
       }
-      return [args, observerAtom]
+      return [args, initAtom]
     },
     (get, set, action) => {
       if (action.type === 'execute') {
-        const [args, observerAtom] = get(queryAtom)
+        const [args] = get(queryAtom)
         const client = getClient(get, set)
         client
           .query(args.query, args.variables, {
@@ -103,15 +66,17 @@ export function atomWithQuery<Data, Variables extends object>(
           })
           .toPromise()
           .then((result) => {
-            set(observerAtom, { type: 'result', result })
+            if (result.data !== undefined) {
+              set(dataAtom, result.data)
+            }
           })
       }
     }
   )
   const queryDataAtom = atom<Data, ResultActions>(
     (get) => {
-      const [, observerAtom] = get(queryAtom)
-      get(observerAtom) // use it here
+      const [, initAtom] = get(queryAtom)
+      get(initAtom) // use it here
       return get(dataAtom)
     },
     (_get, set, action) => set(queryAtom, action) // delegate action
