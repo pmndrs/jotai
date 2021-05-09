26f64d30e6289864330265ca340697924679c7f9
daishi <daishi@axlight.com>
2021-05-09 09:38:48 +0900

    wip: urql/atomWithQuery with unsub (#427)

--- src/urql/atomWithQuery.ts
+++ src/urql/atomWithQuery.ts
@@ -1,8 +1,16 @@
-import { DocumentNode } from 'graphql'
-import { TypedDocumentNode, OperationContext, RequestPolicy } from '@urql/core'
+import { pipe, subscribe } from 'wonka'
+import {
+  Client,
+  TypedDocumentNode,
+  OperationContext,
+  OperationResult,
+  RequestPolicy,
+} from '@urql/core'
 import { WritableAtom, atom } from 'jotai'
 import type { Getter } from '../core/types'
 import { getClient } from './clientAtom'
+
+type GraphQLExtensions = OperationResult['extensions']
 
 type ResultActions = {
   type: 'execute'
@@ -10,7 +18,7 @@ type ResultActions = {
 }
 
 type QueryArgs<Data, Variables extends object> = {
-  query: string | DocumentNode | TypedDocumentNode<Data, Variables>
+  query: TypedDocumentNode<Data, Variables>
   variables?: Variables
   requestPolicy?: RequestPolicy
   context?: Partial<OperationContext>
@@ -19,9 +27,11 @@ type QueryArgs<Data, Variables extends object> = {
 
 export function atomWithQuery<Data, Variables extends object>(
   createQueryArgs: (get: Getter) => QueryArgs<Data, Variables>
-): WritableAtom<Data, ResultActions> {
-  const dataAtom = atom<Data | Promise<Data>>(
-    new Promise<Data>(() => {}) // infinite pending
+): WritableAtom<[Data, GraphQLExtensions], ResultActions> {
+  const operationResultAtom = atom<
+    OperationResult<Data, Variables> | Promise<OperationResult<Data, Variables>>
+  >(
+    new Promise<OperationResult<Data, Variables>>(() => {}) // infinite pending
   )
   const queryAtom = atom<
     [QueryArgs<Data, Variables>, WritableAtom<null, any>],
@@ -29,28 +39,57 @@ export function atomWithQuery<Data, Variables extends object>(
   >(
     (get) => {
       const args = createQueryArgs(get)
-      const initAtom = atom(null, (get, set) => {
-        set(
-          dataAtom,
-          new Promise<Data>(() => {}) // new fetch
-        )
-        if (!args.pause) {
-          const client = getClient(get, set)
-          client
-            .query(args.query, args.variables, {
+      const initAtom = atom(
+        null,
+        (
+          get,
+          set,
+          action:
+            | { type: 'init'; initializer: (client: Client) => void }
+            | { type: 'result'; result: OperationResult<Data, Variables> }
+        ) => {
+          if (action.type === 'init') {
+            set(
+              operationResultAtom,
+              new Promise<OperationResult<Data, Variables>>(() => {}) // new fetch
+            )
+            if (!args.pause) {
+              action.initializer(getClient(get, set))
+            }
+          } else if (action.type === 'result') {
+            if (!action.result.stale) {
+              set(operationResultAtom, action.result)
+            }
+          }
+        }
+      )
+      initAtom.onMount = (dispatch) => {
+        let unsub: (() => void) | undefined | false
+        const initializer = (client: Client) => {
+          const subscription = pipe(
+            client.query(args.query, args.variables, {
               requestPolicy: args.requestPolicy,
               ...args.context,
+            }),
+            subscribe((result) => {
+              dispatch({ type: 'result', result })
             })
-            .toPromise()
-            .then((result) => {
-              if (result.data !== undefined) {
-                set(dataAtom, result.data)
-              }
-            })
+          )
+          if (unsub === false) {
+            subscription.unsubscribe()
+          } else {
+            unsub = () => {
+              subscription.unsubscribe()
+            }
+          }
         }
-      })
-      initAtom.onMount = (init) => {
-        init()
+        dispatch({ type: 'init', initializer })
+        return () => {
+          if (unsub) {
+            unsub()
+          }
+          unsub = false
+        }
       }
       return [args, initAtom]
     },
@@ -66,18 +105,26 @@ export function atomWithQuery<Data, Variables extends object>(
           })
           .toPromise()
           .then((result) => {
-            if (result.data !== undefined) {
-              set(dataAtom, result.data)
+            if (!result.stale /* XXX can this happen? */) {
+              set(operationResultAtom, result)
             }
           })
       }
     }
   )
-  const queryDataAtom = atom<Data, ResultActions>(
+  const queryDataAtom = atom<[Data, GraphQLExtensions], ResultActions>(
     (get) => {
       const [, initAtom] = get(queryAtom)
       get(initAtom) // use it here
-      return get(dataAtom)
+      const operationResult = get(operationResultAtom)
+      if (operationResult.error) {
+        throw operationResult.error
+      }
+      if (operationResult.data === undefined) {
+        // XXX can this happen?
+        throw new Error('no data in operation result')
+      }
+      return [operationResult.data, operationResult.extensions]
     },
     (_get, set, action) => set(queryAtom, action) // delegate action
   )
