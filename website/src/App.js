fd13aa6943ed8ce9775e965fc290d06f099cb9a3
daishi <daishi@axlight.com>
2021-05-09 21:34:43 +0900

    fix tests

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg'
-import './App.css'
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
