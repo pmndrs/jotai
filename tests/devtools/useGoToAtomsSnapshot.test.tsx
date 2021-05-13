9bb008dd4d8fde81165e8701a17738f8d690abc3
Thisen <thisen-dk@hotmail.com>
2021-05-13 18:19:09 +0200

    wip tests

---
+++ tests/devtools/useGoToAtomsSnapshot.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { fireEvent, render } from '@testing-library/react'
+import { Provider, atom, useAtom } from '../../src/index'
+import { useAtomsSnapshot, useGoToAtomsSnapshot } from '../../src/devtools'
+
+beforeEach(() => {
+  process.env.NODE_ENV = 'development'
+})
+afterEach(() => {
+  process.env.NODE_ENV = 'test'
+})
+
+it('useGoToAtomsSnapshot should modify atoms snapshot', async () => {
+  const petAtom = atom('cat')
+
+  const DisplayPet: React.FC = () => {
+    const [pet] = useAtom(petAtom)
+    return <p>{pet}</p>
+  }
+
+  const UpdateSnapshot: React.FC = () => {
+    const snapshot = useAtomsSnapshot()
+    const goToSnapshot = useGoToAtomsSnapshot()
+    return (
+      <button
+        onClick={() => {
+          const newSnapshot = new Map(snapshot)
+          newSnapshot.set(petAtom, 'dog')
+          goToSnapshot(snapshot)
+        }}>
+        click
+      </button>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <Provider>
+      <DisplayPet />
+      <UpdateSnapshot />
+    </Provider>
+  )
+
+  await findByText('cat')
+  fireEvent.click(getByText('click'))
+  await findByText('dog')
+})
