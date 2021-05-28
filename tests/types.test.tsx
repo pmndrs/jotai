2926444f0a478130f22d2f306325271d4c6b442c
Mathis Møller <mmm@cryosinternational.com>
2021-05-28 09:45:32 +0200

    Add type tests of useAtom

--- tests/types.test.tsx
+++ tests/types.test.tsx
@@ -1,5 +1,6 @@
 import { expectType } from 'ts-expect'
-import { atom, Atom, PrimitiveAtom, WritableAtom } from '../src/index'
+import { atom, Atom, PrimitiveAtom, useAtom, WritableAtom } from '../src/index'
+import { SetAtom } from '../src/core/atom'
 
 it('atom() should return the correct types', () => {
   function Component() {
@@ -25,5 +26,32 @@ it('atom() should return the correct types', () => {
       set(primitiveAtom, get(primitiveAtom) - 1)
     })
     expectType<WritableAtom<null, number>>(writeonlyDerivedAtom)
+  }
+})
+
+it('useAtom should return the correct types', () => {
+  function Component() {
+    // primitive atom
+    const primitiveAtom = atom(0)
+    expectType<[number, SetAtom<number>]>(useAtom(primitiveAtom))
+
+    // read-only derived atom
+    const readonlyDerivedAtom = atom((get) => get(primitiveAtom) * 2)
+    expectType<[number, SetAtom<number>]>(useAtom(readonlyDerivedAtom))
+
+    // read-write derived atom
+    const readWriteDerivedAtom = atom(
+      (get) => get(primitiveAtom),
+      (get, set, value: number) => {
+        set(primitiveAtom, get(primitiveAtom) + value)
+      }
+    )
+    expectType<[number, SetAtom<number>]>(useAtom(readWriteDerivedAtom))
+
+    // write-only derived atom
+    const writeonlyDerivedAtom = atom(null, (get, set) => {
+      set(primitiveAtom, get(primitiveAtom) - 1)
+    })
+    expectType<[null, SetAtom<number>]>(useAtom(writeonlyDerivedAtom))
   }
 })
