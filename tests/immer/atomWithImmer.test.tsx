d1e3bc79b8cb301fc3d348f56ec6f567dcc78ce5
aslemammad <zorofight94@gmail.com>
2021-06-04 18:20:37 +0430

    add a test

--- tests/immer/atomWithImmer.test.tsx
+++ tests/immer/atomWithImmer.test.tsx
@@ -38,3 +38,32 @@ it('atomWithImmer with useAtom', async () => {
   fireEvent.click(getByText('Decrease'))
   await findByText('count: 0')
 })
+
+it('atomWithImmer with WritableAtom<Value, Value> signature', async () => {
+  const countAtom = atomWithImmer(0)
+
+  const Parent: React.FC = () => {
+    const [count, setCount] = useAtom(countAtom)
+    return (
+      <>
+        <div>count: {count}</div>
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+        <button onClick={() => setCount(count - 1)}>Decrease</button>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <Provider>
+      <Parent />
+    </Provider>
+  )
+
+  await findByText('count: 0')
+
+  fireEvent.click(getByText('Increase'))
+  await findByText('count: 1')
+
+  fireEvent.click(getByText('Decrease'))
+  await findByText('count: 0')
+})
