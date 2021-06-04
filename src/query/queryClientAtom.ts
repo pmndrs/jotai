cd485f8622f08aaed2ccf7eaa873f1a57f0aa990
Daishi Kato <dai-shi@users.noreply.github.com>
2021-06-04 23:57:01 +0900

    fix(query): rewrite atomWithQuery (#505)
    * add failing tests for #500
    
    * remove unsupported tests
    
    * fix(query): rewrite atomWithQuery
    
    * chore: rename r
    
    * chore: prefer switch
    
    * prefer unsubscribe to destory

--- src/query/queryClientAtom.ts
+++ src/query/queryClientAtom.ts
@@ -1,14 +1,4 @@
 import { atom } from 'jotai'
-import type { Getter, Setter } from 'jotai'
 import { QueryClient } from 'react-query'
 
-export const queryClientAtom = atom<QueryClient | null>(null)
-
-export const getQueryClient = (get: Getter, set: Setter): QueryClient => {
-  let queryClient = get(queryClientAtom)
-  if (queryClient === null) {
-    queryClient = new QueryClient()
-    set(queryClientAtom, queryClient)
-  }
-  return queryClient
-}
+export const queryClientAtom = atom(() => new QueryClient())
