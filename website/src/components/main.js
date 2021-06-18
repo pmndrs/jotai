98bcfa6f1711cf6363c489fc54ce0996abf23d42
sandren <20441876+sandren@users.noreply.github.com>
2021-06-17 23:06:20 -0400

    fix: spacing

--- website/src/components/main.js
+++ website/src/components/main.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 export const Main = ({ children }) => {
   return (
-    <main className="w-full max-w-4xl mx-auto p-8 lg:p-16 pt-26 lg:pt-32 space-y-16">
-      {children}
-    </main>
+    <main className="max-w-4xl mx-auto p-8 lg:p-16 pt-26 lg:pt-32 space-y-16">{children}</main>
   );
 };
