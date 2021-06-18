9a61b0299e0a0d753e1aafbd28a960129542b702
daishi <daishi@axlight.com>
2021-06-18 17:49:00 +0900

    wip: follow jotai/query pattern

--- src/urql/clientAtom.ts
+++ src/urql/clientAtom.ts
@@ -1,14 +1,4 @@
 import { atom } from 'jotai'
-import { Client, createClient } from '@urql/core'
-import type { Getter, Setter } from '../core/atom'
+import { createClient } from '@urql/core'
 
-export const clientAtom = atom<Client | null>(null)
-
-export const getClient = (get: Getter, set: Setter): Client => {
-  let client = get(clientAtom)
-  if (client === null) {
-    client = createClient({ url: '/graphql' })
-    set(clientAtom, client)
-  }
-  return client
-}
+export const clientAtom = atom(() => createClient({ url: '/graphql' }))
