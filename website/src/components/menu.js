a4085bfce1a4f3e504b9e528277428c13521cf7f
sandren <20441876+sandren@users.noreply.github.com>
2021-06-25 15:51:16 -0400

    fix(website): resolve popover issue on mobile (try 2)

--- website/src/components/menu.js
+++ website/src/components/menu.js
@@ -6,11 +6,11 @@ import { Button, Navigation } from '../components'
 export const Menu = () => {
   return (
     <div className="fixed bottom-0 right-0 lg:hidden p-4">
-      <Popover className="flex flex-col items-end space-y-4">
-        <Popover.Panel className="relative z-10">
-          <Navigation className="flex flex-col p-4 space-y-4 border border-gray-700 rounded-lg bg-gray-900 shadow-2xl" />
+      <Popover className="relative z-10 flex flex-col items-end space-y-4">
+        <Popover.Panel className="relative z-20">
+          <Navigation className="relative z-30 flex flex-col p-4 space-y-4 border border-gray-700 rounded-lg bg-gray-900 shadow-2xl" />
         </Popover.Panel>
-        <Popover.Button className="relative z-20 focus:outline-none shadow-2xl">
+        <Popover.Button className="relative z-40 focus:outline-none shadow-2xl">
           <Button
             icon="menu"
             dark={true}
