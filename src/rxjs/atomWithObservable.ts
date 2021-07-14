b88ee0876b0952fe68826b709ae748da29a6190d
daishi <daishi@axlight.com>
2021-07-14 11:02:25 +0900

    feat: writable atomWithObservable

--- src/rxjs/atomWithObservable.ts
+++ src/rxjs/atomWithObservable.ts
@@ -1,11 +1,19 @@
-import { Observable } from 'rxjs'
+import { Observable, Subject } from 'rxjs'
 import { first } from 'rxjs/operators'
 import { atom } from 'jotai'
-import type { Atom, Getter } from 'jotai'
+import type { Atom, WritableAtom, Getter } from 'jotai'
+
+export function atomWithObservable<TData>(
+  createObservable: (get: Getter) => Subject<TData>
+): WritableAtom<TData, TData>
 
 export function atomWithObservable<TData>(
   createObservable: (get: Getter) => Observable<TData>
-): Atom<TData> {
+): Atom<TData>
+
+export function atomWithObservable<TData>(
+  createObservable: (get: Getter) => Observable<TData> | Subject<TData>
+) {
   type Result = { data: TData } | { error: unknown }
   const observableResultAtom = atom((get) => {
     const observable = createObservable(get)
@@ -41,16 +49,27 @@ export function atomWithObservable<TData>(
       const subscription = observable.subscribe(dataListener, errorListener)
       return () => subscription.unsubscribe()
     }
-    return { resultAtom }
+    return { resultAtom, observable }
   })
-  const observableAtom = atom((get) => {
-    observableResultAtom.scope = observableAtom.scope
-    const { resultAtom } = get(observableResultAtom)
-    const result = get(resultAtom)
-    if ('error' in result) {
-      throw result.error
+  const observableAtom = atom(
+    (get) => {
+      observableResultAtom.scope = observableAtom.scope
+      const { resultAtom } = get(observableResultAtom)
+      const result = get(resultAtom)
+      if ('error' in result) {
+        throw result.error
+      }
+      return result.data
+    },
+    (get, _set, data: TData) => {
+      observableResultAtom.scope = observableAtom.scope
+      const { observable } = get(observableResultAtom)
+      if ('next' in observable) {
+        observable.next(data)
+      } else {
+        throw new Error('observable is not subject')
+      }
     }
-    return result.data
-  })
+  )
   return observableAtom
 }
--- tests/rxjs/atomWithObservable.test.tsx
+++ tests/rxjs/atomWithObservable.test.tsx
@@ -1,9 +1,9 @@
 import React, { Suspense } from 'react'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { getTestProvider } from '../testUtils'
 import { useAtom } from '../../src/index'
 import { atomWithObservable } from '../../src/rxjs'
-import { Observable } from 'rxjs'
+import { Observable, Subject } from 'rxjs'
 
 const Provider = getTestProvider()
 
@@ -30,4 +30,42 @@ it('count state', async () => {
   )
 
   await findByText('count: 1')
+})
+
+it('writable count state', async () => {
+  const observableAtom = atomWithObservable(() => {
+    const observable = new Observable<number>((subscriber) => {
+      subscriber.next(1)
+    })
+    const subject = new Subject<number>()
+    // is this usual to delay the subscription?
+    setTimeout(() => {
+      observable.subscribe(subject)
+    }, 100)
+    return subject
+  })
+
+  const Counter: React.FC = () => {
+    const [state$, dispatch] = useAtom(observableAtom)
+
+    return (
+      <>
+        count: {state$}
+        <button onClick={() => dispatch(9)}>button</button>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <Provider>
+      <Suspense fallback="loading...">
+        <Counter />
+      </Suspense>
+    </Provider>
+  )
+
+  await findByText('count: 1')
+
+  fireEvent.click(getByText('button'))
+  await findByText('count: 9')
 })
