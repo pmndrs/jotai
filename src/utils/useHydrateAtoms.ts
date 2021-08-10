1ebe3a48ac3884167b068554a322c620c6720f5f
Mathis Møller <mmm@cryosinternational.com>
2021-08-10 10:17:47 +0200

    Allow hydration on atom once per store

--- src/utils/useHydrateAtoms.ts
+++ src/utils/useHydrateAtoms.ts
@@ -1,27 +1,37 @@
-import { useContext, useMemo } from 'react'
+import { useContext } from 'react'
 import { SECRET_INTERNAL_getStoreContext as getStoreContext } from 'jotai'
 import type { Atom, Scope } from '../core/atom'
+import type { Store } from '../core/contexts'
 
-const hydratedSymbol = Symbol()
+const hydratedMap: WeakMap<Store, WeakSet<Atom<unknown>>> = new WeakMap()
 
 export function useHydrateAtoms(
-  values: Iterable<
-    readonly [Atom<unknown> & { [hydratedSymbol]?: boolean }, unknown]
-  >,
+  values: Iterable<readonly [Atom<unknown>, unknown]>,
   scope?: Scope
 ) {
   const StoreContext = getStoreContext(scope)
-  const restoreAtoms = useContext(StoreContext)[3]
+  const store = useContext(StoreContext)
+  const restoreAtoms = store[3]
 
-  useMemo(() => {
-    const tuplesToRestore = []
-    for (const tuple of values) {
-      const atom = tuple[0]
-      if (atom[hydratedSymbol] !== true) {
-        tuplesToRestore.push(tuple)
-        atom[hydratedSymbol] = true
-      }
+  const hydratedSet = getHydratedSet(store)
+  const tuplesToRestore = []
+  for (const tuple of values) {
+    const atom = tuple[0]
+    if (!hydratedSet.has(atom)) {
+      hydratedSet.add(atom)
+      tuplesToRestore.push(tuple)
     }
+  }
+  if (tuplesToRestore.length) {
     restoreAtoms(tuplesToRestore)
-  }, [values, restoreAtoms])
+  }
+}
+
+function getHydratedSet(store: Store) {
+  let hydratedSet = hydratedMap.get(store)
+  if (!hydratedSet) {
+    hydratedSet = new Set()
+    hydratedMap.set(store, hydratedSet)
+  }
+  return hydratedSet
 }
