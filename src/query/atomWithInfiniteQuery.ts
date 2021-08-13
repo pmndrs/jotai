1c1e183b0da3380beca5c98be56b1a6ef2a0e51e
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-08-13 09:59:04 -0500

    fix(query): improve initial data method (#653)
    * fix: improve initial data method
    
    * fix: better data condition, if syntax

--- src/query/atomWithInfiniteQuery.ts
+++ src/query/atomWithInfiniteQuery.ts
@@ -50,14 +50,24 @@ export function atomWithInfiniteQuery<
       let settlePromise:
         | ((data: InfiniteData<TData> | null, err?: TError) => void)
         | null = null
-      const getInitialData = () =>
-        typeof options.initialData === 'function'
-          ? (
-              options.initialData as InitialDataFunction<
-                InfiniteData<TQueryData>
-              >
-            )()
-          : options.initialData
+
+      const getInitialData = () => {
+        let data: InfiniteData<TQueryData> | InfiniteData<TData> | undefined =
+          queryClient.getQueryData<InfiniteData<TData>>(options.queryKey)
+
+        if (data === undefined && options.initialData) {
+          data =
+            typeof options.initialData === 'function'
+              ? (
+                  options.initialData as InitialDataFunction<
+                    InfiniteData<TQueryData>
+                  >
+                )()
+              : options.initialData
+        }
+
+        return data
+      }
 
       const initialData = getInitialData()
 
@@ -114,7 +124,7 @@ export function atomWithInfiniteQuery<
 
       const observer = new InfiniteQueryObserver(queryClient, defaultedOptions)
 
-      if (!initialData) {
+      if (initialData === undefined) {
         observer
           .fetchOptimistic(defaultedOptions)
           .then(listener)
