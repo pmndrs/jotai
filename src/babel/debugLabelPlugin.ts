ad2df1edc13944938aa2f1912ea4daff61ad2810
Thisen <thisen-dk@hotmail.com>
2021-09-11 14:39:46 +0200

    Handle default exports Add tests

--- src/babel/debugLabelPlugin.ts
+++ src/babel/debugLabelPlugin.ts
@@ -1,31 +1,57 @@
+import path from 'path'
 import babel, { PluginObj, types } from '@babel/core'
+import templateBuilder from '@babel/template'
 
 export default function debugLabelPlugin({
   types: t,
 }: typeof babel): PluginObj {
   return {
     visitor: {
-      VariableDeclaration(path) {
-        const declarators = path.get('declarations')
-        for (const declarator of declarators) {
-          if (
-            t.isIdentifier(declarator.node.id) &&
-            t.isCallExpression(declarator.node.init) &&
-            isAtom(t, declarator.node.init.callee)
-          ) {
-            declarator.parentPath.insertAfter(
-              t.expressionStatement(
-                t.assignmentExpression(
-                  '=',
-                  t.memberExpression(
-                    t.identifier(declarator.node.id.name),
-                    t.identifier('debugLabel')
-                  ),
-                  t.stringLiteral(declarator.node.id.name)
-                )
+      ExportDefaultDeclaration(nodePath, state) {
+        const { node } = nodePath
+        if (
+          t.isCallExpression(node.declaration) &&
+          isAtom(t, node.declaration.callee)
+        ) {
+          const filename = state.filename || 'unknown'
+
+          let displayName = path.basename(filename, path.extname(filename))
+
+          console.log(state)
+          // ./{module name}/index.js
+          if (displayName === 'index') {
+            displayName = path.basename(path.dirname(filename))
+          }
+          // Relies on visiting the variable declaration to add the debugLabel
+          const buildExport = templateBuilder(`
+          const %%atomIdentifier%% = %%atom%%;
+          export default %%atomIdentifier%%
+          `)
+          const ast = buildExport({
+            atomIdentifier: t.identifier(displayName),
+            atom: node.declaration,
+          })
+          nodePath.replaceWithMultiple(ast as babel.Node[])
+        }
+      },
+      VariableDeclarator(path) {
+        if (
+          t.isIdentifier(path.node.id) &&
+          t.isCallExpression(path.node.init) &&
+          isAtom(t, path.node.init.callee)
+        ) {
+          path.parentPath.insertAfter(
+            t.expressionStatement(
+              t.assignmentExpression(
+                '=',
+                t.memberExpression(
+                  t.identifier(path.node.id.name),
+                  t.identifier('debugLabel')
+                ),
+                t.stringLiteral(path.node.id.name)
               )
             )
-          }
+          )
         }
       },
     },
--- tests/babel/debugLabelPlugin.test.ts
+++ tests/babel/debugLabelPlugin.test.ts
@@ -3,42 +3,67 @@ import { transformSync } from '@babel/core'
 
 const plugin = path.join(__dirname, '../../src/babel/debugLabelPlugin')
 
-const transform = (code: string) =>
+const transform = (code: string, filename?: string) =>
   transformSync(code, {
     babelrc: false,
     configFile: false,
+    filename,
     plugins: [[plugin]],
   })?.code
 
 it('Should add a debugLabel to an atom', () => {
-  expect(
-    transform(`
-    const countAtom = atom(0);
-    `)
-  ).toMatchInlineSnapshot(`
+  expect(transform(`const countAtom = atom(0);`)).toMatchInlineSnapshot(`
     "const countAtom = atom(0);
     countAtom.debugLabel = \\"countAtom\\";"
   `)
 })
 
 it('Should handle a atom from a default export', () => {
-  expect(
-    transform(`
-    const countAtom = jotai.atom(0);
-    `)
-  ).toMatchInlineSnapshot(`
+  expect(transform(`const countAtom = jotai.atom(0);`)).toMatchInlineSnapshot(`
     "const countAtom = jotai.atom(0);
     countAtom.debugLabel = \\"countAtom\\";"
   `)
 })
 
 it('Should handle a atom being exported', () => {
-  expect(
-    transform(`
-    export const countAtom = atom(0);
-    `)
-  ).toMatchInlineSnapshot(`
+  expect(transform(`export const countAtom = atom(0);`)).toMatchInlineSnapshot(`
     "export const countAtom = atom(0);
     countAtom.debugLabel = \\"countAtom\\";"
+  `)
+})
+
+it('Should handle a default exported atom', () => {
+  expect(transform(`export default atom(0);`, 'countAtom.ts'))
+    .toMatchInlineSnapshot(`
+    "const countAtom = atom(0);
+    countAtom.debugLabel = \\"countAtom\\";
+    export default countAtom;"
+  `)
+})
+
+it('Should handle a default exported atom in a barrel file', () => {
+  expect(transform(`export default atom(0);`, 'atoms/index.ts'))
+    .toMatchInlineSnapshot(`
+    "const atoms = atom(0);
+    atoms.debugLabel = \\"atoms\\";
+    export default atoms;"
+  `)
+})
+
+it('Should handle all types of exports', () => {
+  expect(
+    transform(
+      `
+      export const countAtom = atom(0);
+      export default atom(0);
+    `,
+      'atoms/index.ts'
+    )
+  ).toMatchInlineSnapshot(`
+    "export const countAtom = atom(0);
+    countAtom.debugLabel = \\"countAtom\\";
+    const atoms = atom(0);
+    atoms.debugLabel = \\"atoms\\";
+    export default atoms;"
   `)
 })
