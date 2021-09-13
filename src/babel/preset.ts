0a0c6fcc7490c9501049dca8970151ad0a41ed8b
Thisen <thisen-dk@hotmail.com>
2021-09-13 19:31:47 +0200

    Default export the preset... oops

--- src/babel/preset.ts
+++ src/babel/preset.ts
@@ -1,7 +1,7 @@
 import babel from '@babel/core'
 import pluginDebugLabel from './plugin-debug-label'
 
-export function jotaiPreset(): { plugins: babel.PluginItem[] } {
+export default function jotaiPreset(): { plugins: babel.PluginItem[] } {
   return {
     plugins: [pluginDebugLabel],
   }
