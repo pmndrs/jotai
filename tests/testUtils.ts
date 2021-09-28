9adbe3152b7895318f9d04f64bc9666571747f2e
daishi <daishi@axlight.com>
2021-09-28 11:25:25 +0900

    using strategy matrix

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -1,7 +1,7 @@
 import { Provider } from 'jotai'
 
 export function getTestProvider() {
-  return process.env.PROVIDER_LESS_MODE
+  return process.env.PROVIDER_LESS_MODE === 'true'
     ? (props: any) => props.children
     : Provider
 }
