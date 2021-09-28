ba9f9020c48d7b3968445e411dfb3cca644e0691
daishi <daishi@axlight.com>
2021-09-28 11:45:53 +0900

    split default test

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -1,6 +1,11 @@
 import { Provider } from 'jotai'
 
 export function getTestProvider() {
+  if (process.env.PROVIDER_LESS_MODE === 'true') {
+    console.log('[[[[[[[[[[[Without Provider]]]]]]]]]]]')
+  } else {
+    console.log('[[[[[[[[[[[With Provider]]]]]]]]]]]')
+  }
   return process.env.PROVIDER_LESS_MODE === 'true'
     ? (props: any) => props.children
     : Provider
