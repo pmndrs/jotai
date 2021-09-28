59b1430d1b2386a3e606c6707e4fa33bc4be2250
daishi <daishi@axlight.com>
2021-09-28 12:32:34 +0900

    fix workflow job name

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -1,11 +1,6 @@
 import { Provider } from 'jotai'
 
 export function getTestProvider() {
-  console.log(
-    '==================PROVIDER_LESS_MODE',
-    process.env.PROVIDER_LESS_MODE,
-    process.env.PROVIDER_LESS_MODE === 'true'
-  )
   return process.env.PROVIDER_LESS_MODE === 'true'
     ? (props: any) => props.children
     : Provider
