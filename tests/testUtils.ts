6e33d926af56fd457152562cbdbbdc2172a313c1
daishi <daishi@axlight.com>
2021-09-28 11:58:14 +0900

    remove debug log

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -1,11 +1,6 @@
 import { Provider } from 'jotai'
 
 export function getTestProvider() {
-  if (process.env.PROVIDER_LESS_MODE === 'true') {
-    console.log('[[[[[[[[[[[Without Provider]]]]]]]]]]]')
-  } else {
-    console.log('[[[[[[[[[[[With Provider]]]]]]]]]]]')
-  }
   return process.env.PROVIDER_LESS_MODE === 'true'
     ? (props: any) => props.children
     : Provider
