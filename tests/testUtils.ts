eaeb50260db539fe84543c12202ef358bf377cb6
daishi <daishi@axlight.com>
2021-09-28 12:15:57 +0900

    mode in job name

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -1,6 +1,11 @@
 import { Provider } from 'jotai'
 
 export function getTestProvider() {
+  console.log(
+    '==================PROVIDER_LESS_MODE',
+    process.env.PROVIDER_LESS_MODE,
+    process.env.PROVIDER_LESS_MODE === 'true'
+  )
   return process.env.PROVIDER_LESS_MODE === 'true'
     ? (props: any) => props.children
     : Provider
