acdc5810a4cddbc519843f8b289e9d22f02b34c4
sandren <20441876+sandren@users.noreply.github.com>
2021-10-07 20:00:31 -0700

    feat(website): add support for anchor links

--- website/gatsby-ssr.js
+++ website/gatsby-ssr.js
@@ -3,12 +3,26 @@ import { Provider as JotaiProvider } from 'jotai'
 import { MDXProvider } from '@mdx-js/react'
 
 import { menuAtom, textAtom, countAtom } from './src/atoms'
-import { Code, CodeSandbox, InlineCode } from './src/components'
+import {
+  Code,
+  CodeSandbox,
+  InlineCode,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+} from './src/components'
 
 const components = {
   code: Code,
   inlineCode: InlineCode,
   CodeSandbox,
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  h5: H5,
+  h6: H6,
 }
 
 export const wrapRootElement = ({ element }) => (
---
+++ website/src/components/headlines.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export const H2 = ({ children }) => {
+  const anchor = getAnchor(children)
+  const link = `#${anchor}`
+
+  return (
+    <h2 id={anchor}>
+      <a href={link}>{children}</a>
+    </h2>
+  )
+}
+
+export const H3 = ({ children }) => {
+  const anchor = getAnchor(children)
+  const link = `#${anchor}`
+
+  return (
+    <h3 id={anchor}>
+      <a href={link}>{children}</a>
+    </h3>
+  )
+}
+
+export const H4 = ({ children }) => {
+  const anchor = getAnchor(children)
+  const link = `#${anchor}`
+
+  return (
+    <h4 id={anchor}>
+      <a href={link}>{children}</a>
+    </h4>
+  )
+}
+
+export const H5 = ({ children }) => {
+  const anchor = getAnchor(children)
+  const link = `#${anchor}`
+
+  return (
+    <h5 id={anchor}>
+      <a href={link}>{children}</a>
+    </h5>
+  )
+}
+
+export const H6 = ({ children }) => {
+  const anchor = getAnchor(children)
+  const link = `#${anchor}`
+
+  return (
+    <h6 id={anchor}>
+      <a href={link}>{children}</a>
+    </h6>
+  )
+}
+
+const getAnchor = (text) => {
+  return typeof text === 'string' ? text.replace(/[ ]/g, '-') : ''
+}
