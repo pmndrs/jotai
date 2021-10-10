2c49d1d8dce3bc4141cb9e18be9816b5eb791dbc
sandren <20441876+sandren@users.noreply.github.com>
2021-10-10 11:46:39 -0700

    feat(website): adjust font scaling of code blocks

--- website/src/components/inline-code.js
+++ website/src/components/inline-code.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const InlineCode = ({ children }) => {
   return (
-    <code className="relative -top-px px-1 py-0.5 bg-gray-100 rounded text-xs lg:text-sm text-black">
+    <code className="relative -top-px px-1 py-0.5 bg-gray-100 rounded text-black">
       {children}
     </code>
   )
