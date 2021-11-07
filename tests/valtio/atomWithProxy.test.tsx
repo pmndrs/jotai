70e985a5102b57cba8b3ccc870fdbd5e926c3d1c
Daishi Kato <dai-shi@users.noreply.github.com>
2021-11-07 12:51:08 +0900

    fix(test): improve valtio test (#819)
    * fix valtio test
    
    * increase timeout?
    
    * empty commit

--- tests/valtio/atomWithProxy.test.tsx
+++ tests/valtio/atomWithProxy.test.tsx
@@ -92,7 +92,7 @@ it('state with a promise', async () => {
     new Promise<string>((resolve) => {
       setTimeout(() => {
         resolve(status)
-      }, 100)
+      }, 500)
     })
 
   const proxyState = proxy({
@@ -120,14 +120,16 @@ it('state with a promise', async () => {
 
   const { findByText, getByText } = render(
     <Provider>
-      <Suspense fallback="loading...">
+      <Suspense fallback="loading">
         <Status />
       </Suspense>
     </Provider>
   )
 
+  await findByText('loading')
   await findByText('status: done')
   fireEvent.click(getByText('button'))
+  await findByText('loading')
   await findByText('status: modified')
 })
 
