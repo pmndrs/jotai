c39c66bfd2b7338aa248360b3a36a3ccab6edb3a
Mathis Møller <mmm@cryosinternational.com>
2021-11-22 22:55:59 +0100

    Docs: Add Vite documentation (#843)
    * Add Vite docs
    
    * Add startup command to Stackblitz template

---
+++ website/src/components/stackblitz.js
@@ -0,0 +1,19 @@
+import LazyLoad from 'react-lazyload'
+
+export const Stackblitz = ({ id, file }) => {
+  return (
+    <div className="mb-8 overflow-hidden shadow-lg rounded-md sm:rounded-lg border-b border-gray-200">
+      <LazyLoad height={400} once>
+        <iframe
+          title={id}
+          className="w-full h-[400px]"
+          src={`https://stackblitz.com/edit/${id}?embed=1${
+            file ? `&file=${file}` : ''
+          }&terminal=dev`}
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        />
+      </LazyLoad>
+    </div>
+  )
+}
