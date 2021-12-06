04649b3d0eeeea423efeecd29797ea9024c2a0c0
aslemammad <zorofight94@gmail.com>
2021-12-06 17:17:07 +0330

    dependencies

--- src/devtools/useAtomsDevtools.ts
+++ src/devtools/useAtomsDevtools.ts
@@ -1,14 +1,13 @@
+import { useContext, useEffect, useRef } from 'react'
+import { useAtomsSnapshot, useGotoAtomsSnapshot } from 'jotai/devtools'
+import { Atom, Scope } from '../core/atom'
+import { getScopeContext } from '../core/contexts'
 import {
   AtomState,
   DEV_GET_ATOM_STATE,
-  DEV_GET_MOUNTED_ATOMS,
   DEV_SUBSCRIBE_STATE,
-  RESTORE_ATOMS,
   Store,
 } from '../core/store'
-import { useContext, useEffect, useRef } from 'react'
-import { Atom, Scope } from '../core/atom'
-import { getScopeContext } from '../core/contexts'
 
 type Config = {
   instanceID?: number
@@ -40,46 +39,36 @@ type Extension = {
 
 type AtomsSnapshot = Map<Atom<unknown>, unknown>
 
-const createAtomsSnapshot = (
-  store: Store,
-  atoms: Atom<unknown>[]
-): AtomsSnapshot => {
-  const tuples = atoms.map<[Atom<unknown>, unknown]>((atom) => {
-    const atomState = store[DEV_GET_ATOM_STATE]?.(atom) ?? ({} as AtomState)
-    return [atom, atomState.v]
-  })
-  return new Map(tuples)
-}
-
-const isEqualSnapshot = (
-  aSnapshot: AtomsSnapshot | undefined,
-  bSnapshot: AtomsSnapshot
-) => {
-  if (aSnapshot?.size !== bSnapshot.size) {
-    return false
-  }
-
-  for (const [atom, aValue] of aSnapshot) {
-    const bValue = bSnapshot.get(atom)
-    if (aValue !== bValue) {
-      return false
-    }
-  }
-
-  return true
-}
-
-const serializeSnapshot = (snapshot: AtomsSnapshot): any => {
+const serializeSnapshot = (snapshot: AtomsSnapshot) => {
   const result: Record<string, unknown> = {}
 
   snapshot.forEach((v, atom) => {
-    result[`${atom}:${atom.debugLabel}`] = v
+    result[atomToDebuggingString(atom)] = v
+  })
+
+  return result
+}
+
+const atomToDebuggingString = (atom: Atom<unknown>) =>
+  `${atom}:${atom.debugLabel}`
+
+const getDependencies = (store: Store, snapshot: AtomsSnapshot) => {
+  const result: Record<string, string[]> = {}
+
+  snapshot.forEach((_, atom) => {
+    const atomState = store[DEV_GET_ATOM_STATE]?.(atom) ?? ({} as AtomState)
+
+    result[atomToDebuggingString(atom)] = [...atomState.d.keys()].map(
+      atomToDebuggingString
+    )
   })
 
   return result
 }
 
 export function useAtomsDevtools(name: string, scope?: Scope) {
+  const snapshot = useAtomsSnapshot()
+  const goToSnapshot = useGotoAtomsSnapshot()
   let extension: Extension | undefined
   try {
     extension = (window as any).__REDUX_DEVTOOLS_EXTENSION__ as Extension
@@ -131,72 +120,52 @@ export function useAtomsDevtools(name: string, scope?: Scope) {
                 const snapshot =
                   snapshots.current[message.payload.actionId - 1]!
 
-                const mountedAtoms = Array.from(
-                  store[DEV_GET_MOUNTED_ATOMS]?.() || []
-                )
-
-                if (
-                  ![...snapshot.keys()].every((atom) =>
-                    mountedAtoms.includes(atom)
-                  )
-                ) {
-                  console.warn(
-                    '[Warn] you cannot do devtools operations (Time-travelling, etc) on unmounted atoms\n'
-                  )
-                  return
-                }
-
-                store[RESTORE_ATOMS](snapshot)
+                goToSnapshot(snapshot)
                 return
               case 'PAUSE_RECORDING':
                 return (isRecording.current = !isRecording.current)
             }
         }
       })
+
+      devtools.current.shouldInit = true
     }
-
-    const callback = (updatedAtom?: Atom<unknown>, isNewAtom?: boolean) => {
-      if (!devtools.current) {
-        return
-      }
-      if (updatedAtom === undefined) {
-        devtools.current.init(undefined)
-        devtools.current.shouldInit = false
-        return
-      }
-
-      if (isTimeTraveling.current) {
-        // better solution to stop logging on JUMP_TO_ACTION & JUMP_TO_STATE ?
-        Promise.resolve().then(() => {
-          isTimeTraveling.current = false
-        })
-      } else if (isRecording.current) {
-        const atoms = Array.from(store[DEV_GET_MOUNTED_ATOMS]?.() || [])
-        if (updatedAtom && isNewAtom && !atoms.includes(updatedAtom)) {
-          atoms.push(updatedAtom)
-        }
-
-        const prevSnapshot = snapshots.current[snapshots.current.length - 1]
-        const snapshot = createAtomsSnapshot(store, atoms)
-        if (isEqualSnapshot(prevSnapshot, snapshot)) {
-          return
-        }
-
-        snapshots.current.push(snapshot)
-
-        const parsedSnapshot = serializeSnapshot(snapshot)
-
-        devtools.current.send(
-          `action:${snapshots.current.length} - ${new Date().toLocaleString()}`,
-          parsedSnapshot
-        )
-      }
-    }
-    const stateUnsubscribe = store[DEV_SUBSCRIBE_STATE]?.(callback)
-    callback()
     return () => {
-      stateUnsubscribe?.()
       devtoolsUnsubscribe?.()
     }
   }, [store])
+
+  useEffect(() => {
+    if (!devtools.current) {
+      return
+    }
+    if (devtools.current.shouldInit) {
+      devtools.current.init(undefined)
+      devtools.current.shouldInit = false
+      return
+    }
+
+    if ([...snapshot.keys()].length === 0) {
+      return
+    }
+
+    if (isTimeTraveling.current) {
+      // better solution to stop logging on JUMP_TO_ACTION & JUMP_TO_STATE ?
+      setTimeout(() => {
+        isTimeTraveling.current = false
+      }, 0)
+    } else if (isRecording.current) {
+      snapshots.current.push(snapshot)
+
+      const parsedSnapshot = serializeSnapshot(snapshot)
+
+      devtools.current.send(
+        `action:${snapshots.current.length} - ${new Date().toLocaleString()}`,
+        {
+          values: parsedSnapshot,
+          dependencies: getDependencies(store, snapshot),
+        }
+      )
+    }
+  }, [snapshot])
 }
