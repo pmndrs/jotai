45e60f676a7cd3a937b4595bbd14e93e058324e6
Thisen <thisen-dk@hotmail.com>
2021-12-07 23:03:51 +0100

    use getAtomState instead of readAtomState

--- src/core/store.ts
+++ src/core/store.ts
@@ -426,8 +426,9 @@ export const createStore = (
           // NOTE technically possible but restricted as it may cause bugs
           throw new Error('atom not writable')
         }
-        const aState = readAtomState(a)
-        if ('v' in aState && Object.is(aState.v, v)) {
+        const aState = getAtomState(a)
+        // Bail out, if update value and current value are equal
+        if (aState && 'v' in aState && Object.is(aState.v, v)) {
           return
         }
         setAtomPromiseOrValue(a, v)
