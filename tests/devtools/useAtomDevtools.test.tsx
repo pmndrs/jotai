efadb5ca187c3a7ce287e2b04f535ca224f7515e
aslemammad <zorofight94@gmail.com>
2021-12-10 23:59:16 +0330

    useAtomDevtools tests

---
+++ tests/devtools/useAtomDevtools.test.tsx
@@ -0,0 +1,201 @@
+import { fireEvent, render } from '@testing-library/react'
+import { atom, useAtom } from 'jotai'
+import { useAtomDevtools } from 'jotai/devtools'
+import { getTestProvider } from '../testUtils'
+
+const Provider = getTestProvider()
+
+let extensionSubscriber: ((message: any) => void) | undefined
+
+let sendArgs: [
+  string,
+  { values: Record<string, unknown>; dependencies: Record<string, unknown> }
+]
+
+const extension = {
+  subscribe: jest.fn((f) => {
+    extensionSubscriber = f
+    return () => {}
+  }),
+  unsubscribe: jest.fn(),
+  send: jest.fn((title, value) => {
+    sendArgs = [title, value]
+  }),
+  init: jest.fn(),
+  error: jest.fn(),
+}
+const extensionConnector = { connect: jest.fn(() => extension) }
+;(window as any).__REDUX_DEVTOOLS_EXTENSION__ = extensionConnector
+
+beforeEach(() => {
+  extensionConnector.connect.mockClear()
+  extension.subscribe.mockClear()
+  extension.unsubscribe.mockClear()
+  extension.send.mockClear()
+  extension.init.mockClear()
+  extension.error.mockClear()
+  extensionSubscriber = undefined
+})
+
+it('connects to the extension by initialiing', () => {
+  const countAtom = atom(0)
+  countAtom.debugLabel = 'countAtom'
+
+  const Counter = () => {
+    useAtomDevtools(countAtom)
+    const [count, setCount] = useAtom(countAtom)
+    return (
+      <>
+        <div>count: {count}</div>
+        <button onClick={() => setCount((c) => c + 1)}>button</button>
+      </>
+    )
+  }
+  render(
+    <Provider>
+      <Counter />
+    </Provider>
+  )
+
+  expect(extension.init).toHaveBeenLastCalledWith(0)
+})
+
+describe('If there is no extension installed...', () => {
+  beforeAll(() => {
+    ;(window as any).__REDUX_DEVTOOLS_EXTENSION__ = undefined
+  })
+  afterAll(() => {
+    ;(window as any).__REDUX_DEVTOOLS_EXTENSION__ = extensionConnector
+  })
+
+  const countAtom = atom(0)
+  countAtom.debugLabel = 'countAtom'
+
+  const Counter = () => {
+    useAtomDevtools(countAtom)
+    const [count, setCount] = useAtom(countAtom)
+    return (
+      <>
+        <div>count: {count}</div>
+        <button onClick={() => setCount((c) => c + 1)}>button</button>
+      </>
+    )
+  }
+  it('does not throw', () => {
+    expect(() => {
+      render(
+        <Provider>
+          <Counter />
+        </Provider>
+      )
+    }).not.toThrow()
+  })
+
+  it('warns in dev env', () => {
+    const originalNodeEnv = process.env.NODE_ENV
+    process.env.NODE_ENV = 'development'
+    const originalConsoleWarn = console.warn
+    console.warn = jest.fn()
+
+    render(
+      <Provider>
+        <Counter />
+      </Provider>
+    )
+    expect(console.warn).toHaveBeenLastCalledWith(
+      'Please install/enable Redux devtools extension'
+    )
+
+    process.env.NODE_ENV = originalNodeEnv
+    console.warn = originalConsoleWarn
+  })
+
+  it('does not warn if not in dev env', () => {
+    const consoleWarn = jest.spyOn(console, 'warn')
+
+    render(
+      <Provider>
+        <Counter />
+      </Provider>
+    )
+    expect(consoleWarn).not.toBeCalled()
+
+    consoleWarn.mockRestore()
+  })
+})
+
+it('updating state should call devtools.send', async () => {
+  const countAtom = atom(0)
+
+  const Counter = () => {
+    useAtomDevtools(countAtom)
+    const [count, setCount] = useAtom(countAtom)
+    return (
+      <>
+        <div>count: {count}</div>
+        <button onClick={() => setCount((c) => c + 1)}>button</button>
+      </>
+    )
+  }
+
+  extension.send.mockClear()
+  const { getByText, findByText } = render(
+    <Provider>
+      <Counter />
+    </Provider>
+  )
+
+  expect(extension.send).toBeCalledTimes(0)
+  fireEvent.click(getByText('button'))
+  await findByText('count: 1')
+  expect(extension.send).toBeCalledTimes(1)
+  fireEvent.click(getByText('button'))
+  await findByText('count: 2')
+  expect(extension.send).toBeCalledTimes(2)
+})
+
+describe('when it receives an message of type...', () => {
+  describe('DISPATCH and payload of type...', () => {
+    describe('JUMP_TO_STATE | JUMP_TO_ACTION...', () => {
+      it('time travelling', async () => {
+        const countAtom = atom(0)
+
+        const Counter = () => {
+          useAtomDevtools(countAtom)
+          const [count, setCount] = useAtom(countAtom)
+          return (
+            <>
+              <div>count: {count}</div>
+              <button onClick={() => setCount((c) => c + 1)}>button</button>
+            </>
+          )
+        }
+
+        extension.send.mockClear()
+        const { getByText, findByText } = render(
+          <Provider>
+            <Counter />
+          </Provider>
+        )
+
+        expect(extension.send).toBeCalledTimes(0)
+        fireEvent.click(getByText('button'))
+        await findByText('count: 1')
+        expect(extension.send).toBeCalledTimes(1)
+        debugger
+        ;(extensionSubscriber as (message: any) => void)({
+          type: 'DISPATCH',
+          payload: { type: 'JUMP_TO_ACTION' },
+          state: JSON.stringify(0),
+        })
+        await findByText('count: 0')
+        expect(extension.send).toBeCalledTimes(1)
+        fireEvent.click(getByText('button'))
+        await findByText('count: 1')
+        fireEvent.click(getByText('button'))
+        await findByText('count: 2')
+        expect(extension.send).toBeCalledTimes(3)
+      })
+    })
+  })
+})
