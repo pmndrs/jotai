c8f9192829eb58a6e3760c242c4cc7f52d530848
Jake Teton-Landis <just.1.jake@gmail.com>
2021-12-30 15:25:21 -0800

    atomWithReducer: change state arg -> value

--- src/utils/atomWithReducer.ts
+++ src/utils/atomWithReducer.ts
@@ -3,17 +3,17 @@ import type { WritableAtom } from 'jotai'
 
 export function atomWithReducer<Value, Action>(
   initialValue: Value,
-  reducer: (state: Value, action?: Action) => Value
+  reducer: (value: Value, action?: Action) => Value
 ): WritableAtom<Value, Action | undefined>
 
 export function atomWithReducer<Value, Action>(
   initialValue: Value,
-  reducer: (state: Value, action: Action) => Value
+  reducer: (value: Value, action: Action) => Value
 ): WritableAtom<Value, Action>
 
 export function atomWithReducer<Value, Action>(
   initialValue: Value,
-  reducer: (state: Value, action: Action) => Value
+  reducer: (value: Value, action: Action) => Value
 ) {
   const anAtom: any = atom(initialValue, (get, set, action: Action) =>
     set(anAtom, reducer(get(anAtom), action))
