49b19fb2a20d4c0fce90acc7fc0dc8f41aa5ccdd
daishi <daishi@axlight.com>
2022-01-18 16:21:17 +0900

    remove probably unnecessary `.push` in render

--- tests/query/atomWithInfiniteQuery.test.tsx
+++ tests/query/atomWithInfiniteQuery.test.tsx
@@ -251,8 +251,6 @@ it('should be able to refetch only specific pages when refetchPages is provided'
       [setState]
     )
 
-    states.push(state)
-
     return (
       <>
         <div>length: {state.pages.length}</div>
@@ -276,7 +274,6 @@ it('should be able to refetch only specific pages when refetchPages is provided'
   await findByText('loading')
   await findByText('length: 1')
   await findByText('page 1: 10')
-  await new Promise((r) => setTimeout(r, 100))
   fireEvent.click(getByText('fetch next page'))
   await findByText('length: 2')
   await findByText('page 2: 11')
