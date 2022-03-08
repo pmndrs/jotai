3c6523f0b12a6eb933e4e54f1d2b41c833458e54
Mathis Møller <thisen-dk@hotmail.com>
2022-03-08 18:47:53 +0100

    Add support for debugLabels for atom utils

--- src/babel/plugin-debug-label.ts
+++ src/babel/plugin-debug-label.ts
@@ -61,15 +61,29 @@ function isAtom(
   t: typeof types,
   callee: babel.types.Expression | babel.types.V8IntrinsicIdentifier
 ) {
-  if (t.isIdentifier(callee) && callee.name === 'atom') {
+  if (t.isIdentifier(callee) && atomFunctionNames.includes(callee.name)) {
     return true
   }
 
   if (t.isMemberExpression(callee)) {
     const { property } = callee
-    if (t.isIdentifier(property) && property.name === 'atom') {
+    if (t.isIdentifier(property) && atomFunctionNames.includes(property.name)) {
       return true
     }
   }
   return false
 }
+
+const atomFunctionNames = [
+  'atom',
+  'atomFamily',
+  'atomWithDefault',
+  'atomWithObservable',
+  'atomWithReducer',
+  'atomWithReset',
+  'atomWithStorage',
+  'freezeAtom',
+  'loadable',
+  'selectAtom',
+  'splitAtom',
+]
