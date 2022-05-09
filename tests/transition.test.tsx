54df141e205abaf3c086c3fbe313454b1e386734
daishi <daishi@axlight.com>
2022-05-09 13:08:49 +0900

    conditional transition test

--- tests/transition.test.tsx
+++ tests/transition.test.tsx
@@ -7,48 +7,53 @@ const Provider = getTestProvider()
 
 jest.mock('../src/core/useDebugState.ts')
 
-it('no extra commit with useTransition (#1125)', async () => {
-  const countAtom = atom(0)
-  const delayedAtom = atom(async (get) => {
-    await new Promise((r) => setTimeout(r, 100))
-    return get(countAtom)
-  })
+const describeWithUseTransition =
+  typeof useTransition === 'function' ? describe : describe.skip
 
-  const commited: { pending: boolean; delayed: number }[] = []
-
-  const Counter = () => {
-    const setCount = useSetAtom(countAtom)
-    const delayed = useAtomValue(delayedAtom)
-    const [pending, startTransition] = useTransition()
-    useEffect(() => {
-      commited.push({ pending, delayed })
+describeWithUseTransition('useTransition', () => {
+  it('no extra commit with useTransition (#1125)', async () => {
+    const countAtom = atom(0)
+    const delayedAtom = atom(async (get) => {
+      await new Promise((r) => setTimeout(r, 100))
+      return get(countAtom)
     })
-    return (
-      <>
-        <div>delayed: {delayed}</div>
-        <button onClick={() => startTransition(() => setCount((c) => c + 1))}>
-          button
-        </button>
-      </>
+
+    const commited: { pending: boolean; delayed: number }[] = []
+
+    const Counter = () => {
+      const setCount = useSetAtom(countAtom)
+      const delayed = useAtomValue(delayedAtom)
+      const [pending, startTransition] = useTransition()
+      useEffect(() => {
+        commited.push({ pending, delayed })
+      })
+      return (
+        <>
+          <div>delayed: {delayed}</div>
+          <button onClick={() => startTransition(() => setCount((c) => c + 1))}>
+            button
+          </button>
+        </>
+      )
+    }
+
+    const { getByText, findByText } = render(
+      <Provider>
+        <Suspense fallback="loading">
+          <Counter />
+        </Suspense>
+      </Provider>
     )
-  }
 
-  const { getByText, findByText } = render(
-    <Provider>
-      <Suspense fallback="loading">
-        <Counter />
-      </Suspense>
-    </Provider>
-  )
+    await findByText('delayed: 0')
 
-  await findByText('delayed: 0')
+    fireEvent.click(getByText('button'))
+    await findByText('delayed: 1')
 
-  fireEvent.click(getByText('button'))
-  await findByText('delayed: 1')
-
-  expect(commited).toEqual([
-    { pending: false, delayed: 0 },
-    { pending: true, delayed: 0 },
-    { pending: false, delayed: 1 },
-  ])
+    expect(commited).toEqual([
+      { pending: false, delayed: 0 },
+      { pending: true, delayed: 0 },
+      { pending: false, delayed: 1 },
+    ])
+  })
 })
