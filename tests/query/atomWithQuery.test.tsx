52f183fd20099ad396b693cd15b907179a407edc
daishi <daishi@axlight.com>
2022-06-19 23:10:08 +0900

    skip loading check

--- tests/query/atomWithQuery.test.tsx
+++ tests/query/atomWithQuery.test.tsx
@@ -607,11 +607,7 @@ describe('error handling', () => {
     await findByText('loading')
     await findByText('errored')
 
-    await act(async () => {
-      await new Promise((r) => setTimeout(r, 100))
-    })
     fireEvent.click(getByText('retry'))
-    await findByText('loading')
     await findByText('count: 1')
 
     await act(async () => {
@@ -621,11 +617,7 @@ describe('error handling', () => {
     await findByText('loading')
     await findByText('errored')
 
-    await act(async () => {
-      await new Promise((r) => setTimeout(r, 100))
-    })
     fireEvent.click(getByText('retry'))
-    await findByText('loading')
     await findByText('count: 3')
   })
 })
