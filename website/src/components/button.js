16301cb3b00de09caab26c0004742a135de459b0
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2022-06-22 05:21:19 +0000

    fix(website): add hover styles for dark mode

--- website/src/components/button.js
+++ website/src/components/button.js
@@ -21,7 +21,7 @@ export const Button = ({
       ? 'space-x-4 rounded-md px-6 py-3 text-base shadow-md sm:rounded-lg'
       : 'space-x-2 rounded px-3 py-1.5 text-xs shadow-sm sm:rounded-md',
     !dark
-      ? 'border-gray-200 bg-gray-100 text-black hover:bg-blue-100 dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300'
+      ? 'border-gray-200 bg-gray-100 text-black hover:bg-blue-100 dark:border-gray-800 dark:bg-gray-900 dark:hover:!border-teal-800 dark:hover:bg-teal-950 dark:text-gray-300'
       : 'border-gray-800 bg-gray-900 text-gray-300',
     !disabled ? 'cursor-pointer' : 'cursor-not-allowed',
     className
--- website/src/components/toc.js
+++ website/src/components/toc.js
@@ -19,7 +19,7 @@ export const TOC = ({ section = '' }) => {
       {sectionLinks.map((sectionLink) => (
         <Link
           to={`/docs/${sectionLink.slug}`}
-          className="inline-flex aspect-video items-center justify-center rounded-md border border-gray-200 bg-gray-100 p-2 text-center leading-snug !text-black !no-underline hover:bg-blue-100 dark:border-gray-800 dark:bg-gray-900 dark:!text-gray-300 sm:rounded-lg">
+          className="inline-flex aspect-video items-center justify-center rounded-md border border-gray-200 bg-gray-100 p-2 text-center leading-snug !text-black !no-underline hover:bg-blue-100 dark:border-gray-800 dark:bg-gray-900 dark:!text-gray-300 dark:hover:!border-teal-800 dark:hover:bg-teal-950 sm:rounded-lg">
           {sectionLink.meta.title}
         </Link>
       ))}
