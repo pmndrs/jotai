27bc3ac12089db6af485d6d1a8ddca82406ccce6
M. Bagher Abiat <zorofight94@gmail.com>
2022-06-25 16:43:42 +0430

    fix: backward compatibility

--- src/devtools/useGotoAtomsSnapshot.ts
+++ src/devtools/useGotoAtomsSnapshot.ts
@@ -3,6 +3,15 @@ import { SECRET_INTERNAL_getScopeContext as getScopeContext } from 'jotai'
 import type { Atom, Scope } from '../core/atom'
 import { DEV_SUBSCRIBE_STATE, RESTORE_ATOMS } from '../core/store'
 
+type AnyAtomValue = unknown
+type AnyAtom = Atom<AnyAtomValue>
+type AtomsValues = Map<AnyAtom, AnyAtomValue> // immutable
+type AtomsDependents = Map<AnyAtom, Set<AnyAtom>> // immutable
+type AtomsSnapshot = Readonly<{
+  values: AtomsValues
+  dependents: AtomsDependents
+}>
+
 export function useGotoAtomsSnapshot(scope?: Scope) {
   const ScopeContext = getScopeContext(scope)
   const scopeContainer = useContext(ScopeContext)
@@ -13,9 +22,17 @@ export function useGotoAtomsSnapshot(scope?: Scope) {
   }
 
   return useCallback(
-    (values: Iterable<readonly [Atom<unknown>, unknown]>) => {
-      store[RESTORE_ATOMS](values)
+    (values: Iterable<readonly [Atom<unknown>, unknown]> | AtomsSnapshot) => {
+      if (isIterable(values)) {
+        store[RESTORE_ATOMS](values)
+        return
+      }
+      store[RESTORE_ATOMS](values.values)
     },
     [store]
   )
+}
+
+function isIterable(item: any): item is Iterable<any> {
+  return typeof item[Symbol.iterator] === 'function'
 }
