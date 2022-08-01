f561677db8f72383b61500f21f460c763331536f
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2022-08-01 05:17:38 +0000

    feat(website): upgrade to Gatsby Head and add preconnect directives

--- website/src/pages/docs/{Mdx.slug}.js
+++ website/src/pages/docs/{Mdx.slug}.js
@@ -1,25 +1,29 @@
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
-import { Head, Jotai, Layout } from '../../components';
+import { Jotai, Layout, Meta } from '../../components';
 
 export default function DocsPage({ data }) {
-  const { slug, frontmatter, body } = data.mdx;
+  const { frontmatter, body } = data.mdx;
+  const { title } = frontmatter;
+
+  return (
+    <Layout showDocs>
+      <div className="mb-4 lg:hidden">
+        <Jotai isDocsPage small />
+      </div>
+      <h1>{title}</h1>
+      <MDXRenderer>{body}</MDXRenderer>
+    </Layout>
+  );
+}
+
+export const Head = ({ data }) => {
+  const { slug, frontmatter } = data.mdx;
   const { title, description } = frontmatter;
   const uri = `docs/${slug}`;
 
-  return (
-    <>
-      <Head title={title} description={description} uri={uri} />
-      <Layout showDocs>
-        <div className="mb-4 lg:hidden">
-          <Jotai isDocsPage small />
-        </div>
-        <h1>{title}</h1>
-        <MDXRenderer>{body}</MDXRenderer>
-      </Layout>
-    </>
-  );
-}
+  return <Meta title={title} description={description} uri={uri} />;
+};
 
 export const pageQuery = graphql`
   query PageQuery($slug: String) {
