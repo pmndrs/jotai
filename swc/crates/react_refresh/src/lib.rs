c630a93ce24e83b786647e8f6aad3fd58c79ff14
Mathis Møller <thisen-dk@hotmail.com>
2022-08-01 23:15:02 +0200

    Fix Clippy

--- swc/crates/react_refresh/src/lib.rs
+++ swc/crates/react_refresh/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
+
 use swc_plugin::{ast::*, metadata::TransformPluginProgramMetadata, plugin_transform};
 
 pub struct TransformVisitor;
@@ -8,26 +10,7 @@ impl VisitMut for TransformVisitor {
     // https://rustdoc.swc.rs/swc_ecma_visit/trait.VisitMut.html
 }
 
-/// An example plugin function with macro support.
-/// `plugin_transform` macro interop pointers into deserialized structs, as well
-/// as returning ptr back to host.
-///
-/// It is possible to opt out from macro by writing transform fn manually via
-/// `__plugin_process_impl(
-///     ast_ptr: *const u8,
-///     ast_ptr_len: i32,
-///     config_str_ptr: *const u8,
-///     config_str_ptr_len: i32,
-///     context_str_ptr: *const u8,
-///     context_str_ptr_len: i32) ->
-///     i32 /*  0 for success, fail otherwise.
-///             Note this is only for internal pointer interop result,
-///             not actual transform result */
-///
-/// if plugin need to handle low-level ptr directly. However, there are
-/// important steps manually need to be performed like sending transformed
-/// results back to host. Refer swc_plugin_macro how does it work internally.
 #[plugin_transform]
-pub fn process_transform(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
-    program.fold_with(&mut as_folder(TransformVisitor))
+pub fn process_transform(_program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
+    unimplemented!()
 }
