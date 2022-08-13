228e6e76c17003a4059197a6be8c6ef57b99c7cd
daishi <daishi@axlight.com>
2022-08-13 09:58:06 +0900

    add two more tests

--- tests/utils/abortableAtom.test.tsx
+++ tests/utils/abortableAtom.test.tsx
@@ -1,5 +1,5 @@
-import { Suspense } from 'react'
-import { fireEvent, render } from '@testing-library/react'
+import { Suspense, useState } from 'react'
+import { fireEvent, render, waitFor } from '@testing-library/react'
 import { atom, useAtomValue, useSetAtom } from 'jotai'
 import { abortableAtom } from 'jotai/utils'
 import { getTestProvider, itSkipIfVersionedWrite } from '../testUtils'
@@ -13,7 +13,6 @@ itSkipIfVersionedWrite('can abort with signal.aborted', async () => {
     const count = get(countAtom)
     await new Promise((r) => setTimeout(r, 100))
     if (signal.aborted) {
-      console.log('-------', signal, { count })
       ++abortedCount
     }
     return count
@@ -56,12 +55,104 @@ itSkipIfVersionedWrite('can abort with signal.aborted', async () => {
   expect(abortedCount).toBe(1)
 })
 
-it('can abort with event listener', async () => {
-  // TODO
+itSkipIfVersionedWrite('can abort with event listener', async () => {
+  const countAtom = atom(0)
+  let abortedCount = 0
+  const derivedAtom = abortableAtom(async (get, { signal }) => {
+    const count = get(countAtom)
+    const callback = () => {
+      ++abortedCount
+    }
+    signal.addEventListener('abort', callback)
+    await new Promise((r) => setTimeout(r, 100))
+    signal.removeEventListener('abort', callback)
+    return count
+  })
+
+  const Component = () => {
+    const count = useAtomValue(derivedAtom)
+    return <div>count: {count}</div>
+  }
+
+  const Controls = () => {
+    const setCount = useSetAtom(countAtom)
+    return (
+      <>
+        <button onClick={() => setCount((c) => c + 1)}>button</button>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <Provider>
+      <Suspense fallback="loading">
+        <Component />
+        <Controls />
+      </Suspense>
+    </Provider>
+  )
+
+  await findByText('loading')
+  await findByText('count: 0')
+  expect(abortedCount).toBe(0)
+
+  fireEvent.click(getByText('button'))
+  fireEvent.click(getByText('button'))
+  await findByText('count: 2')
+  expect(abortedCount).toBe(1)
+
+  fireEvent.click(getByText('button'))
+  await findByText('count: 3')
+  expect(abortedCount).toBe(1)
 })
 
-it('can abort on unmount', async () => {
-  // TODO
+itSkipIfVersionedWrite('can abort on unmount', async () => {
+  const countAtom = atom(0)
+  let abortedCount = 0
+  const derivedAtom = abortableAtom(async (get, { signal }) => {
+    const count = get(countAtom)
+    await new Promise((r) => setTimeout(r, 100))
+    if (signal.aborted) {
+      ++abortedCount
+    }
+    return count
+  })
+
+  const Component = () => {
+    const count = useAtomValue(derivedAtom)
+    return <div>count: {count}</div>
+  }
+
+  const Parent = () => {
+    const setCount = useSetAtom(countAtom)
+    const [show, setShow] = useState(true)
+    return (
+      <>
+        {show ? <Component /> : 'hidden'}
+        <button onClick={() => setCount((c) => c + 1)}>button</button>
+        <button onClick={() => setShow((x) => !x)}>toggle</button>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <Provider>
+      <Suspense fallback="loading">
+        <Parent />
+      </Suspense>
+    </Provider>
+  )
+
+  await findByText('loading')
+  await findByText('count: 0')
+  expect(abortedCount).toBe(0)
+
+  fireEvent.click(getByText('button'))
+  fireEvent.click(getByText('toggle'))
+  await findByText('hidden')
+  await waitFor(() => {
+    expect(abortedCount).toBe(1)
+  })
 })
 
 it('throws aborted error (like fetch)', async () => {
