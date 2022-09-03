ccc756287a049acebb227fcfe9e059b3a3f82fa1
daishi <daishi@axlight.com>
2022-09-03 10:44:27 +0900

    Revert "remove if condition"
    This reverts commit fd0e951e365285cdc109240ae355513379d42011.

--- src/core/useAtomValue.ts
+++ src/core/useAtomValue.ts
@@ -65,7 +65,9 @@ export function useAtomValue<Value>(
 
   useEffect(() => {
     const { v: versionFromProvider } = scopeContainer
-    store[COMMIT_ATOM](atom, versionFromProvider)
+    if (versionFromProvider) {
+      store[COMMIT_ATOM](atom, versionFromProvider)
+    }
     // Call `rerenderIfChanged` whenever this atom is invalidated. Note
     // that derived atoms may not be recomputed yet.
     const unsubscribe = store[SUBSCRIBE_ATOM](
