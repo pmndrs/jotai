3ebcbd1b533b8b30902ea00bb196d7c99cb6b800
daishi <daishi@axlight.com>
2022-09-04 22:08:25 +0900

    add test for atomWithMutation

--- tests/urql/atomWithMutation.test.tsx
+++ tests/urql/atomWithMutation.test.tsx
@@ -1,30 +1,43 @@
 import { StrictMode, Suspense } from 'react'
-import { fireEvent, render } from '@testing-library/react'
+import { fireEvent, render, waitFor } from '@testing-library/react'
 import type { Client } from '@urql/core'
 import { delay, fromValue, pipe, take, toPromise } from 'wonka'
 import { atom, useAtom } from 'jotai'
 import { atomWithMutation } from 'jotai/urql'
-import { getTestProvider } from '../testUtils'
+import { getTestProvider, itSkipIfVersionedWrite } from '../testUtils'
 
 const withPromise = (source$: any) => {
   source$.toPromise = () => pipe(source$, take(1), toPromise)
   return source$
 }
-const clientMock = {
-  mutation: () =>
-    withPromise(pipe(fromValue({ data: { count: 1 } }), delay(100))),
-} as unknown as Client
+
+const generateClient = (error?: () => boolean) =>
+  ({
+    mutation: () => {
+      const source$ = withPromise(
+        pipe(
+          fromValue(
+            error?.()
+              ? { error: new Error('fetch error') }
+              : { data: { count: 1 } }
+          ),
+          delay(100)
+        )
+      )
+      return source$
+    },
+  } as unknown as Client)
 
 const Provider = getTestProvider()
 
 it('mutation basic test', async () => {
   const countAtom = atomWithMutation<{ count: number }, Record<string, never>>(
     () => 'mutation Test { count }',
-    () => clientMock
+    () => generateClient()
   )
-  const mutateAtom = atom(null, (_get, set) => {
+  const mutateAtom = atom(null, (_get, set) =>
     set(countAtom, { variables: {} })
-  })
+  )
 
   const Counter = () => {
     const [{ data }] = useAtom(countAtom)
@@ -56,4 +69,60 @@ it('mutation basic test', async () => {
   await new Promise((r) => setTimeout(r, 100))
   fireEvent.click(getByText('mutate'))
   await findByText('count: 1')
+})
+
+describe('error handling', () => {
+  itSkipIfVersionedWrite('mutation error test', async () => {
+    const countAtom = atomWithMutation<
+      { count: number },
+      Record<string, never>
+    >(
+      () => 'mutation Test { count }',
+      () => generateClient(() => true)
+    )
+    const mutateAtom = atom(null, (_get, set) =>
+      set(countAtom, { variables: {} })
+    )
+
+    const Counter = () => {
+      const [{ data }] = useAtom(countAtom)
+      return (
+        <>
+          <div>count: {data?.count}</div>
+        </>
+      )
+    }
+
+    let errored = false
+    const Controls = () => {
+      const [, mutate] = useAtom(mutateAtom)
+      const handleClick = async () => {
+        try {
+          await mutate()
+        } catch {
+          errored = true
+        }
+      }
+      return <button onClick={handleClick}>mutate</button>
+    }
+
+    const { getByText, findByText } = render(
+      <StrictMode>
+        <Provider>
+          <Suspense fallback="loading">
+            <Counter />
+          </Suspense>
+          <Controls />
+        </Provider>
+      </StrictMode>
+    )
+
+    await findByText('loading')
+
+    await new Promise((r) => setTimeout(r, 100))
+    fireEvent.click(getByText('mutate'))
+    await waitFor(() => {
+      expect(errored).toBe(true)
+    })
+  })
 })
