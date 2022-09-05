0e0fc9783331da0be42f44a46c1d0f832f16cd76
daishi <daishi@axlight.com>
2022-09-05 17:40:55 +0900

    wip: atomWithSubscription test

--- tests/urql/atomWithSubscription.test.tsx
+++ tests/urql/atomWithSubscription.test.tsx
@@ -1,17 +1,34 @@
-import { StrictMode, Suspense } from 'react'
+import { Component, StrictMode, Suspense, useContext } from 'react'
+import type { ReactNode } from 'react'
 import { fireEvent, render } from '@testing-library/react'
 import type { Client, TypedDocumentNode } from '@urql/core'
 import { interval, map, pipe } from 'wonka'
-import { atom, useAtom } from 'jotai'
+import {
+  atom,
+  SECRET_INTERNAL_getScopeContext as getScopeContext,
+  useAtom,
+  useSetAtom,
+} from 'jotai'
 import { atomWithSubscription } from 'jotai/urql'
 import { StrictModeUnlessVersionedWrite, getTestProvider } from '../testUtils'
 
-const generateClient = (id = 'default') =>
+// This is only used to pass tests with unstable_enableVersionedWrite
+const useRetryFromError = (scope?: symbol | string | number) => {
+  const ScopeContext = getScopeContext(scope)
+  const { r: retryFromError } = useContext(ScopeContext)
+  return retryFromError || ((fn) => fn())
+}
+
+const generateClient = (id = 'default', error?: () => boolean) =>
   ({
     subscription: () =>
       pipe(
         interval(100),
-        map((i: number) => ({ data: { id, count: i } }))
+        map((i: number) =>
+          error?.()
+            ? { error: new Error('fetch error') }
+            : { data: { id, count: i } }
+        )
       ),
   } as unknown as Client)
 
@@ -243,4 +260,144 @@ it('null client suspense', async () => {
   await findByText('default count: 0')
   await findByText('default count: 1')
   await findByText('default count: 2')
+})
+
+describe('error handling', () => {
+  class ErrorBoundary extends Component<
+    { message?: string; retry?: () => void; children: ReactNode },
+    { hasError: boolean }
+  > {
+    constructor(props: { message?: string; children: ReactNode }) {
+      super(props)
+      this.state = { hasError: false }
+    }
+    static getDerivedStateFromError() {
+      return { hasError: true }
+    }
+    render() {
+      return this.state.hasError ? (
+        <div>
+          {this.props.message || 'errored'}
+          {this.props.retry && (
+            <button
+              onClick={() => {
+                this.props.retry?.()
+                this.setState({ hasError: false })
+              }}>
+              retry
+            </button>
+          )}
+        </div>
+      ) : (
+        this.props.children
+      )
+    }
+  }
+
+  it('can catch error in error boundary', async () => {
+    const countAtom = atomWithSubscription(
+      () => ({
+        query: 'subscription Test { count }' as unknown as TypedDocumentNode<{
+          count: number
+        }>,
+        variables: {},
+      }),
+      () => clientMock
+    )
+
+    const Counter = () => {
+      const [{ data }] = useAtom(countAtom)
+      return <div>count: {data.count}</div>
+    }
+
+    const { findByText } = render(
+      <Provider>
+        <ErrorBoundary>
+          <Suspense fallback="loading">
+            <Counter />
+          </Suspense>
+        </ErrorBoundary>
+      </Provider>
+    )
+
+    await findByText('loading')
+    await findByText('errored')
+  })
+
+  it('can recover from error', async () => {
+    let willThrowError = true
+    const countAtom = atomWithSubscription(
+      () => ({
+        query: 'subscription Test { count }' as unknown as TypedDocumentNode<{
+          count: number
+        }>,
+        variables: {},
+      }),
+      () => generateClient(undefined, () => willThrowError)
+    )
+
+    const Counter = () => {
+      const [
+        {
+          data: { count },
+        },
+        dispatch,
+      ] = useAtom(countAtom)
+      const refetch = () => dispatch({ type: 'refetch' })
+      return (
+        <>
+          <div>count: {count}</div>
+          <button onClick={refetch}>refetch</button>
+        </>
+      )
+    }
+
+    const App = () => {
+      const dispatch = useSetAtom(countAtom)
+      const retryFromError = useRetryFromError()
+      const retry = () => {
+        retryFromError(() => {
+          dispatch({ type: 'refetch' })
+        })
+      }
+      return (
+        <ErrorBoundary retry={retry}>
+          <Suspense fallback="loading">
+            <Counter />
+          </Suspense>
+        </ErrorBoundary>
+      )
+    }
+
+    const { findByText, getByText } = render(
+      <Provider>
+        <App />
+      </Provider>
+    )
+
+    await findByText('loading')
+    await findByText('errored')
+
+    await new Promise((r) => setTimeout(r, 100))
+    willThrowError = false
+    fireEvent.click(getByText('retry'))
+    await findByText('loading')
+    await findByText('count: 0')
+    await findByText('count: 1')
+    await findByText('count: 2')
+
+    await new Promise((r) => setTimeout(r, 100))
+    willThrowError = true
+    fireEvent.click(getByText('refetch'))
+    await findByText('loading')
+    await findByText('errored')
+
+    await new Promise((r) => setTimeout(r, 100))
+    willThrowError = false
+    fireEvent.click(getByText('retry'))
+    await findByText('loading')
+    await findByText('count: 0')
+    await findByText('count: 1')
+    await findByText('count: 2')
+  })
 })
