a4d0f30c645765e53c930945ef2fabe7db863fe6
Daishi Kato <dai-shi@users.noreply.github.com>
2022-09-06 20:20:27 +0900

    fix(core,utils): loadable with StrictMode (#1397)
    * add StrictMode in tests
    
    * fix test
    
    * a very hacky workaround
    
    * refactor: avoid async function

--- src/core/store.ts
+++ src/core/store.ts
@@ -658,9 +658,18 @@ export const createStore = (
       }
       if ('p' in aState) {
         if (options?.unstable_promise) {
-          return aState.p.then(() =>
-            writeGetter(a as unknown as Atom<Promise<V>>, options)
-          )
+          return aState.p.then(() => {
+            const s = getAtomState(version, a)
+            if (s && 'p' in s && s.p === aState.p) {
+              // FIXME this is very very hacky
+              // there should be better solutions
+              // with suspensePromise.ts cancel handling
+              return new Promise((resolve) => setTimeout(resolve)).then(() =>
+                writeGetter(a as unknown as Atom<Promise<V>>, options)
+              )
+            }
+            return writeGetter(a as unknown as Atom<Promise<V>>, options)
+          })
         }
         if (__DEV__) {
           console.info(
