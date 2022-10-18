9bc20069f75966eb5ef064d172d900a5a3bc2ba6
Mathis Møller <thisen-dk@hotmail.com>
2022-10-18 11:20:43 +0200

    feat(babel): Support custom atom names (#1480)
    * Support custom atom names
    
    * Changes afer review

--- tests/babel/preset.test.ts
+++ tests/babel/preset.test.ts
@@ -3,12 +3,16 @@ import { transformSync } from '@babel/core'
 
 const preset = path.join(__dirname, '../../src/babel/preset')
 
-const transform = (code: string, filename?: string) =>
+const transform = (
+  code: string,
+  filename?: string,
+  customAtomNames?: string[]
+) =>
   transformSync(code, {
     babelrc: false,
     configFile: false,
     filename,
-    presets: [[preset]],
+    presets: [[preset, { customAtomNames }]],
   })?.code
 
 it('Should add a debugLabel and cache to an atom', () => {
@@ -148,4 +152,28 @@ it('Should fail if no filename is available', () => {
   expect(() => transform(`const countAtom = atom(0);`)).toThrowError(
     'Filename must be available'
   )
+})
+
+it('Should handle custom atom names', () => {
+  expect(
+    transform(`const mySpecialThing = atom(0);`, '/src/atoms.ts', [
+      'mySpecialThing',
+    ])
+  ).toMatchInlineSnapshot(`
+    "globalThis.jotaiAtomCache = globalThis.jotaiAtomCache || {
+      cache: new Map(),
+
+      get(name, inst) {
+        if (this.cache.has(name)) {
+          return this.cache.get(name);
+        }
+
+        this.cache.set(name, inst);
+        return inst;
+      }
+
+    };
+    const mySpecialThing = globalThis.jotaiAtomCache.get("/src/atoms.ts/mySpecialThing", atom(0));
+    mySpecialThing.debugLabel = "mySpecialThing";"
+  `)
 })
