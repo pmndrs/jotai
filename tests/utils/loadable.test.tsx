7eeabb63f1471e551d456457de87f86306b01416
daishi <daishi@axlight.com>
2022-11-08 17:06:07 +0900

    refactor loadable test to be consistent with other tests

--- tests/utils/loadable.test.tsx
+++ tests/utils/loadable.test.tsx
@@ -7,9 +7,9 @@ import { getTestProvider } from '../testUtils'
 const Provider = getTestProvider()
 
 it('loadable turns suspense into values', async () => {
-  let resolveAsync!: (x: number) => void
+  let resolve: (x: number) => void = () => {}
   const asyncAtom = atom(() => {
-    return new Promise<number>((resolve) => (resolveAsync = resolve))
+    return new Promise<number>((r) => (resolve = r))
   })
 
   const { findByText } = render(
@@ -21,14 +21,14 @@ it('loadable turns suspense into values', async () => {
   )
 
   await findByText('Loading...')
-  resolveAsync(5)
+  resolve(5)
   await findByText('Data: 5')
 })
 
 it('loadable turns errors into values', async () => {
-  let rejectAsync!: (error: unknown) => void
+  let reject: (error: unknown) => void = () => {}
   const asyncAtom = atom(() => {
-    return new Promise<number>((_resolve, reject) => (rejectAsync = reject))
+    return new Promise<number>((_res, rej) => (reject = rej))
   })
 
   const { findByText } = render(
@@ -40,14 +40,14 @@ it('loadable turns errors into values', async () => {
   )
 
   await findByText('Loading...')
-  rejectAsync(new Error('An error occurred'))
+  reject(new Error('An error occurred'))
   await findByText('Error: An error occurred')
 })
 
 it('loadable turns primitive throws into values', async () => {
-  let rejectAsync!: (error: unknown) => void
+  let reject: (error: unknown) => void = () => {}
   const asyncAtom = atom(() => {
-    return new Promise<number>((_resolve, reject) => (rejectAsync = reject))
+    return new Promise<number>((_res, rej) => (reject = rej))
   })
 
   const { findByText } = render(
@@ -59,16 +59,16 @@ it('loadable turns primitive throws into values', async () => {
   )
 
   await findByText('Loading...')
-  rejectAsync('An error occurred')
+  reject('An error occurred')
   await findByText('An error occurred')
 })
 
 it('loadable goes back to loading after re-fetch', async () => {
-  let resolveAsync!: (x: number) => void
+  let resolve: (x: number) => void = () => {}
   const refreshAtom = atom(0)
   const asyncAtom = atom((get) => {
     get(refreshAtom)
-    return new Promise<number>((resolve) => (resolveAsync = resolve))
+    return new Promise<number>((r) => (resolve = r))
   })
 
   const Refresh = () => {
@@ -92,23 +92,23 @@ it('loadable goes back to loading after re-fetch', async () => {
   )
 
   getByText('Loading...')
-  resolveAsync(5)
+  resolve(5)
   await findByText('Data: 5')
   fireEvent.click(getByText('refresh'))
   await findByText('Loading...')
-  resolveAsync(6)
+  resolve(6)
   await findByText('Data: 6')
 })
 
 it('loadable can recover from error', async () => {
-  let resolveAsync!: (x: number) => void
-  let rejectAsync!: (error: unknown) => void
+  let resolve: (x: number) => void = () => {}
+  let reject: (error: unknown) => void = () => {}
   const refreshAtom = atom(0)
   const asyncAtom = atom((get) => {
     get(refreshAtom)
-    return new Promise<number>((resolve, reject) => {
-      resolveAsync = resolve
-      rejectAsync = reject
+    return new Promise<number>((res, rej) => {
+      resolve = res
+      reject = rej
     })
   })
 
@@ -133,11 +133,11 @@ it('loadable can recover from error', async () => {
   )
 
   getByText('Loading...')
-  rejectAsync(new Error('An error occurred'))
+  reject(new Error('An error occurred'))
   await findByText('Error: An error occurred')
   fireEvent.click(getByText('refresh'))
   await findByText('Loading...')
-  resolveAsync(6)
+  resolve(6)
   await findByText('Data: 6')
 })
 
@@ -204,11 +204,11 @@ it('loadable can use resolved promises syncronously', async () => {
 })
 
 it('loadable of a derived async atom does not trigger infinite loop (#1114)', async () => {
-  let resolveAsync!: (x: number) => void
+  let resolve: (x: number) => void = () => {}
   const baseAtom = atom(0)
   const asyncAtom = atom((get) => {
     get(baseAtom)
-    return new Promise<number>((resolve) => (resolveAsync = resolve))
+    return new Promise<number>((r) => (resolve = r))
   })
 
   const Trigger = () => {
@@ -231,7 +231,7 @@ it('loadable of a derived async atom does not trigger infinite loop (#1114)', as
 
   getByText('Loading...')
   fireEvent.click(getByText('trigger'))
-  resolveAsync(5)
+  resolve(5)
   await findByText('Data: 5')
 })
 
@@ -257,12 +257,8 @@ it('loadable of a derived async atom with error does not trigger infinite loop (
 })
 
 it('does not repeatedly attempt to get the value of an unresolved promise atom wrapped in a loadable', async () => {
-  let resolveAsync: (value: number) => void
-  const baseAtom = atom(
-    new Promise<number>((resolve) => {
-      resolveAsync = resolve
-    })
-  )
+  let resolve: (value: number) => void = () => {}
+  const baseAtom = atom(new Promise<number>((r) => (resolve = r)))
 
   let callsToGetBaseAtom = 0
   const derivedAtom = atom((get) => {
@@ -284,7 +280,7 @@ it('does not repeatedly attempt to get the value of an unresolved promise atom w
   expect(callsToGetBaseAtom).toBeLessThanOrEqual(3)
 
   callsToGetBaseAtom = 0
-  resolveAsync!(5)
+  resolve(5)
   await findByText('Data: 5')
   expect(callsToGetBaseAtom).toBe(1)
 })
