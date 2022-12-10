591b2d84dc2e349b5cc5dff981eb6611783f5d85
daishi <daishi@axlight.com>
2022-12-10 21:44:32 +0900

    deprecate jotai/redux

--- src/redux.ts
+++ src/redux.ts
@@ -1,1 +1,11 @@
-export { atomWithStore } from 'jotai-redux'
+import * as JotaiRedux from 'jotai-redux'
+
+/**
+ * @deprecated use `jotai-redux` instead
+ */
+export const atomWithStore: typeof JotaiRedux.atomWithStore = (
+  ...args: any[]
+) => {
+  console.warn('[DEPRECATED] use `jotai-redux` instead.')
+  return (JotaiRedux.atomWithStore as any)(...args)
+}
