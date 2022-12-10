c3ac0ad9e9ae679984f57cdef9017c11ccbcc8c0
daishi <daishi@axlight.com>
2022-12-10 21:39:55 +0900

    deprecate jotai/xstat

--- src/xstate.ts
+++ src/xstate.ts
@@ -1,1 +1,10 @@
-export { atomWithMachine, RESTART } from 'jotai-xstate'
+export { RESTART } from 'jotai-xstate'
+
+import * as JotaiXstate from 'jotai-xstate'
+
+export const atomWithMachine: typeof JotaiXstate.atomWithMachine = (
+  ...args: any[]
+) => {
+  console.warn('[DEPRECATED] use `jotai-xstate` instead.')
+  return (JotaiXstate.atomWithMachine as any)(...args)
+}
