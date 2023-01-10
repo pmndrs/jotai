2e516d62238bcdccfb82ed364d12e7ebb97b3ca1
Daishi Kato <dai-shi@users.noreply.github.com>
2023-01-10 20:21:40 +0900

    feat(devtools): deprecate jotai/devtools to prepare for v2 (#1677)
    * deprecate jotai/devtools to prepare for v2
    
    * empty commit
    
    * empty commit
    
    * empty commit
    
    * empty commit
    
    * empty commit
    
    * empty commit
    
    * empty commit
    
    * empty commit

--- src/devtools.ts
+++ src/devtools.ts
@@ -1,7 +1,51 @@
-export {
-  useAtomsDebugValue,
-  useAtomDevtools,
-  useAtomsSnapshot,
-  useGotoAtomsSnapshot,
-  useAtomsDevtools,
-} from 'jotai-devtools'
+import * as JotaiDevtools from 'jotai-devtools'
+
+/**
+ * @deprecated use `jotai-devtools` instead.
+ */
+export const useAtomsDebugValue: typeof JotaiDevtools.useAtomsDebugValue = (
+  ...args: any[]
+) => {
+  console.warn('[DEPRECATED]: use `jotai-devtools` instead.')
+  return (JotaiDevtools.useAtomsDebugValue as any)(...args)
+}
+
+/**
+ * @deprecated use `jotai-devtools` instead.
+ */
+export const useAtomDevtools: typeof JotaiDevtools.useAtomDevtools = (
+  ...args: any[]
+) => {
+  console.warn('[DEPRECATED]: use `jotai-devtools` instead.')
+  return (JotaiDevtools.useAtomDevtools as any)(...args)
+}
+
+/**
+ * @deprecated use `jotai-devtools` instead.
+ */
+export const useAtomsSnapshot: typeof JotaiDevtools.useAtomsSnapshot = (
+  ...args: any[]
+) => {
+  console.warn('[DEPRECATED]: use `jotai-devtools` instead.')
+  return (JotaiDevtools.useAtomsSnapshot as any)(...args)
+}
+
+/**
+ * @deprecated use `jotai-devtools` instead.
+ */
+export const useGotoAtomsSnapshot: typeof JotaiDevtools.useGotoAtomsSnapshot = (
+  ...args: any[]
+) => {
+  console.warn('[DEPRECATED]: use `jotai-devtools` instead.')
+  return (JotaiDevtools.useGotoAtomsSnapshot as any)(...args)
+}
+
+/**
+ * @deprecated use `jotai-devtools` instead.
+ */
+export const useAtomsDevtools: typeof JotaiDevtools.useAtomsDevtools = (
+  ...args: any[]
+) => {
+  console.warn('[DEPRECATED]: use `jotai-devtools` instead.')
+  return (JotaiDevtools.useAtomsDevtools as any)(...args)
+}
