241001cdc391498f727b78ac831615f45daf0dfc
daishi <daishi@axlight.com>
2023-01-15 22:01:37 +0900

    fix(react): delete v2 API devtools

--- src/react/devtools/types.ts
+++
@@ -1,9 +0,0 @@
-import type {} from '@redux-devtools/extension'
-
-// FIXME https://github.com/reduxjs/redux-devtools/issues/1097
-// This is an INTERNAL type alias.
-export type Message = {
-  type: string
-  payload?: any
-  state?: any
-}
