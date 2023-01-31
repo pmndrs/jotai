0d7e5b847cb41e6dc9b275b33e6b1279a0a19746
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2023-01-31 04:06:16 +0000

    fix(website): revise scroll lock implementation

--- website/src/components/inline-code.js
+++ website/src/components/inline-code.js
@@ -6,8 +6,8 @@ export const InlineCode = ({ dark = false, children }) => {
       className={cx(
         'relative -top-px rounded px-1 py-0.5 text-sm lg:text-base',
         !dark
-          ? 'bg-gray-100 text-black dark:bg-gray-900 dark:text-gray-400'
-          : 'bg-gray-200 text-black dark:bg-gray-900 dark:text-gray-400',
+          ? 'bg-gray-100 text-black dark:bg-gray-800 dark:text-gray-300'
+          : 'bg-gray-200 text-black dark:bg-gray-700 dark:text-gray-200',
       )}
     >
       {children}
--- website/src/components/modal.js
+++ website/src/components/modal.js
@@ -1,15 +1,18 @@
 import { Dialog } from '@headlessui/react';
+import { RemoveScroll } from 'react-remove-scroll';
 
-export const Modal = ({ isOpen, onClose, children, ...rest }) => (
-  <Dialog open={isOpen} onClose={onClose} {...rest}>
-    <div
-      className="z-100 fixed top-0 right-0 bottom-0 h-full w-8 bg-white dark:bg-gray-950"
-      aria-hidden="true"
-    />
-    <div className="fixed inset-0 z-[1000] flex justify-center bg-black/50 p-8 backdrop-blur sm:p-12 lg:p-32">
-      <div className="w-full max-w-3xl">
-        <Dialog.Panel className="z-[1001] min-w-full">{children}</Dialog.Panel>
+export const Modal = ({ isOpen, onClose, children, ...rest }) => {
+  return (
+    <Dialog open={isOpen} onClose={onClose} {...rest}>
+      <div
+        className="z-100 fixed top-0 right-0 bottom-0 h-full w-8 bg-white dark:bg-gray-950"
+        aria-hidden="true"
+      />
+      <div className="fixed inset-0 z-[1000] flex justify-center bg-black/50 p-8 backdrop-blur sm:p-12 lg:p-32">
+        <RemoveScroll className="w-full max-w-3xl">
+          <Dialog.Panel className="z-[1001] min-w-full">{children}</Dialog.Panel>
+        </RemoveScroll>
       </div>
-    </div>
-  </Dialog>
-);
+    </Dialog>
+  );
+};
