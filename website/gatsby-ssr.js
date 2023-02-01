92f80aa60fee82d4d0d97ca9efae5a1d4c5eb126
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2023-02-01 01:48:19 +0000

    fix(website): restore dark mode support

--- website/gatsby-ssr.js
+++ website/gatsby-ssr.js
@@ -1,5 +1,41 @@
 export { wrapRootElement } from './gatsby-shared';
 
-export const onRenderBody = ({ setHtmlAttributes }) => {
+export const onRenderBody = ({ setHtmlAttributes, setPreBodyComponents }) => {
   setHtmlAttributes({ lang: 'en' });
+  setPreBodyComponents([
+    <script
+      key="dark-mode"
+      dangerouslySetInnerHTML={{
+        __html: `(function() {
+  var storageKey = 'darkMode';
+  var classNameDark = 'dark';
+  var classNameLight = 'light';
+  function setClassOnDocumentBody(darkMode) {
+    document.body.classList.add(darkMode ? classNameDark : classNameLight);
+    document.body.classList.remove(darkMode ? classNameLight : classNameDark);
+  }
+  var preferDarkQuery = '(prefers-color-scheme: dark)';
+  var mql = window.matchMedia(preferDarkQuery);
+  var supportsColorSchemeQuery = mql.media === preferDarkQuery;
+  var localStorageTheme = null;
+  try {
+    localStorageTheme = localStorage.getItem(storageKey);
+  } catch (err) {}
+  var localStorageExists = localStorageTheme !== null;
+  if (localStorageExists) {
+    localStorageTheme = JSON.parse(localStorageTheme);
+  }
+  if (localStorageExists) {
+    setClassOnDocumentBody(localStorageTheme);
+  } else if (supportsColorSchemeQuery) {
+    setClassOnDocumentBody(mql.matches);
+    localStorage.setItem(storageKey, mql.matches);
+  } else {
+    var isDarkMode = document.body.classList.contains(classNameDark);
+    localStorage.setItem(storageKey, JSON.stringify(isDarkMode));
+  }
+})();`,
+      }}
+    />,
+  ]);
 };
--- website/src/components/toggle.js
+++ website/src/components/toggle.js
@@ -1,33 +1,45 @@
+import { useCallback, useEffect } from 'react';
 import cx from 'classnames';
-// import useDarkMode from 'use-dark-mode';
+import { useAtom } from 'jotai';
+import { darkModeAtom } from '../atoms';
 import { Icon } from '../components/icon';
 
 export const Toggle = () => {
-  // const darkMode = useDarkMode(false, {
-  //   classNameDark: 'dark',
-  //   classNameLight: 'light',
-  // });
+  const [darkMode, setDarkMode] = useAtom(darkModeAtom);
+
+  const toggleDarkMode = useCallback(() => {
+    setDarkMode(!darkMode);
+  }, [darkMode, setDarkMode]);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark');
+      document.body.classList.remove('light');
+    } else {
+      document.body.classList.add('light');
+      document.body.classList.remove('dark');
+    }
+  }, [darkMode]);
 
   return (
     <div className="absolute top-0 right-0 lg:fixed">
       <button
         type="button"
-        // onClick={darkMode.toggle}
-        title="Temporarily out of service"
-        className="relative m-4 inline-flex h-10 w-10 cursor-not-allowed select-none items-center justify-center rounded-full border border-gray-200 bg-gray-100 text-black shadow-md hover:border-blue-200 hover:bg-blue-100 dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300 dark:!shadow-none dark:hover:!border-teal-800 dark:hover:bg-teal-950"
+        onClick={toggleDarkMode}
+        className="relative m-4 inline-flex h-10 w-10 select-none items-center justify-center rounded-full border border-gray-200 bg-gray-100 text-black shadow-md hover:border-blue-200 hover:bg-blue-100 dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300 dark:!shadow-none dark:hover:!border-teal-800 dark:hover:bg-teal-950"
       >
         <div className="relative">
           <Icon
             icon="sun"
             className={cx(
-              false ? 'opacity-100' : 'opacity-0',
+              darkMode ? 'opacity-100' : 'opacity-0',
               'h-5 w-5 fill-current transition-opacity duration-300 ease-in-out',
             )}
           />
           <Icon
             icon="moon"
             className={cx(
-              false ? 'opacity-0' : 'opacity-100',
+              darkMode ? 'opacity-0' : 'opacity-100',
               'absolute left-0 top-0 h-5 w-5 fill-current transition-opacity duration-300 ease-in-out',
             )}
           />
