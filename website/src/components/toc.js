a1d0d82fdfd7e2db822b29618f34eb946eca9341
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2023-02-02 05:47:36 +0000

    feat(website): revise table of contents

--- website/src/components/toc.js
+++ website/src/components/toc.js
@@ -7,12 +7,12 @@ export const TOC = ({ section = '' }) => {
   const sectionLinks = parseDocs(docs, section);
 
   return (
-    <section className="mt-4 grid grid-cols-2 gap-4 text-sm md:grid-cols-3 md:text-base lg:grid-cols-4">
+    <section className="mt-4 grid grid-cols-2 gap-4 text-sm md:grid-cols-3 md:text-base lg:grid-cols-5">
       {sectionLinks.map((sectionLink) => (
         <Link
           key={sectionLink.slug}
           to={`/docs/${sectionLink.slug}`}
-          className="inline-flex aspect-video items-center justify-center rounded-md border border-gray-200 bg-gray-100 p-2 text-center leading-snug !text-black !no-underline hover:bg-blue-100 dark:border-gray-800 dark:bg-gray-900 dark:!text-gray-300 dark:hover:!border-teal-800 dark:hover:bg-teal-950 sm:rounded-lg sm:p-4"
+          className="inline-flex aspect-video items-center justify-center rounded-md border border-gray-200 bg-gray-100 p-2  text-center leading-snug !text-black !no-underline hover:border-blue-200 hover:bg-blue-100 dark:border-gray-800 dark:bg-gray-900 dark:!text-gray-300 dark:hover:!border-teal-800 dark:hover:bg-teal-950 sm:rounded-lg sm:p-4"
         >
           {sectionLink.meta.title}
         </Link>
