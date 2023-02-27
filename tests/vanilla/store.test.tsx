2f55ab185b2575f62fed76621a7d6329675fb405
daishi <daishi@axlight.com>
2023-02-27 11:45:30 +0900

    adding tests

--- tests/vanilla/store.test.tsx
+++ tests/vanilla/store.test.tsx
@@ -32,3 +32,39 @@ it('should not fire subscription if derived atom value is the same', async () =>
   store.set(countAtom, 1)
   expect(callback).toBeCalledTimes(calledTimes)
 })
+
+it('should unmount with store.get', async () => {
+  const store = createStore()
+  const countAtom = atom(0)
+  const callback = jest.fn()
+  const unsub = store.sub(countAtom, callback)
+  store.get(countAtom)
+  unsub()
+  const mountedAtoms = store.dev_get_mounted_atoms?.() ?? []
+  expect([...mountedAtoms].length).toBe(0)
+})
+
+it('should unmount dependencies with store.get', async () => {
+  const store = createStore()
+  const countAtom = atom(0)
+  const derivedAtom = atom((get) => get(countAtom) * 2)
+  const callback = jest.fn()
+  const unsub = store.sub(derivedAtom, callback)
+  store.get(derivedAtom)
+  unsub()
+  const mountedAtoms = store.dev_get_mounted_atoms?.() ?? []
+  expect([...mountedAtoms].length).toBe(0)
+})
+
+it('should unmount tree dependencies with store.get', async () => {
+  const store = createStore()
+  const countAtom = atom(0)
+  const derivedAtom = atom((get) => get(countAtom) * 2)
+  const anotherDerivedAtom = atom((get) => get(countAtom) * 3)
+  const callback = jest.fn()
+  const unsub = store.sub(anotherDerivedAtom, callback)
+  store.get(derivedAtom)
+  unsub()
+  const mountedAtoms = store.dev_get_mounted_atoms?.() ?? []
+  expect([...mountedAtoms].length).toBe(0)
+})
