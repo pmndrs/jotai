230778d95b2ca11d43e355214bd6759b8f3f9cb4
Arjun <14841132+arjunvegda@users.noreply.github.com>
2023-03-05 17:08:33 -0500

    chore: initial vitest setup

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  resolve: {
+    alias: [
+      { find: /^jotai$/, replacement: './src/index.ts' },
+      { find: /^jotai(.*)$/, replacement: './src/$1.ts' },
+    ],
+  },
+  test: {
+    name: 'jotai',
+    globals: true,
+    environment: 'jsdom',
+    dir: 'tests',
+    reporters: 'basic',
+    coverage: {
+      reporter: ['text', 'json', 'html', 'text-summary'],
+      reportsDirectory: './coverage/',
+    },
+  },
+})
