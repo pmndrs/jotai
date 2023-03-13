b61771425afeb4810b1f811bb959855425e783f9
Arjun <14841132+arjunvegda@users.noreply.github.com>
2023-03-13 19:19:53 -0400

    test: deprecate legacy tests + update scripts

---
+++ tests/react/utils/useAtomCallback.test.tsx
@@ -0,0 +1,174 @@
+import { StrictMode, useCallback, useEffect, useState } from 'react'
+import { fireEvent, render, waitFor } from '@testing-library/react'
+import { useAtom } from 'jotai/react'
+import { useAtomCallback } from 'jotai/react/utils'
+import { atom } from 'jotai/vanilla'
+
+it('useAtomCallback with get', async () => {
+  const countAtom = atom(0)
+
+  const Counter = () => {
+    const [count, setCount] = useAtom(countAtom)
+    return (
+      <>
+        <div>atom count: {count}</div>
+        <button onClick={() => setCount((c) => c + 1)}>dispatch</button>
+      </>
+    )
+  }
+
+  const Monitor = () => {
+    const [count, setCount] = useState(0)
+    const readCount = useAtomCallback(
+      useCallback((get) => {
+        const currentCount = get(countAtom)
+        setCount(currentCount)
+        return currentCount
+      }, [])
+    )
+    useEffect(() => {
+      const timer = setInterval(async () => {
+        await readCount()
+      }, 10)
+      return () => {
+        clearInterval(timer)
+      }
+    }, [readCount])
+    return (
+      <>
+        <div>state count: {count}</div>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <StrictMode>
+      <Counter />
+      <Monitor />
+    </StrictMode>
+  )
+
+  await findByText('atom count: 0')
+  fireEvent.click(getByText('dispatch'))
+  await waitFor(() => {
+    getByText('atom count: 1')
+    getByText('state count: 1')
+  })
+})
+
+it('useAtomCallback with set and update', async () => {
+  const countAtom = atom(0)
+  const changeableAtom = atom(0)
+  const Counter = () => {
+    const [count, setCount] = useAtom(countAtom)
+    return (
+      <>
+        <div>count: {count}</div>
+        <button onClick={() => setCount((c) => c + 1)}>dispatch</button>
+      </>
+    )
+  }
+
+  const Monitor = () => {
+    const [changeableCount] = useAtom(changeableAtom)
+    const changeCount = useAtomCallback(
+      useCallback((get, set) => {
+        const currentCount = get(countAtom)
+        set(changeableAtom, currentCount)
+        return currentCount
+      }, [])
+    )
+    useEffect(() => {
+      const timer = setInterval(async () => {
+        await changeCount()
+      }, 10)
+      return () => {
+        clearInterval(timer)
+      }
+    }, [changeCount])
+    return (
+      <>
+        <div>changeable count: {changeableCount}</div>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <StrictMode>
+      <Counter />
+      <Monitor />
+    </StrictMode>
+  )
+
+  await findByText('count: 0')
+  fireEvent.click(getByText('dispatch'))
+  await waitFor(() => {
+    getByText('count: 1')
+    getByText('changeable count: 1')
+  })
+})
+
+it('useAtomCallback with set and update and arg', async () => {
+  const countAtom = atom(0)
+
+  const App = () => {
+    const [count] = useAtom(countAtom)
+    const setCount = useAtomCallback(
+      useCallback((_get, set, arg: number) => {
+        set(countAtom, arg)
+        return arg
+      }, [])
+    )
+
+    return (
+      <div>
+        <p>count: {count}</p>
+        <button onClick={() => setCount(42)}>dispatch</button>
+      </div>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+
+  await findByText('count: 0')
+  fireEvent.click(getByText('dispatch'))
+  await waitFor(() => {
+    getByText('count: 42')
+  })
+})
+
+it('useAtomCallback with sync atom (#1100)', async () => {
+  const countAtom = atom(0)
+
+  const Counter = () => {
+    const [count, setCount] = useAtom(countAtom)
+    const readCount = useAtomCallback(useCallback((get) => get(countAtom), []))
+    useEffect(() => {
+      const promiseOrValue = readCount()
+      if (typeof promiseOrValue !== 'number') {
+        throw new Error('should return number')
+      }
+    }, [readCount])
+    return (
+      <>
+        <div>atom count: {count}</div>
+        <button onClick={() => setCount((c) => c + 1)}>dispatch</button>
+      </>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <StrictMode>
+      <Counter />
+    </StrictMode>
+  )
+
+  await findByText('atom count: 0')
+
+  fireEvent.click(getByText('dispatch'))
+  await findByText('atom count: 1')
+})
---
+++ tests/react/vanilla-utils/atomFamily.test.tsx
@@ -0,0 +1,282 @@
+import { StrictMode, Suspense, useState } from 'react'
+import { fireEvent, render, waitFor } from '@testing-library/react'
+import { useAtom, useSetAtom } from 'jotai/react'
+import { atom } from 'jotai/vanilla'
+import type { SetStateAction, WritableAtom } from 'jotai/vanilla'
+import { atomFamily } from 'jotai/vanilla/utils'
+
+it('new atomFamily impl', async () => {
+  const myFamily = atomFamily((param: string) => atom(param))
+
+  const Displayer = ({ index }: { index: string }) => {
+    const [count] = useAtom(myFamily(index))
+    return <div>count: {count}</div>
+  }
+  const { findByText } = render(
+    <StrictMode>
+      <Displayer index={'a'} />
+    </StrictMode>
+  )
+
+  await findByText('count: a')
+})
+
+it('primitive atomFamily returns same reference for same parameters', async () => {
+  const myFamily = atomFamily((num: number) => atom({ num }))
+  expect(myFamily(0)).toEqual(myFamily(0))
+  expect(myFamily(0)).not.toEqual(myFamily(1))
+  expect(myFamily(1)).not.toEqual(myFamily(0))
+})
+
+it('read-only derived atomFamily returns same reference for same parameters', async () => {
+  const arrayAtom = atom([0])
+  const myFamily = atomFamily((num: number) =>
+    atom((get) => get(arrayAtom)[num] as number)
+  )
+  expect(myFamily(0)).toEqual(myFamily(0))
+  expect(myFamily(0)).not.toEqual(myFamily(1))
+  expect(myFamily(1)).not.toEqual(myFamily(0))
+})
+
+it('removed atom creates a new reference', async () => {
+  const bigAtom = atom([0])
+  const myFamily = atomFamily((num: number) =>
+    atom((get) => get(bigAtom)[num] as number)
+  )
+
+  const savedReference = myFamily(0)
+
+  expect(savedReference).toEqual(myFamily(0))
+
+  myFamily.remove(0)
+
+  const newReference = myFamily(0)
+
+  expect(savedReference).not.toEqual(newReference)
+
+  myFamily.remove(1337)
+
+  expect(myFamily(0)).toEqual(newReference)
+})
+
+it('primitive atomFamily initialized with props', async () => {
+  const myFamily = atomFamily((param: number) => atom(param))
+
+  const Displayer = ({ index }: { index: number }) => {
+    const [count, setCount] = useAtom(myFamily(index))
+    return (
+      <div>
+        count: {count}
+        <button onClick={() => setCount((c) => c + 10)}>button</button>
+      </div>
+    )
+  }
+
+  const Parent = () => {
+    const [index, setIndex] = useState(1)
+
+    return (
+      <div>
+        <button onClick={() => setIndex((i) => i + 1)}>increment</button>
+        <Displayer index={index} />
+      </div>
+    )
+  }
+
+  const { findByText, getByText } = render(
+    <StrictMode>
+      <Parent />
+    </StrictMode>
+  )
+
+  await findByText('count: 1')
+
+  fireEvent.click(getByText('button'))
+  await findByText('count: 11')
+
+  fireEvent.click(getByText('increment'))
+  await findByText('count: 2')
+
+  fireEvent.click(getByText('button'))
+  await findByText('count: 12')
+})
+
+it('derived atomFamily functionality as usual', async () => {
+  const arrayAtom = atom([0, 0, 0])
+
+  const myFamily = atomFamily((param: number) =>
+    atom(
+      (get) => get(arrayAtom)[param] as number,
+      (_, set, update) => {
+        set(arrayAtom, (oldArray) => {
+          if (typeof oldArray[param] === 'undefined') return oldArray
+
+          const newValue =
+            typeof update === 'function'
+              ? update(oldArray[param] as number)
+              : update
+
+          const newArray = [
+            ...oldArray.slice(0, param),
+            newValue,
+            ...oldArray.slice(param + 1),
+          ]
+
+          return newArray
+        })
+      }
+    )
+  )
+
+  const Displayer = ({
+    index,
+    countAtom,
+  }: {
+    index: number
+    countAtom: WritableAtom<number, [SetStateAction<number>], void>
+  }) => {
+    const [count, setCount] = useAtom(countAtom)
+    return (
+      <div>
+        index: {index}, count: {count}
+        <button onClick={() => setCount((oldValue) => oldValue + 1)}>
+          increment #{index}
+        </button>
+      </div>
+    )
+  }
+
+  const indicesAtom = atom((get) => [...new Array(get(arrayAtom).length)])
+
+  const Parent = () => {
+    const [indices] = useAtom(indicesAtom)
+
+    return (
+      <div>
+        {indices.map((_, index) => (
+          <Displayer key={index} index={index} countAtom={myFamily(index)} />
+        ))}
+      </div>
+    )
+  }
+
+  const { getByText } = render(
+    <StrictMode>
+      <Parent />
+    </StrictMode>
+  )
+
+  await waitFor(() => {
+    getByText('index: 0, count: 0')
+    getByText('index: 1, count: 0')
+    getByText('index: 2, count: 0')
+  })
+
+  fireEvent.click(getByText('increment #1'))
+  await waitFor(() => {
+    getByText('index: 0, count: 0')
+    getByText('index: 1, count: 1')
+    getByText('index: 2, count: 0')
+  })
+
+  fireEvent.click(getByText('increment #0'))
+  await waitFor(() => {
+    getByText('index: 0, count: 1')
+    getByText('index: 1, count: 1')
+    getByText('index: 2, count: 0')
+  })
+
+  fireEvent.click(getByText('increment #2'))
+  await waitFor(() => {
+    getByText('index: 0, count: 1')
+    getByText('index: 1, count: 1')
+    getByText('index: 2, count: 1')
+  })
+})
+
+it('custom equality function work', async () => {
+  const bigAtom = atom([0])
+
+  const badFamily = atomFamily((num: { index: number }) =>
+    atom((get) => get(bigAtom)[num.index] as number)
+  )
+
+  const goodFamily = atomFamily(
+    (num: { index: number }) =>
+      atom((get) => get(bigAtom)[num.index] as number),
+    (l, r) => l.index === r.index
+  )
+
+  expect(badFamily({ index: 0 })).not.toEqual(badFamily({ index: 0 }))
+  expect(badFamily({ index: 0 })).not.toEqual(badFamily({ index: 0 }))
+
+  expect(goodFamily({ index: 0 })).toEqual(goodFamily({ index: 0 }))
+  expect(goodFamily({ index: 0 })).not.toEqual(goodFamily({ index: 1 }))
+})
+
+it('a derived atom from an async atomFamily (#351)', async () => {
+  const countAtom = atom(1)
+  const resolve: (() => void)[] = []
+  const getAsyncAtom = atomFamily((n: number) =>
+    atom(async () => {
+      await new Promise<void>((r) => resolve.push(r))
+      return n + 10
+    })
+  )
+  const derivedAtom = atom((get) => get(getAsyncAtom(get(countAtom))))
+
+  const Counter = () => {
+    const setCount = useSetAtom(countAtom)
+    const [derived] = useAtom(derivedAtom)
+    return (
+      <>
+        <div>derived: {derived}</div>
+        <button onClick={() => setCount((c) => c + 1)}>button</button>
+      </>
+    )
+  }
+
+  const { getByText, findByText } = render(
+    <StrictMode>
+      <Suspense fallback="loading">
+        <Counter />
+      </Suspense>
+    </StrictMode>
+  )
+
+  await findByText('loading')
+  resolve.splice(0).forEach((fn) => fn())
+  await findByText('derived: 11')
+
+  fireEvent.click(getByText('button'))
+  await findByText('loading')
+  resolve.splice(0).forEach((fn) => fn())
+  await findByText('derived: 12')
+
+  fireEvent.click(getByText('button'))
+  await findByText('loading')
+  resolve.splice(0).forEach((fn) => fn())
+  await findByText('derived: 13')
+})
+
+it('setShouldRemove with custom equality function', async () => {
+  const myFamily = atomFamily(
+    (num: { index: number }) => atom(num),
+    (l, r) => l.index === r.index
+  )
+  let firstTime = true
+  myFamily.setShouldRemove(() => {
+    if (firstTime) {
+      firstTime = false
+      return true
+    }
+    return false
+  })
+
+  const family1 = myFamily({ index: 0 })
+  const family2 = myFamily({ index: 0 })
+  const family3 = myFamily({ index: 0 })
+
+  expect(family1).not.toBe(family2)
+  expect(family2).toBe(family3)
+})
