67a0445f6dde7dce6c9c480f1556ee5a527a4481
sandren <20441876+sandren@users.noreply.github.com>
2023-03-19 22:10:15 -0700

    fix(website): resolve hydration error

--- website/gatsby-browser.js
+++ website/gatsby-browser.js
@@ -1,8 +1,3 @@
 import './src/styles/index.css';
-import { Layout } from './src/components/layout';
 
-export { wrapRootElement } from './gatsby-shared';
-
-export const wrapPageElement = ({ element, props }) => {
-  return <Layout showDocs={props.path.startsWith('/docs')}>{element}</Layout>;
-};
+export { wrapRootElement, wrapPageElement } from './gatsby-shared';
