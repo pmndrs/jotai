099a1c91ee2fd77750c90dc9765e5acf697912e5
Eric Dowell <eric.dowell@shipt.com>
2023-04-27 12:58:20 -0500

    Moving test to see if that cleans up diff readability.

--- tests/react/utils/useHydrateAtoms.test.tsx
+++ tests/react/utils/useHydrateAtoms.test.tsx
@@ -5,42 +5,6 @@ import { useAtom } from 'jotai/react'
 import { useHydrateAtoms } from 'jotai/react/utils'
 import { atom } from 'jotai/vanilla'
 
-it('useHydrateAtoms should only hydrate on first render using a Map', async () => {
-  const countAtom = atom(0)
-
-  const Counter = ({ initialCount }: { initialCount: number }) => {
-    useHydrateAtoms(
-      new Map<typeof countAtom, ReturnType<(typeof countAtom)['read']>>([
-        [countAtom, initialCount],
-      ])
-    )
-    const [countValue, setCount] = useAtom(countAtom)
-
-    return (
-      <>
-        <div>count: {countValue}</div>
-        <button onClick={() => setCount((count) => count + 1)}>dispatch</button>
-      </>
-    )
-  }
-  const { findByText, getByText, rerender } = render(
-    <StrictMode>
-      <Counter initialCount={42} />
-    </StrictMode>
-  )
-
-  await findByText('count: 42')
-  fireEvent.click(getByText('dispatch'))
-  await findByText('count: 43')
-
-  rerender(
-    <StrictMode>
-      <Counter initialCount={65} />
-    </StrictMode>
-  )
-  await findByText('count: 43')
-})
-
 it('useHydrateAtoms should only hydrate on first render', async () => {
   const countAtom = atom(0)
   const statusAtom = atom('fulfilled')
@@ -95,6 +59,42 @@ it('useHydrateAtoms should only hydrate on first render', async () => {
   )
   await findByText('count: 43')
   await findByText('status: fulfilled')
+})
+
+it('useHydrateAtoms should only hydrate on first render using a Map', async () => {
+  const countAtom = atom(0)
+
+  const Counter = ({ initialCount }: { initialCount: number }) => {
+    useHydrateAtoms(
+      new Map<typeof countAtom, ReturnType<(typeof countAtom)['read']>>([
+        [countAtom, initialCount],
+      ])
+    )
+    const [countValue, setCount] = useAtom(countAtom)
+
+    return (
+      <>
+        <div>count: {countValue}</div>
+        <button onClick={() => setCount((count) => count + 1)}>dispatch</button>
+      </>
+    )
+  }
+  const { findByText, getByText, rerender } = render(
+    <StrictMode>
+      <Counter initialCount={42} />
+    </StrictMode>
+  )
+
+  await findByText('count: 42')
+  fireEvent.click(getByText('dispatch'))
+  await findByText('count: 43')
+
+  rerender(
+    <StrictMode>
+      <Counter initialCount={65} />
+    </StrictMode>
+  )
+  await findByText('count: 43')
 })
 
 it('useHydrateAtoms should not trigger unnecessary re-renders', async () => {
