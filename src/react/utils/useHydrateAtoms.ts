8619dbfebc6540f106b87ea424d79ebc89e90a79
Eric Dowell <eric.dowell@shipt.com>
2023-05-01 20:47:58 -0500

    Overloading function to include Map/Array.

--- src/react/utils/useHydrateAtoms.ts
+++ src/react/utils/useHydrateAtoms.ts
@@ -4,17 +4,26 @@ import type { WritableAtom } from '../../vanilla.ts'
 type Store = ReturnType<typeof useStore>
 type Options = Parameters<typeof useStore>[0]
 type AnyWritableAtom = WritableAtom<unknown, any[], any>
+type AtomMap<A = AnyWritableAtom, V = unknown> = Map<A, V>
 type AtomTuple<A = AnyWritableAtom, V = unknown> = readonly [A, V]
 type InferAtoms<T extends Iterable<AtomTuple>> = {
   [K in keyof T]: T[K] extends AtomTuple<infer A>
     ? A extends AnyWritableAtom
       ? AtomTuple<A, ReturnType<A['read']>>
       : T[K]
-    : any
+    : never
 }
 
 const hydratedMap: WeakMap<Store, WeakSet<AnyWritableAtom>> = new WeakMap()
 
+export function useHydrateAtoms<T extends AtomMap>(
+  values: T,
+  options?: Options
+): void
+export function useHydrateAtoms<T extends Array<AtomTuple>>(
+  values: InferAtoms<T>,
+  options?: Options
+): void
 export function useHydrateAtoms<T extends Iterable<AtomTuple>>(
   values: InferAtoms<T>,
   options?: Options
