d932ccbc6c512a863f632929e48b1a83183ec3d4
Eric Dowell <github@ericdowell.com>
2023-05-02 00:05:34 -0500

    Adding ts-ignore for ts 3.8 support.

--- tests/react/utils/types.test.tsx
+++ tests/react/utils/types.test.tsx
@@ -6,16 +6,25 @@ it('useHydrateAtoms should not allow invalid atom types when array is passed', (
   function Component() {
     const countAtom = atom(0)
     const activeAtom = atom(true)
+    // Adding @ts-ignore for typescript 3.8 support
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     // @ts-expect-error TS2769
     useHydrateAtoms([
       [countAtom, 'foo'],
       [activeAtom, 0],
     ])
+    // Adding @ts-ignore for typescript 3.8 support
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     // @ts-expect-error TS2769
     useHydrateAtoms([
       [countAtom, 1],
       [activeAtom, 0],
     ])
+    // Adding @ts-ignore for typescript 3.8 support
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     // @ts-expect-error TS2769
     useHydrateAtoms([
       [countAtom, true],
