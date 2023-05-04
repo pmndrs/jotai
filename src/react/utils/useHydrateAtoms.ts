1c08b200cf10be960ed871bbd9fbada52065b292
Eric Dowell <github@ericdowell.com>
2023-05-04 09:00:36 -0500

    fix(react): Infer useHydrateAtoms value types from atom.read ReturnType (#1898)
    * Exporting AnyWritableAtom for external usage.
    
    * Updating test title.
    
    * Removing duplicate test.
    
    * Inferring type for useHydrateAtoms values based on generic.
    
    * Update src/react/utils/useHydrateAtoms.ts
    
    * Creating recursive type InferAtoms.
    
    * Updating test to use two different atoms.
    
    * Updating test to pass.
    
    * Reworking InferAtoms to work with Iterable.
    
    * Switching from 'never' to 'any', continue support for 'Map'.
    
    * Adding test to confirm continued support for 'Map'.
    
    * Removing newline between types.
    
    * Moving test to see if that cleans up diff readability.
    
    * Overloading function to include Map/Array.
    
    * Updating test for Map to include two atoms.
    
    * Adding types test for useHydrateAtoms.
    
    * Exporting all possible overloaded types.
    
    * Updating type test to include a few other atom type mismatches.
    
    * Adding ts-ignore for ts 3.8 support.
    
    ---------
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- src/react/utils/useHydrateAtoms.ts
+++ src/react/utils/useHydrateAtoms.ts
@@ -4,11 +4,32 @@ import type { WritableAtom } from '../../vanilla.ts'
 type Store = ReturnType<typeof useStore>
 type Options = Parameters<typeof useStore>[0]
 type AnyWritableAtom = WritableAtom<unknown, any[], any>
+type AtomMap<A = AnyWritableAtom, V = unknown> = Map<A, V>
+type AtomTuple<A = AnyWritableAtom, V = unknown> = readonly [A, V]
+type InferAtoms<T extends Iterable<AtomTuple>> = {
+  [K in keyof T]: T[K] extends AtomTuple<infer A>
+    ? A extends AnyWritableAtom
+      ? AtomTuple<A, ReturnType<A['read']>>
+      : T[K]
+    : never
+}
 
 const hydratedMap: WeakMap<Store, WeakSet<AnyWritableAtom>> = new WeakMap()
 
-export function useHydrateAtoms(
-  values: Iterable<readonly [AnyWritableAtom, unknown]>,
+export function useHydrateAtoms<T extends Array<AtomTuple>>(
+  values: InferAtoms<T>,
+  options?: Options
+): void
+export function useHydrateAtoms<T extends AtomMap>(
+  values: T,
+  options?: Options
+): void
+export function useHydrateAtoms<T extends Iterable<AtomTuple>>(
+  values: InferAtoms<T>,
+  options?: Options
+): void
+export function useHydrateAtoms<T extends Iterable<AtomTuple>>(
+  values: InferAtoms<T>,
   options?: Options
 ) {
   const store = useStore(options)
---
+++ tests/react/utils/types.test.tsx
@@ -0,0 +1,35 @@
+import { it } from '@jest/globals'
+import { useHydrateAtoms } from 'jotai/react/utils'
+import { atom } from 'jotai/vanilla'
+
+it('useHydrateAtoms should not allow invalid atom types when array is passed', () => {
+  function Component() {
+    const countAtom = atom(0)
+    const activeAtom = atom(true)
+    // Adding @ts-ignore for typescript 3.8 support
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // @ts-expect-error TS2769
+    useHydrateAtoms([
+      [countAtom, 'foo'],
+      [activeAtom, 0],
+    ])
+    // Adding @ts-ignore for typescript 3.8 support
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // @ts-expect-error TS2769
+    useHydrateAtoms([
+      [countAtom, 1],
+      [activeAtom, 0],
+    ])
+    // Adding @ts-ignore for typescript 3.8 support
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // @ts-expect-error TS2769
+    useHydrateAtoms([
+      [countAtom, true],
+      [activeAtom, false],
+    ])
+  }
+  Component
+})
