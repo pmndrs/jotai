56738748f397b068aba298f2fad4ac98974e1148
Eric Dowell <github@ericdowell.com>
2023-05-07 22:22:38 -0500

    Adding test that makes sure a Map works with HydrateAtoms.

--- tests/react/utils/hydrateAtoms.test.tsx
+++ tests/react/utils/hydrateAtoms.test.tsx
@@ -33,3 +33,33 @@ it('HydrateAtoms should only hydrate on first render', async () => {
   )
   await findByText('count: 43')
 })
+
+it('HydrateAtoms should only hydrate on first render using a Map', async () => {
+  const countAtom = atom(0)
+  const Counter = ({ initialCount }: { initialCount: number }) => {
+    const [countValue, setCount] = useAtom(countAtom)
+
+    return (
+      <HydrateAtoms values={new Map([[countAtom, initialCount]])}>
+        <div>count: {countValue}</div>
+        <button onClick={() => setCount((count) => count + 1)}>dispatch</button>
+      </HydrateAtoms>
+    )
+  }
+  const { findByText, getByText, rerender } = render(
+    <StrictMode>
+      <Counter initialCount={42} />
+    </StrictMode>
+  )
+
+  await findByText('count: 42')
+  fireEvent.click(getByText('dispatch'))
+  await findByText('count: 43')
+
+  rerender(
+    <StrictMode>
+      <Counter initialCount={65} />
+    </StrictMode>
+  )
+  await findByText('count: 43')
+})
