76f9f8bbfdfb6457bfe378a0056639ead8840a13
daishi <daishi@axlight.com>
2023-06-16 22:17:27 +0900

    refactor

--- tests/vanilla/utils/types.test.tsx
+++ tests/vanilla/utils/types.test.tsx
@@ -7,20 +7,17 @@ import { selectAtom } from 'jotai/vanilla/utils'
 
 it('selectAtom() should return the correct types', () => {
   const doubleCount = (x: number) => x * 2
-  function Component() {
-    const syncAtom = atom(0)
-    const syncSelectedAtom = selectAtom(syncAtom, doubleCount)
-    expectType<TypeEqual<Atom<number>, typeof syncSelectedAtom>>(true)
+  const syncAtom = atom(0)
+  const syncSelectedAtom = selectAtom(syncAtom, doubleCount)
+  expectType<TypeEqual<Atom<number>, typeof syncSelectedAtom>>(true)
 
-    const asyncAtom = atom(Promise.resolve(0))
-    const asyncSelectedAtom = selectAtom(asyncAtom, doubleCount)
-    expectType<TypeEqual<Atom<Promise<number>>, typeof asyncSelectedAtom>>(true)
+  const asyncAtom = atom(Promise.resolve(0))
+  const asyncSelectedAtom = selectAtom(asyncAtom, doubleCount)
+  expectType<TypeEqual<Atom<Promise<number>>, typeof asyncSelectedAtom>>(true)
 
-    const maybeAsyncAtom = atom(Promise.resolve(0) as number | Promise<number>)
-    const maybeAsyncSelectedAtom = selectAtom(maybeAsyncAtom, doubleCount)
-    expectType<
-      TypeEqual<Atom<number | Promise<number>>, typeof maybeAsyncSelectedAtom>
-    >(true)
-  }
-  Component
+  const maybeAsyncAtom = atom(Promise.resolve(0) as number | Promise<number>)
+  const maybeAsyncSelectedAtom = selectAtom(maybeAsyncAtom, doubleCount)
+  expectType<
+    TypeEqual<Atom<number | Promise<number>>, typeof maybeAsyncSelectedAtom>
+  >(true)
 })
