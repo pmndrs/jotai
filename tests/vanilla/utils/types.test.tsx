9e1ec669e4fb38f71a28fbc92612a3636c2cd3b7
daishi <daishi@axlight.com>
2023-06-19 09:30:11 +0900

    fix type test

--- tests/vanilla/utils/types.test.tsx
+++ tests/vanilla/utils/types.test.tsx
@@ -2,7 +2,7 @@ import { expectType } from 'ts-expect'
 import type { TypeEqual } from 'ts-expect'
 import { it } from 'vitest'
 import { atom } from 'jotai/vanilla'
-import type { Atom } from 'jotai/vanilla'
+import type { Atom, SetStateAction, WritableAtom } from 'jotai/vanilla'
 import { selectAtom, unstable_unwrap as unwrap } from 'jotai/vanilla/utils'
 
 it('selectAtom() should return the correct types', () => {
@@ -26,13 +26,28 @@ it('unwrap() should return the correct types', () => {
   const getFallbackValue = () => -1
   const syncAtom = atom(0)
   const syncUnwrappedAtom = unwrap(syncAtom, getFallbackValue)
-  expectType<TypeEqual<Atom<number>, typeof syncUnwrappedAtom>>(true)
+  expectType<
+    TypeEqual<
+      WritableAtom<number, [SetStateAction<number>], void>,
+      typeof syncUnwrappedAtom
+    >
+  >(true)
 
   const asyncAtom = atom(Promise.resolve(0))
   const asyncUnwrappedAtom = unwrap(asyncAtom, getFallbackValue)
-  expectType<TypeEqual<Atom<number>, typeof asyncUnwrappedAtom>>(true)
+  expectType<
+    TypeEqual<
+      WritableAtom<number, [SetStateAction<Promise<number>>], void>,
+      typeof asyncUnwrappedAtom
+    >
+  >(true)
 
   const maybeAsyncAtom = atom(Promise.resolve(0) as number | Promise<number>)
   const maybeAsyncUnwrappedAtom = unwrap(maybeAsyncAtom, getFallbackValue)
-  expectType<TypeEqual<Atom<number>, typeof maybeAsyncUnwrappedAtom>>(true)
+  expectType<
+    TypeEqual<
+      WritableAtom<number, [SetStateAction<number | Promise<number>>], void>,
+      typeof maybeAsyncUnwrappedAtom
+    >
+  >(true)
 })
