90010924ec0606dd53261de26056664be573380c
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2023-08-27 20:48:54 -0700

    feat(website): improve search (#2097)

--- website/src/components/toc.js
+++ website/src/components/toc.js
@@ -12,7 +12,7 @@ export const TOC = ({ section = '' }) => {
         <Link
           key={sectionLink.slug}
           to={`/docs/${sectionLink.slug}`}
-          className="inline-flex aspect-video items-center justify-center rounded-md border border-gray-200 bg-gray-100 p-2  text-center leading-snug !text-black !no-underline hover:border-blue-200 hover:bg-blue-100 dark:border-gray-800 dark:bg-gray-900 dark:!text-gray-300 dark:hover:!border-teal-800 dark:hover:bg-teal-950 sm:rounded-lg sm:p-4"
+          className="inline-flex aspect-video items-center justify-center rounded-md border border-gray-200 bg-gray-100 p-2  text-center leading-snug !text-black !no-underline hover:border-blue-200 hover:bg-blue-100 dark:border-gray-800 dark:bg-gray-900 dark:!text-gray-300 dark:!border-none dark:hover:bg-white sm:rounded-lg sm:p-4 dark:hover:!text-black"
         >
           {sectionLink.meta.title}
         </Link>
