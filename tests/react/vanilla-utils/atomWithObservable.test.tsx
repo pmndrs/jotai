3e5b68aec3a853527c7783c81fdca4206074d7e2
Daishi Kato <dai-shi@users.noreply.github.com>
2023-09-02 13:00:30 +0900

    Update atomWithObservable.test.tsx

--- tests/react/vanilla-utils/atomWithObservable.test.tsx
+++ tests/react/vanilla-utils/atomWithObservable.test.tsx
@@ -130,54 +130,6 @@ it('writable count state without initial value', async () => {
   await findByText('count: 3')
 })
 
-// See: https://github.com/pmndrs/jotai/discussions/2007
-it('writable count state without initial value and rxjs chain', async () => {
-  const single$ = new Subject<number>()
-  const double$ = single$.pipe(map((n) => n * 2))
-  const singleAtom = atomWithObservable(() => single$)
-  const doubleAtom = atomWithObservable(() => double$)
-
-  // THIS WORKS:
-  // const singleAndDoubleAtom = atom((get) =>
-  //   Promise.all([get(singleAtom), get(doubleAtom)])
-  // )
-
-  // in CounterValue:
-  // const [single, double] = useAtomValue(singleAndDoubleAtom)
-
-  const CounterValue = () => {
-    const single = useAtomValue(singleAtom)
-    const double = useAtomValue(doubleAtom)
-
-    return (
-      <>
-        single: {single}, double: {double}
-      </>
-    )
-  }
-
-  const CounterButton = () => {
-    return <button onClick={() => single$.next(2)}>button</button>
-  }
-
-  const { findByText, getByText } = render(
-    <StrictMode>
-      <Suspense fallback="loading">
-        <CounterValue />
-      </Suspense>
-      <CounterButton />
-    </StrictMode>
-  )
-
-  await findByText('loading')
-
-  fireEvent.click(getByText('button'))
-  await findByText('single: 2, double: 4')
-
-  act(() => single$.next(3))
-  await findByText('single: 3, double: 6')
-})
-
 it('writable count state with delayed value', async () => {
   const subject = new Subject<number>()
   const observableAtom = atomWithObservable(() => {
