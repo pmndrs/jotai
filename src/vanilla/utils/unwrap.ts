52832d25ea8e07b643e34988f17cf9a71cc3c9e0
satoren <satoreyo@hotmail.com>
2023-11-01 11:58:06 +0900

    Update src/vanilla/utils/unwrap.ts
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- src/vanilla/utils/unwrap.ts
+++ src/vanilla/utils/unwrap.ts
@@ -95,7 +95,7 @@ export function unwrap<Value, Args extends unknown[], Result, PendingValue>(
       return atom(
         (get) => {
           const state = get(promiseAndValueAtom)
-          if ('f' in state) {
+          if ('f' in state) { // is pending
             return state.f
           }
           return state.v
