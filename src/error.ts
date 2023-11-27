7c442951aeb9afd2124935b7b159e77638a734ac
Alex Yang <himself65@outlook.com>
2023-11-26 18:01:24 -0600

    Revert "build: fix bundler might cause error"
    This reverts commit 87e2144b92e1dad9c2a8df68ec51034b4fcaacd9.

--- src/error.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This file is used to throw an error when the library is imported inside a react-server environment.
- *
- * For example, in Next.js
- * ```ts
- * 'use server';
- * import 'jotai/react';
- */
-throw new Error('This file cannot be used inside a react-server environment.')
