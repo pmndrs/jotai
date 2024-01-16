a359266c873dd0e044a9857b3d477c803e00838a
daishi <daishi@axlight.com>
2024-01-16 14:37:13 +0900

    chore(deps): update dev dependencies

--- src/vanilla/store.ts
+++ src/vanilla/store.ts
@@ -430,11 +430,8 @@ export const createStore = () => {
     }
     try {
       const valueOrPromise = atom.read(getter, options as any)
-      return setAtomValueOrPromise(
-        atom,
-        valueOrPromise,
-        nextDependencies,
-        () => controller?.abort(),
+      return setAtomValueOrPromise(atom, valueOrPromise, nextDependencies, () =>
+        controller?.abort(),
       )
     } catch (error) {
       return setAtomError(atom, error, nextDependencies)
--- src/vanilla/typeUtils.ts
+++ src/vanilla/typeUtils.ts
@@ -5,24 +5,13 @@ export type Setter = Parameters<
   WritableAtom<unknown, unknown[], unknown>['write']
 >[1]
 
-export type ExtractAtomValue<AtomType> = AtomType extends Atom<infer Value>
-  ? Value
-  : never
+export type ExtractAtomValue<AtomType> =
+  AtomType extends Atom<infer Value> ? Value : never
 
-export type ExtractAtomArgs<AtomType> = AtomType extends WritableAtom<
-  any,
-  infer Args,
-  any
->
-  ? Args
-  : never
+export type ExtractAtomArgs<AtomType> =
+  AtomType extends WritableAtom<any, infer Args, any> ? Args : never
 
-export type ExtractAtomResult<AtomType> = AtomType extends WritableAtom<
-  any,
-  any[],
-  infer Result
->
-  ? Result
-  : never
+export type ExtractAtomResult<AtomType> =
+  AtomType extends WritableAtom<any, any[], infer Result> ? Result : never
 
 export type SetStateAction<Value> = ExtractAtomArgs<PrimitiveAtom<Value>>[0]
