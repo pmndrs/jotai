84de9226275b49e1dd22d4ef8b028ffd9f410bef
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-02-16 05:55:56 +0000

    feat(website): UI/UX improvements

--- website/src/components/main.js
+++ website/src/components/main.js
@@ -1,9 +1,6 @@
 export const Main = ({ children, ...rest }) => {
   return (
-    <main
-      className="p-6 sm:p-8 lg:mt-8 lg:w-full lg:max-w-prose lg:flex-shrink xl:max-w-4xl xl:p-16 2xl:max-w-5xl"
-      {...rest}
-    >
+    <main className="p-6 sm:p-8 lg:mt-8 lg:max-w-5xl lg:flex-shrink xl:p-16" {...rest}>
       {children}
     </main>
   );
