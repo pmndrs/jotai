8f2afaeabf8494c2797ac834c8c8b6421580fd12
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-03-04 00:59:33 +0000

    fix(website): add scroll overflow

--- website/src/components/modal.js
+++ website/src/components/modal.js
@@ -8,9 +8,11 @@ export const Modal = ({ isOpen, onClose, children, ...rest }) => {
         className="z-100 fixed top-0 right-0 bottom-0 h-full w-8 bg-white dark:bg-gray-950"
         aria-hidden="true"
       />
-      <div className="fixed inset-0 z-[1000] flex justify-center bg-black/50 p-8 backdrop-blur sm:p-12 2xl:p-32">
+      <div className="fixed inset-0 h-full w-full z-[1000] flex justify-center bg-black/50 p-8 backdrop-blur sm:p-12 2xl:p-32">
         <RemoveScroll className="w-full max-w-3xl">
-          <Dialog.Panel className="z-[1001] min-w-full">{children}</Dialog.Panel>
+          <Dialog.Panel className="z-[1001] min-w-full overflow-y-auto max-h-full">
+            {children}
+          </Dialog.Panel>
         </RemoveScroll>
       </div>
     </Dialog>
