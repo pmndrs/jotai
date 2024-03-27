193bc1fb6d09817b7d24881d8b47d9c2811ee929
Iwo Plaza <iwoplaza@gmail.com>
2024-03-27 16:19:26 +0100

    add `unstable_is` hack for use in `jotai-store`

--- src/vanilla/utils/atomWithLazy.ts
+++ src/vanilla/utils/atomWithLazy.ts
@@ -1,11 +1,20 @@
 import { atom } from '../../vanilla.ts'
-import type { SetStateAction } from '../../vanilla.ts'
+import type { Atom, PrimitiveAtom, SetStateAction } from '../../vanilla.ts'
 
 export function atomWithLazy<Value>(makeInitial: () => Value) {
-  const wrappedAtom = atom(() => atom(makeInitial()))
+  const wrappedAtom: Atom<PrimitiveAtom<Value>> & { init?: Atom<undefined> } =
+    atom(() => atom(makeInitial()))
 
-  return atom(
+  const proxyAtom = atom(
     (get) => get(get(wrappedAtom)),
     (get, set, value: SetStateAction<Value>) => set(get(wrappedAtom), value),
   )
+
+  wrappedAtom.init = atom(undefined)
+  // when writing to wrappedAtom through proxyAtom, the store
+  // thinks we are actually storing the value of `proxyAtom`.
+  wrappedAtom.unstable_is = (a) =>
+    a.unstable_is ? a.unstable_is(proxyAtom) : a === proxyAtom
+
+  return proxyAtom
 }
