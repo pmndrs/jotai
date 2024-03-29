0249b0e96a5c350c074816d9b6027b6e55b77119
Iwo Plaza <iwoplaza@gmail.com>
2024-03-29 20:32:10 +0100

    clear type declarations

--- src/vanilla/utils/atomWithLazy.ts
+++ src/vanilla/utils/atomWithLazy.ts
@@ -1,6 +1,8 @@
-import { atom } from '../../vanilla.ts'
+import { PrimitiveAtom, atom } from '../../vanilla.ts'
 
-export function atomWithLazy<Value>(makeInitial: () => Value) {
+export function atomWithLazy<Value>(
+  makeInitial: () => Value,
+): PrimitiveAtom<Value> {
   return {
     ...atom(undefined as unknown as Value),
     get init() {
