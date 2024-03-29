240b13de864aa5de7bbc9d90f72980edd4b97ba1
Iwo Plaza <iwoplaza@gmail.com>
2024-03-29 20:12:38 +0100

    add more elegant implementation for atomWithLazy

--- src/vanilla/utils/atomWithLazy.ts
+++ src/vanilla/utils/atomWithLazy.ts
@@ -1,20 +1,10 @@
 import { atom } from '../../vanilla.ts'
-import type { Atom, PrimitiveAtom, SetStateAction } from '../../vanilla.ts'
 
 export function atomWithLazy<Value>(makeInitial: () => Value) {
-  const wrappedAtom: Atom<PrimitiveAtom<Value>> & { init?: Atom<undefined> } =
-    atom(() => atom(makeInitial()))
-
-  const proxyAtom = atom(
-    (get) => get(get(wrappedAtom)),
-    (get, set, value: SetStateAction<Value>) => set(get(wrappedAtom), value),
-  )
-
-  wrappedAtom.init = atom(undefined)
-  // when writing to wrappedAtom through proxyAtom, the store
-  // thinks we are actually storing the value of `proxyAtom`.
-  wrappedAtom.unstable_is = (a) =>
-    a.unstable_is ? a.unstable_is(proxyAtom) : a === proxyAtom
-
-  return proxyAtom
+  return {
+    ...atom(undefined as unknown as Value),
+    get init() {
+      return makeInitial()
+    },
+  }
 }
