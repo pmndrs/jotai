93a28f4d0b87c5d9fec4932e9ee0dcd67c1cec0a
Oliver Hoff <oliver@hofff.com>
2024-04-07 14:06:41 +0200

    fix(vanilla): setters do not freeze new values (#2476)
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- tests/react/vanilla-utils/freezeAtom.test.tsx
+++ tests/react/vanilla-utils/freezeAtom.test.tsx
@@ -1,43 +1,69 @@
 import { StrictMode } from 'react'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { it } from 'vitest'
 import { useAtom } from 'jotai/react'
 import { atom } from 'jotai/vanilla'
 import { freezeAtom, freezeAtomCreator } from 'jotai/vanilla/utils'
 
 it('freezeAtom basic test', async () => {
-  const objAtom = atom({ count: 0 })
+  const objAtom = atom({ deep: {} }, (_get, set, _ignored?) => {
+    set(objAtom, { deep: {} })
+  })
 
   const Component = () => {
-    const [obj] = useAtom(freezeAtom(objAtom))
+    const [obj, setObj] = useAtom(freezeAtom(objAtom))
 
-    return <div>isFrozen: {`${Object.isFrozen(obj)}`}</div>
+    return (
+      <>
+        <button onClick={setObj}>change</button>
+        <div>
+          isFrozen: {`${Object.isFrozen(obj) && Object.isFrozen(obj.deep)}`}
+        </div>
+      </>
+    )
   }
 
-  const { findByText } = render(
+  const { getByText, findByText } = render(
     <StrictMode>
       <Component />
     </StrictMode>,
   )
 
   await findByText('isFrozen: true')
+
+  fireEvent.click(getByText('change'))
+
+  await findByText('isFrozen: true')
 })
 
 it('freezeAtomCreator basic test', async () => {
   const createFrozenAtom = freezeAtomCreator(atom)
-  const objAtom = createFrozenAtom({ count: 0 })
+  const objAtom = createFrozenAtom({ deep: {} }, (_get, set, _ignored?) => {
+    set(objAtom, { deep: {} })
+  })
 
   const Component = () => {
-    const [obj] = useAtom(objAtom)
+    const [obj, setObj] = useAtom(objAtom)
 
-    return <div>isFrozen: {`${Object.isFrozen(obj)}`}</div>
+    return (
+      <>
+        <button onClick={setObj}>change</button>
+        <div>
+          isFrozen: {`${Object.isFrozen(obj) && Object.isFrozen(obj.deep)}`}
+        </div>
+      </>
+    )
   }
 
-  const { findByText } = render(
+  const { getByText, findByText } = render(
     <StrictMode>
       <Component />
     </StrictMode>,
   )
+
+  await findByText('isFrozen: true')
+
+  fireEvent.click(getByText('change'))
 
   await findByText('isFrozen: true')
 })
