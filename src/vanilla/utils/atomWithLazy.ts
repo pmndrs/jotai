b70d71f728d846050154d62ee2075e55611ee486
Alex Yang <himself65@outlook.com>
2024-04-08 14:25:50 -0500

    fix: lazyMap on `atomWithLazy`

--- src/vanilla/utils/atomWithLazy.ts
+++ src/vanilla/utils/atomWithLazy.ts
@@ -1,13 +1,20 @@
 import { PrimitiveAtom, atom } from '../../vanilla.ts'
 
+const lazyMap = new WeakMap<() => unknown, unknown>()
+
 export function atomWithLazy<Value>(
   makeInitial: () => Value,
 ): PrimitiveAtom<Value> {
   const a = atom(undefined as unknown as Value)
   delete (a as { init?: Value }).init
   Object.defineProperty(a, 'init', {
-    get() {
-      return makeInitial()
+    get(): Value {
+      if (lazyMap.has(makeInitial)) {
+        return lazyMap.get(makeInitial) as Value
+      }
+      const initResult = makeInitial()
+      lazyMap.set(makeInitial, initResult)
+      return initResult
     },
   })
   return a
