1ed92aedbe4953275f99a2b42cb72a07c1e28859
daishi <daishi@axlight.com>
2024-04-10 09:32:49 +0900

    more memory leak tests

--- tests/vanilla/memoryleaks.test.ts
+++ tests/vanilla/memoryleaks.test.ts
@@ -1,14 +1,59 @@
 import LeakDetector from 'jest-leak-detector'
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { atom, createStore } from 'jotai/vanilla'
 
-it('should not have memory leaks with an atom', async () => {
-  const store = createStore()
-  let detector: LeakDetector
-  ;(() => {
-    const objAtom = atom({})
-    detector = new LeakDetector(store.get(objAtom))
-  })()
-  const isLeaking = await detector.isLeaking()
-  expect(isLeaking).toBe(false)
+describe('test memory leaks (get & set only)', () => {
+  it('one atom', async () => {
+    const store = createStore()
+    let detector: LeakDetector
+    ;(() => {
+      const objAtom = atom({})
+      detector = new LeakDetector(store.get(objAtom))
+    })()
+    expect(await detector.isLeaking()).toBe(false)
+  })
+
+  it('two atoms', async () => {
+    const store = createStore()
+    let detector1: LeakDetector
+    let detector2: LeakDetector
+    ;(() => {
+      const objAtom = atom({})
+      detector1 = new LeakDetector(store.get(objAtom))
+      const derivedAtom = atom((get) => ({ obj: get(objAtom) }))
+      detector2 = new LeakDetector(store.get(derivedAtom))
+    })()
+    expect(await detector1.isLeaking()).toBe(false)
+    expect(await detector2.isLeaking()).toBe(false)
+  })
+})
+
+describe('test memory leaks (with subscribe)', () => {
+  it('one atom', async () => {
+    const store = createStore()
+    let detector: LeakDetector
+    ;(() => {
+      const objAtom = atom({})
+      detector = new LeakDetector(store.get(objAtom))
+      const unsub = store.sub(objAtom, () => {})
+      unsub()
+    })()
+    expect(await detector.isLeaking()).toBe(false)
+  })
+
+  it('two atoms', async () => {
+    const store = createStore()
+    let detector1: LeakDetector
+    let detector2: LeakDetector
+    ;(() => {
+      const objAtom = atom({})
+      detector1 = new LeakDetector(store.get(objAtom))
+      const derivedAtom = atom((get) => ({ obj: get(objAtom) }))
+      detector2 = new LeakDetector(store.get(derivedAtom))
+      const unsub = store.sub(derivedAtom, () => {})
+      unsub()
+    })()
+    expect(await detector1.isLeaking()).toBe(false)
+    expect(await detector2.isLeaking()).toBe(false)
+  })
 })
