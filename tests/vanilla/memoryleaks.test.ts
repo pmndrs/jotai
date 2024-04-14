07a83be75b6fa856f57c61cd9dea6f757c6f9374
Oliver Hoff <oliver@hofff.com>
2024-04-14 17:59:41 +0200

    chore: improve memory tests

--- tests/vanilla/memoryleaks.test.ts
+++ tests/vanilla/memoryleaks.test.ts
@@ -1,14 +1,26 @@
 import LeakDetector from 'jest-leak-detector'
 import { expect, it } from 'vitest'
-import { atom, createStore } from 'jotai/vanilla'
+import { Atom, atom, createStore } from 'jotai/vanilla'
 
-it('should not have memory leaks with an atom', async () => {
+it('should not hold onto atoms that are not mounted', async () => {
   const store = createStore()
-  let detector: LeakDetector
-  ;(() => {
-    const objAtom = atom({})
-    detector = new LeakDetector(store.get(objAtom))
-  })()
-  const isLeaking = await detector.isLeaking()
-  expect(isLeaking).toBe(false)
+
+  let objAtom: Atom<unknown> | undefined = atom({})
+  const detector = new LeakDetector(objAtom)
+  store.get(objAtom)
+  objAtom = undefined
+
+  await expect(detector.isLeaking()).resolves.toBe(false)
+})
+
+it('should not hold onto dependent atoms that are not mounted', async () => {
+  const store = createStore()
+
+  const objAtom = atom({})
+  let depAtom: Atom<unknown> | undefined = atom((get) => get(objAtom))
+  const detector = new LeakDetector(depAtom)
+  store.get(depAtom)
+  depAtom = undefined
+
+  await expect(detector.isLeaking()).resolves.toBe(false)
 })
