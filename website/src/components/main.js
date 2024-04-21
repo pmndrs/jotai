0ec391e7b5411bef048149fe1f45c501fdd2cc05
ouweiya <higogle1@gmail.com>
2024-04-22 05:22:37 +0800

    fixed layout flickering.

--- website/src/components/main.js
+++ website/src/components/main.js
@@ -1,6 +1,6 @@
 export const Main = ({ children, ...rest }) => {
   return (
-    <main className="p-6 sm:p-8 lg:mt-8 lg:max-w-5xl lg:flex-shrink xl:p-16" {...rest}>
+    <main className="p-6 sm:p-8 lg:mt-8 lg:max-w-5xl lg:flex-shrink xl:p-16 flex-grow" {...rest}>
       {children}
     </main>
   );
