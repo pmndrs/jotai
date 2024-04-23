27bcdfe07fac54fd8553a7041cfd2a6a727923b0
ouweiya <higogle1@gmail.com>
2024-04-23 15:10:41 +0800

    Improving the document reading experience, centering the main text is more conducive to reading.

--- website/src/components/sidebar.js
+++ website/src/components/sidebar.js
@@ -10,7 +10,7 @@ export const Sidebar = ({ showDocs = false }) => {
   const setShowHelp = useSetAtom(helpAtom);
 
   return (
-    <aside className="scrollbar sticky top-0 hidden h-full max-h-screen min-h-full w-full flex-shrink-0 flex-col justify-between overflow-y-scroll overscroll-none p-8 lg:flex lg:max-w-[288px] xl:max-w-[384px] xl:p-16 2xl:max-w-[448px]">
+    <aside className="scrollbar sticky top-0 hidden h-full max-h-screen min-h-full w-full flex-shrink-0 flex-col justify-between overflow-y-scroll overscroll-none p-8 lg:flex lg:max-w-[288px] 2xl:max-w-[384px] xl:p-8 2xl:max-w-[360px]">
       <div className="flex-grow">
         <Jotai isDocsPage={showDocs} />
         <div className="mt-8 flex flex-col space-y-4">
