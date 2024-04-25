80a61e3585288ab6d2b49d10aa313066e8f7fb20
ouweiya <higogle1@gmail.com>
2024-04-25 22:49:21 +0800

    Fix Gatsby Link URL partial match.

--- website/src/components/docs.js
+++ website/src/components/docs.js
@@ -34,7 +34,6 @@ export const Docs = ({ className = '', ...rest }) => {
                   onClick={() => setIsMenuOpen(false)}
                   className="relative -left-3 inline-block whitespace-nowrap rounded border dark:!border-none border-transparent px-2 py-1 text-base text-black hover:!border-blue-200 hover:bg-blue-100 dark:text-gray-300 dark:hover:!text-black dark:hover:bg-white"
                   activeClassName="!border-blue-200 dark:!border-white bg-blue-100 dark:bg-white dark:!text-black"
-                  partiallyActive
                 >
                   {doc.meta.title}
                 </Link>
