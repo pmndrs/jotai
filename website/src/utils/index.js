86eb61670394b896268436fa1c935084f8fcad85
ouweiya <higogle1@gmail.com>
2024-04-26 13:17:54 +0800

    When the title is inline code, the ID is undefined

--- website/src/utils/index.js
+++ website/src/utils/index.js
@@ -1,5 +1,22 @@
 import kebabCase from 'just-kebab-case';
+import React from 'react';
+
+const getTextContent = (children) => {
+  let text = '';
+  React.Children.toArray(children).forEach((child) => {
+    if (typeof child === 'string') {
+      text += child;
+    } else if (React.isValidElement(child) && child.props.children) {
+      text += getTextContent(child.props.children);
+    }
+  });
+  return text;
+};
 
 export const getAnchor = (value) => {
-  return typeof value === 'string' ? kebabCase(value.toLowerCase().replaceAll("'", '')) : '';
+  if (typeof value === 'string') {
+    return kebabCase(value.toLowerCase().replaceAll("'", ''));
+  } else {
+    return kebabCase(getTextContent(value).toLowerCase().replaceAll("'", ''));
+  }
 };
