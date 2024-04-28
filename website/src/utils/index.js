90ce79caf800ec09c9ffff0fcc8207c91dc301e5
sandren <20441876+sandren@users.noreply.github.com>
2024-04-28 14:14:58 -0700

    fix: revise import

--- website/src/utils/index.js
+++ website/src/utils/index.js
@@ -1,18 +1,20 @@
+import { Children, isValidElement } from 'react';
 import kebabCase from 'just-kebab-case';
-import React from 'react';
-
-const getTextContent = (children) => {
-  let text = '';
-  React.Children.toArray(children).forEach((child) => {
-    if (typeof child === 'string') {
-      text += child;
-    } else if (React.isValidElement(child) && child.props.children) {
-      text += getTextContent(child.props.children);
-    }
-  });
-  return text;
-};
 
 export const getAnchor = (value) => {
   return kebabCase(getTextContent(value).toLowerCase().replaceAll("'", ''));
+};
+
+const getTextContent = (children) => {
+  let text = '';
+
+  Children.toArray(children).forEach((child) => {
+    if (typeof child === 'string') {
+      text += child;
+    } else if (isValidElement(child) && child.props.children) {
+      text += getTextContent(child.props.children);
+    }
+  });
+
+  return text;
 };
