c1bcad768b2338d960872c9a51933e03921d91b2
Arjun <14841132+arjunvegda@users.noreply.github.com>
2024-05-20 10:09:21 -0400

    test: add failing test for store.sub not being called afer restore

--- tests/vanilla/storedev.test.tsx
+++ tests/vanilla/storedev.test.tsx
@@ -149,4 +149,28 @@ describe.skipIf(!IS_DEV_STORE2)('[DEV-ONLY] dev-only methods rev4', () => {
     expect(store.get(countAtom)).toBe(2)
     expect(store.get?.(derivedAtom)).toBe(4)
   })
+
+  it('should restore atoms and call store listeners correctly', () => {
+    const store = createStore() as any
+    if (!('dev4_restore_atoms' in store)) {
+      throw new Error('dev methods are not available')
+    }
+    const countAtom = atom(0)
+    const derivedAtom = atom((get) => get(countAtom) * 2)
+    const countCb = vi.fn()
+    const derivedCb = vi.fn()
+    store.set(countAtom, 2)
+    store.set(countAtom, 4)
+    const unsubCount = store.sub(countAtom, countCb)
+    const unsubDerived = store.sub(derivedAtom, derivedCb)
+    store.dev4_restore_atoms([
+      [countAtom, 1],
+      [derivedAtom, 2],
+    ])
+
+    expect(countCb).toHaveBeenCalled()
+    expect(derivedCb).toHaveBeenCalled()
+    unsubCount()
+    unsubDerived()
+  })
 })
