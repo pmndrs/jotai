aa2725b685a2c1a32c2176c09c3189c9e688b984
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-11-18 05:07:14 +0000

    feat(website): UI/UX improvements

--- website/src/components/toc.js
+++ website/src/components/toc.js
@@ -8,13 +8,13 @@ export const TOC = ({ section = '' }) => {
   const sectionLinks = parseDocs(docs, section);
 
   return (
-    <section className="mt-4 grid grid-cols-2 gap-4 text-xs sm:text-sm md:grid-cols-3 md:text-base lg:grid-cols-4 2xl:grid-cols-5">
-      {sectionLinks.map((sectionLink) => (
+    <section className="mt-4 grid grid-cols-2 gap-4 text-xs sm:text-sm md:grid-cols-3 md:text-base lg:grid-cols-4">
+      {sectionLinks?.map((sectionLink) => (
         <Link
           key={sectionLink.slug}
           to={`/docs/${sectionLink.slug}`}
           title={sectionLink.meta.title}
-          className="inline-flex aspect-video items-center justify-center rounded-md border border-gray-200 bg-gray-100 p-2 text-center leading-snug !text-black !no-underline hover:border-blue-200 hover:bg-blue-100 dark:border-gray-800 dark:bg-gray-900 dark:!text-gray-300 dark:!border-none dark:hover:bg-white sm:rounded-lg sm:p-4 dark:hover:!text-black"
+          className="inline-flex aspect-[21/9] items-center justify-center rounded-md border border-gray-200 bg-gray-100 p-2 text-center leading-snug !text-black !no-underline hover:border-blue-200 hover:bg-blue-100 dark:border-gray-800 dark:bg-gray-900 dark:!text-gray-300 dark:!border-none dark:hover:bg-white sm:rounded-lg sm:p-4 dark:hover:!text-black text-base text-pretty"
         >
           <div
             className={cx(
@@ -80,7 +80,7 @@ const parseDocs = (docs, section) => {
     ];
   });
 
-  newDocs = newDocs.find((docSection) => docSection.title === section).contents;
+  newDocs = newDocs.find((docSection) => docSection.title === section)?.contents;
 
   return newDocs;
 };
