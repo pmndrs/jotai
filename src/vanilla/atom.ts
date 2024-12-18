4bb5bb42a3c01f20f59da4c9aa9c97c17dfbc754
David Maskasky <dmaskasky@gmail.com>
2024-12-17 19:57:56 -0800

    support store.onChange and atom.onInit hooks

--- src/vanilla/atom.ts
+++ src/vanilla/atom.ts
@@ -1,3 +1,5 @@
+import type { Store } from './store'
+
 type Getter = <Value>(atom: Atom<Value>) => Value
 
 type Setter = <Value, Args extends unknown[], Result>(
@@ -47,6 +49,10 @@ export interface Atom<Value> {
    * @private
    */
   debugPrivate?: boolean
+  /**
+   * Fires after atom is referenced by the store for the first time
+   */
+  unstable_onInit?: (store: Store) => void
 }
 
 export interface WritableAtom<Value, Args extends unknown[], Result>
