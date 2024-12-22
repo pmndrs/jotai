36062fb13b79849da36748c0e2673d898343dff4
Daryl Castro <69996340+leweyse@users.noreply.github.com>
2024-12-22 02:59:50 +0100

    Fix: update examples (#2883)
    * fix: update readme (examples)
    
    * fix: update relevant dependencies
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- examples/todos/src/index.tsx
+++ examples/todos/src/index.tsx
@@ -1,6 +1,7 @@
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import 'antd/dist/antd.css'
 import './styles.css'
 import App from './App'
 
-render(<App />, document.getElementById('root'))
+const rootElement = document.getElementById('root')
+createRoot(rootElement!).render(<App />)
