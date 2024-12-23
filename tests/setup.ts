d81d89ebcd07a2779392cbedd83ea08892bd3eea
daishi <daishi@axlight.com>
2024-12-23 11:45:51 +0900

    revert setup.ts

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,15 +1,1 @@
 import '@testing-library/jest-dom/vitest'
-import { expect } from 'vitest'
-
-expect.extend({
-  toHaveBeenCalledBefore(received, expected) {
-    const pass =
-      received.mock.invocationCallOrder[0] <
-      expected.mock.invocationCallOrder[0]
-    return {
-      pass,
-      message: () =>
-        `expected ${received} to have been called before ${expected}`,
-    }
-  },
-})
