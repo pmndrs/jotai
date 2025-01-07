b213c268248243d5255f9c8a273cc75323b5e087
daishi <daishi@axlight.com>
2025-01-07 21:35:10 +0900

    separate internals entrypoint

---
+++ src/vanilla/internals.ts
@@ -0,0 +1,9 @@
+// Internal functions (subject to change without notice)
+// In case you rely on them, be sure to pin the version
+export {
+  INTERNAL_getSecretStoreMethods,
+  INTERNAL_buildStore,
+  INTERNAL_createBatch,
+  INTERNAL_addBatchFunc,
+  INTERNAL_flushBatch,
+} from './store.ts'
