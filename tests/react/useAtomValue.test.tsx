e81459c3cb467156d6417b979552bbbf82b38f15
Donald Thai <donald.thai@gmail.com>
2025-05-21 20:01:36 -0700

    lint fix

--- tests/react/useAtomValue.test.tsx
+++ tests/react/useAtomValue.test.tsx
@@ -1,5 +1,5 @@
 import React, { StrictMode, Suspense } from 'react'
-import { render, screen, act } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { it } from 'vitest'
 import { useAtomValue, useSetAtom } from 'jotai/react'
