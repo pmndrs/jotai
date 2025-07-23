cbaf63797881a9a3eb70bb77d547cc459a86525d
David Maskasky <dmaskasky@gmail.com>
2025-07-23 12:36:21 -0700

    add test for building block stale references

--- tests/vanilla/internals.test.tsx
+++ tests/vanilla/internals.test.tsx
@@ -1,8 +1,11 @@
-import { describe, expect, it } from 'vitest'
-import { createStore } from 'jotai'
+import { describe, expect, it, vi } from 'vitest'
+import { atom, createStore } from 'jotai'
 import {
-  INTERNAL_buildStoreRev2 as INTERNAL_buildStore,
-  INTERNAL_getBuildingBlocksRev2 as INTERNAL_getBuildingBlocks,
+  type INTERNAL_AtomState as AtomState,
+  type INTERNAL_AtomStateMap as AtomStateMap,
+  type INTERNAL_BuildingBlocks as BuildingBlocks,
+  INTERNAL_buildStoreRev2 as buildStore,
+  INTERNAL_getBuildingBlocksRev2 as getBuildingBlocks,
 } from 'jotai/vanilla/internals'
 
 describe('internals', () => {
@@ -12,15 +15,41 @@ describe('internals', () => {
     }
     {
       const store = createStore()
-      const buildingBlocks = INTERNAL_getBuildingBlocks(store)
+      const buildingBlocks = getBuildingBlocks(store)
       expect(buildingBlocks.length).toBe(24)
       expect(isSparse(buildingBlocks)).toBe(false)
     }
     {
-      const store = INTERNAL_buildStore()
-      const buildingBlocks = INTERNAL_getBuildingBlocks(store)
+      const store = buildStore()
+      const buildingBlocks = getBuildingBlocks(store)
       expect(buildingBlocks.length).toBe(24)
       expect(isSparse(buildingBlocks)).toBe(false)
     }
+  })
+
+  it('internals should not hold stale references', () => {
+    const createMockAtomStateMap = () => {
+      return {
+        get: vi.fn(() => {
+          return {
+            d: new Map(),
+            p: new Set(),
+            n: 0,
+            v: 0,
+          } satisfies AtomState
+        }),
+        set: vi.fn(),
+      } satisfies AtomStateMap
+    }
+    const mockAtomStateMap1 = createMockAtomStateMap()
+    const buildingBlocks1: Partial<BuildingBlocks> = [mockAtomStateMap1]
+    const store1 = buildStore(...buildingBlocks1)
+    const buildingBlocks2: BuildingBlocks = [...getBuildingBlocks(store1)]
+    const mockAtomStateMap2 = createMockAtomStateMap()
+    buildingBlocks2[0] = mockAtomStateMap2
+    const store2 = buildStore(...buildingBlocks2)
+    store2.get(atom(0))
+    expect(mockAtomStateMap1.get).not.toBeCalled()
+    expect(mockAtomStateMap2.get).toBeCalled()
   })
 })
