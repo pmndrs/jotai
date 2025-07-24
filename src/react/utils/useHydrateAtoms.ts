10af6279e59dbec262fe03bcd14d4ec7f126ac89
daishi <daishi@axlight.com>
2025-07-24 11:01:54 +0900

    cherry pick c2080e03282d133bab5bffbc5ee8d21f82e4567a

--- src/react/utils/useHydrateAtoms.ts
+++ src/react/utils/useHydrateAtoms.ts
@@ -7,21 +7,10 @@ type Options = Parameters<typeof useStore>[0] & {
 }
 type AnyWritableAtom = WritableAtom<unknown, never[], unknown>
 
-type SpreadArgs<Args extends readonly unknown[]> = Args extends readonly [
-  infer First,
-  ...infer Rest,
-]
-  ? readonly [First, ...Rest]
-  : Args extends readonly [infer Single]
-    ? readonly [Single]
-    : readonly []
-
 type InferAtomTuples<T> = {
-  [K in keyof T]: T[K] extends readonly [infer A, ...infer _Rest]
+  [K in keyof T]: T[K] extends readonly [infer A, unknown]
     ? A extends WritableAtom<unknown, infer Args, infer _Result>
-      ? Args extends readonly unknown[]
-        ? readonly [A, ...SpreadArgs<Args>]
-        : readonly [A]
+      ? readonly [A, ...Args]
       : T[K]
     : never
 }
@@ -33,7 +22,7 @@ export type INTERNAL_InferAtomTuples<T> = InferAtomTuples<T>
 const hydratedMap: WeakMap<Store, WeakSet<AnyWritableAtom>> = new WeakMap()
 
 export function useHydrateAtoms<
-  T extends (readonly [AnyWritableAtom, ...any[]])[],
+  T extends (readonly [AnyWritableAtom, ...unknown[]])[],
 >(values: InferAtomTuples<T>, options?: Options): void
 
 export function useHydrateAtoms<T extends Map<AnyWritableAtom, unknown>>(
@@ -42,11 +31,11 @@ export function useHydrateAtoms<T extends Map<AnyWritableAtom, unknown>>(
 ): void
 
 export function useHydrateAtoms<
-  T extends Iterable<readonly [AnyWritableAtom, ...any[]]>,
+  T extends Iterable<readonly [AnyWritableAtom, ...unknown[]]>,
 >(values: InferAtomTuples<T>, options?: Options): void
 
 export function useHydrateAtoms<
-  T extends Iterable<readonly [AnyWritableAtom, ...any[]]>,
+  T extends Iterable<readonly [AnyWritableAtom, ...unknown[]]>,
 >(values: T, options?: Options) {
   const store = useStore(options)
 
