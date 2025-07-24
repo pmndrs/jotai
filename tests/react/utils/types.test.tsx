b37574bed287415a7cbc52a4761264ded186594e
daishi <daishi@axlight.com>
2025-07-24 12:27:58 +0900

    wip

--- tests/react/utils/types.test.tsx
+++ tests/react/utils/types.test.tsx
@@ -6,13 +6,14 @@ it('useHydrateAtoms should not allow invalid atom types when array is passed', (
   function Component() {
     const countAtom = atom(0)
     const activeAtom = atom(true)
-    // [ONLY-TS-4.0.5] @ts-ignore
-    // @ts-expect-error TS2769 [SKIP-TS-3.9.7]
+    // [ONLY-TS-4.1.5] [ONLY-TS-4.0.5] @ts-ignore
+    // @ts-expect-error TS2769 [SKIP-TS-4.1.5] [SKIP-TS-4.0.5] [SKIP-TS-3.9.7]
     useHydrateAtoms([
       [countAtom, 'foo'],
       [activeAtom, 0],
     ])
-    // @ts-expect-error TS2769 [SKIP-TS-3.9.7]
+    // [ONLY-TS-4.1.5] [ONLY-TS-4.0.5] @ts-ignore
+    // @ts-expect-error TS2769 [SKIP-TS-4.1.5] [SKIP-TS-4.0.5] [SKIP-TS-3.9.7]
     useHydrateAtoms([
       [countAtom, 1],
       [activeAtom, 0],
