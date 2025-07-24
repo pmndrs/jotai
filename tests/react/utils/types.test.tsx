bcc0fcd294e7acdd14ef4edaeab33be0a64fb09b
daishi <daishi@axlight.com>
2025-07-24 13:13:34 +0900

    fix?

--- tests/react/utils/types.test.tsx
+++ tests/react/utils/types.test.tsx
@@ -11,15 +11,15 @@ it('useHydrateAtoms should not allow invalid atom types when array is passed', (
       [countAtom, 'foo'],
       [activeAtom, 0],
     ])
-    // @ts-expect-error TS2769 [SKIP-TS-5.0.4] [SKIP-TS-4.0.5] [SKIP-TS-3.9.7]
+    // @ts-expect-error TS2769 [SKIP-TS-5.0.4] [SKIP-TS-4.9.5] [SKIP-TS-4.8.4] [SKIP-TS-4.7.4] [SKIP-TS-4.6.4] [SKIP-TS-4.5.5] [SKIP-TS-4.4.4] [SKIP-TS-4.3.5] [SKIP-TS-4.2.3] [SKIP-TS-4.1.5] [SKIP-TS-4.0.5] [SKIP-TS-3.9.7]
     useHydrateAtoms([
       [countAtom, 1],
-      // [ONLY-TS-5.0.4] [ONLY-TS-4.0.5] @ts-ignore
+      // [ONLY-TS-5.0.4] [ONLY-TS-4.9.5] [ONLY-TS-4.8.4] [ONLY-TS-4.7.4] [ONLY-TS-4.6.4] [ONLY-TS-4.5.5] [ONLY-TS-4.4.4] [ONLY-TS-4.3.5] [ONLY-TS-4.2.3] [ONLY-TS-4.1.5] [ONLY-TS-4.0.5] @ts-ignore
       [activeAtom, 0],
     ])
-    // @ts-expect-error TS2769 [SKIP-TS-5.0.4] [SKIP-TS-4.0.5] [SKIP-TS-3.9.7]
+    // @ts-expect-error TS2769 [SKIP-TS-5.0.4] [SKIP-TS-4.9.5] [SKIP-TS-4.8.4] [SKIP-TS-4.7.4] [SKIP-TS-4.6.4] [SKIP-TS-4.5.5] [SKIP-TS-4.4.4] [SKIP-TS-4.3.5] [SKIP-TS-4.2.3] [SKIP-TS-4.1.5] [SKIP-TS-4.0.5] [SKIP-TS-3.9.7]
     useHydrateAtoms([
-      // [ONLY-TS-5.0.4] [ONLY-TS-4.0.5] @ts-ignore
+      // [ONLY-TS-5.0.4] [ONLY-TS-4.9.5] [ONLY-TS-4.8.4] [ONLY-TS-4.7.4] [ONLY-TS-4.6.4] [ONLY-TS-4.5.5] [ONLY-TS-4.4.4] [ONLY-TS-4.3.5] [ONLY-TS-4.2.3] [ONLY-TS-4.1.5] [ONLY-TS-4.0.5] @ts-ignore
       [countAtom, true],
       [activeAtom, false],
     ])
