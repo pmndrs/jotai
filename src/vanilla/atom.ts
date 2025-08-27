02133d117719bf39aae92a210c3d6f859d1593e7
August Toman-Yih <august@zeck.app>
2025-08-27 09:19:38 -0700

    use unknown instead of void per feedback

--- src/vanilla/atom.ts
+++ src/vanilla/atom.ts
@@ -58,7 +58,7 @@ export interface Atom<Value> {
 
 export interface WritableAtom<Value, Args extends unknown[], Result>
   extends Atom<Value> {
-  read: Read<Value, SetAtom<Args, void>>
+  read: Read<Value, SetAtom<Args, unknown>>
   write: Write<Args, Result>
   onMount?: OnMount<Args, Result>
 }
@@ -75,7 +75,7 @@ let keyCount = 0 // global key count for all atoms
 
 // writable derived atom
 export function atom<Value, Args extends unknown[], Result>(
-  read: Read<Value, SetAtom<Args, void>>,
+  read: Read<Value, SetAtom<Args, unknown>>,
   write: Write<Args, Result>,
 ): WritableAtom<Value, Args, Result>
 
@@ -98,7 +98,7 @@ export function atom<Value>(
 ): PrimitiveAtom<Value> & WithInitialValue<Value>
 
 export function atom<Value, Args extends unknown[], Result>(
-  read?: Value | Read<Value, SetAtom<Args, void>>,
+  read?: Value | Read<Value, SetAtom<Args, unknown>>,
   write?: Write<Args, Result>,
 ) {
   const key = `atom${++keyCount}`
@@ -110,7 +110,7 @@ export function atom<Value, Args extends unknown[], Result>(
     },
   } as WritableAtom<Value, Args, Result> & { init?: Value | undefined }
   if (typeof read === 'function') {
-    config.read = read as Read<Value, SetAtom<Args, void>>
+    config.read = read as Read<Value, SetAtom<Args, unknown>>
   } else {
     config.init = read
     config.read = defaultRead
