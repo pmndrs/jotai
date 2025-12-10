ce786265ebf655dbd1ca101d15bdbc82f1e1c6e7
Wonsuk Choi <sukvvon@gmail.com>
2025-12-11 00:51:31 +0900

    test(types): migrate from 'ts-expect' to vitest 'expectTypeOf'

--- tests/vanilla/types.test.tsx
+++ tests/vanilla/types.test.tsx
@@ -1,6 +1,4 @@
-import { expectType } from 'ts-expect'
-import type { TypeOf } from 'ts-expect'
-import { expect, it } from 'vitest'
+import { expectTypeOf, it } from 'vitest'
 import { atom } from 'jotai/vanilla'
 import type {
   Atom,
@@ -12,58 +10,64 @@ import type {
 } from 'jotai/vanilla'
 
 it('atom() should return the correct types', () => {
-  function Component() {
-    // primitive atom
-    const primitiveAtom = atom(0)
-    expectType<PrimitiveAtom<number>>(primitiveAtom)
-    expectType<TypeOf<PrimitiveAtom<number>, typeof primitiveAtom>>(true)
-    expectType<TypeOf<PrimitiveAtom<number | undefined>, typeof primitiveAtom>>(
-      false,
-    )
+  // primitive atom
+  const primitiveAtom = atom(0)
+  // NOTE: expectTypeOf is not available in TypeScript 4.0.5 and below, toExtend is not available in TypeScript 4.6.4 and below
+  // [ONLY-TS-4.6.4] [ONLY-TS-4.5.5] [ONLY-TS-4.4.4] [ONLY-TS-4.3.5] [ONLY-TS-4.2.3] [ONLY-TS-4.1.5] [ONLY-TS-4.0.5] [ONLY-TS-3.9.7] [ONLY-TS-3.8.3] @ts-ignore
+  expectTypeOf(primitiveAtom).toExtend<PrimitiveAtom<number>>()
+  // [ONLY-TS-4.6.4] [ONLY-TS-4.5.5] [ONLY-TS-4.4.4] [ONLY-TS-4.3.5] [ONLY-TS-4.2.3] [ONLY-TS-4.1.5] [ONLY-TS-4.0.5] [ONLY-TS-3.9.7] [ONLY-TS-3.8.3] @ts-ignore
+  expectTypeOf(primitiveAtom).not.toExtend<PrimitiveAtom<number | undefined>>()
 
-    // primitive atom without initial value
-    const primitiveWithoutInitialAtom = atom<number | undefined>()
-    expectType<PrimitiveAtom<number | undefined>>(primitiveWithoutInitialAtom)
-    expectType<PrimitiveAtom<undefined>>(atom())
+  // primitive atom without initial value
+  const primitiveWithoutInitialAtom = atom<number | undefined>()
+  // [ONLY-TS-4.6.4] [ONLY-TS-4.5.5] [ONLY-TS-4.4.4] [ONLY-TS-4.3.5] [ONLY-TS-4.2.3] [ONLY-TS-4.1.5] [ONLY-TS-4.0.5] [ONLY-TS-3.9.7] [ONLY-TS-3.8.3] @ts-ignore
+  expectTypeOf(primitiveWithoutInitialAtom).toExtend<
+    PrimitiveAtom<number | undefined>
+  >()
 
-    // read-only derived atom
-    const readonlyDerivedAtom = atom((get) => get(primitiveAtom) * 2)
-    expectType<Atom<number>>(readonlyDerivedAtom)
+  const undefinedAtom = atom<undefined>()
+  // [ONLY-TS-4.6.4] [ONLY-TS-4.5.5] [ONLY-TS-4.4.4] [ONLY-TS-4.3.5] [ONLY-TS-4.2.3] [ONLY-TS-4.1.5] [ONLY-TS-4.0.5] [ONLY-TS-3.9.7] [ONLY-TS-3.8.3] @ts-ignore
+  expectTypeOf(undefinedAtom).toExtend<PrimitiveAtom<undefined>>()
 
-    // read-write derived atom
-    const readWriteDerivedAtom = atom(
-      (get) => get(primitiveAtom),
-      (get, set, value: number) => {
-        set(primitiveAtom, get(primitiveAtom) + value)
-      },
-    )
-    expectType<WritableAtom<number, [number], void>>(readWriteDerivedAtom)
+  // read-only derived atom
+  const readonlyDerivedAtom = atom((get) => get(primitiveAtom) * 2)
+  // [ONLY-TS-4.6.4] [ONLY-TS-4.5.5] [ONLY-TS-4.4.4] [ONLY-TS-4.3.5] [ONLY-TS-4.2.3] [ONLY-TS-4.1.5] [ONLY-TS-4.0.5] [ONLY-TS-3.9.7] [ONLY-TS-3.8.3] @ts-ignore
+  expectTypeOf(readonlyDerivedAtom).toExtend<Atom<number>>()
 
-    // write-only derived atom
-    const writeonlyDerivedAtom = atom(null, (get, set) => {
-      set(primitiveAtom, get(primitiveAtom) - 1)
-    })
-    expectType<WritableAtom<null, [], void>>(writeonlyDerivedAtom)
-  }
-  expect(Component).toBeDefined()
+  // read-write derived atom
+  const readWriteDerivedAtom = atom(
+    (get) => get(primitiveAtom),
+    (get, set, value: number) => {
+      set(primitiveAtom, get(primitiveAtom) + value)
+    },
+  )
+  // [ONLY-TS-4.6.4] [ONLY-TS-4.5.5] [ONLY-TS-4.4.4] [ONLY-TS-4.3.5] [ONLY-TS-4.2.3] [ONLY-TS-4.1.5] [ONLY-TS-4.0.5] [ONLY-TS-3.9.7] [ONLY-TS-3.8.3] @ts-ignore
+  expectTypeOf(readWriteDerivedAtom).toExtend<
+    WritableAtom<number, [number], void>
+  >()
+
+  // write-only derived atom
+  const writeonlyDerivedAtom = atom(null, (get, set) => {
+    set(primitiveAtom, get(primitiveAtom) - 1)
+  })
+  // [ONLY-TS-4.6.4] [ONLY-TS-4.5.5] [ONLY-TS-4.4.4] [ONLY-TS-4.3.5] [ONLY-TS-4.2.3] [ONLY-TS-4.1.5] [ONLY-TS-4.0.5] [ONLY-TS-3.9.7] [ONLY-TS-3.8.3] @ts-ignore
+  expectTypeOf(writeonlyDerivedAtom).toExtend<WritableAtom<null, [], void>>()
 })
 
 it('type utils should work', () => {
-  function Component() {
-    const readWriteAtom = atom(
-      (_get) => 1 as number,
-      async (_get, _set, _value: string) => {},
-    )
-    expect(readWriteAtom).toBeDefined()
+  const readWriteAtom = atom(
+    (_get) => 1 as number,
+    async (_get, _set, _value: string) => {},
+  )
+  void readWriteAtom
 
-    const value: ExtractAtomValue<typeof readWriteAtom> = 1
-    expectType<number>(value)
-
-    const args: ExtractAtomArgs<typeof readWriteAtom> = ['']
-    expectType<[string]>(args)
-
-    const result: ExtractAtomResult<typeof readWriteAtom> = Promise.resolve()
-    expectType<Promise<void>>(result)
-  }
-  expect(Component).toBeDefined()
+  type ReadWriteAtom = typeof readWriteAtom
+  // [ONLY-TS-4.0.5] [ONLY-TS-3.9.7] [ONLY-TS-3.8.3] @ts-ignore
+  expectTypeOf<ExtractAtomValue<ReadWriteAtom>>().toEqualTypeOf<number>()
+  // [ONLY-TS-4.0.5] [ONLY-TS-3.9.7] [ONLY-TS-3.8.3] @ts-ignore
+  expectTypeOf<ExtractAtomArgs<ReadWriteAtom>>().toEqualTypeOf<[string]>()
+  // [ONLY-TS-4.0.5] [ONLY-TS-3.9.7] [ONLY-TS-3.8.3] @ts-ignore
+  expectTypeOf<ExtractAtomResult<ReadWriteAtom>>().toEqualTypeOf<
+    Promise<void>
+  >()
 })
