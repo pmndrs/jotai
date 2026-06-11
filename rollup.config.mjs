a6f4bf8836ce5f828fa077871f677f94c0bcbd22
Daishi Kato <dai-shi@users.noreply.github.com>
2026-06-11 21:14:44 +0900

    [v3] drop CJS (#3341)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,11 @@
 /*global process*/
 import path from 'path'
 import alias from '@rollup/plugin-alias'
-import babelPlugin from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import typescript from '@rollup/plugin-typescript'
 import banner2 from 'rollup-plugin-banner2'
 import esbuild from 'rollup-plugin-esbuild'
-import createBabelConfig from './babel.config.mjs'
 
 const extensions = ['.js', '.ts', '.tsx']
 const { root } = path.parse(process.cwd())
@@ -24,15 +22,6 @@ function external(id) {
 }
 
 const cscComment = `'use client';\n`
-
-function getBabelOptions(targets) {
-  return {
-    ...createBabelConfig({ env: (env) => env === 'build' }, targets),
-    extensions,
-    comments: false,
-    babelHelpers: 'bundled',
-  }
-}
 
 function getEsbuild(env = 'development') {
   return esbuild({
@@ -80,25 +69,6 @@ function createESMConfig(input, output, clientOnly) {
   }
 }
 
-function createCommonJSConfig(input, output, clientOnly) {
-  return {
-    input,
-    output: { file: `${output}.cjs`, format: 'cjs' },
-    external,
-    plugins: [
-      alias({ entries: entries.filter((entry) => !entry.find.test(input)) }),
-      resolve({ extensions }),
-      replace({
-        'import.meta.env?.MODE': 'process.env.NODE_ENV',
-        delimiters: ['\\b', '\\b(?!(\\.|/))'],
-        preventAssignment: true,
-      }),
-      babelPlugin(getBabelOptions({ ie: 11 })),
-      banner2(() => clientOnly && cscComment),
-    ],
-  }
-}
-
 export default function (args) {
   let c = Object.keys(args).find((key) => key.startsWith('config-'))
   const clientOnly = Object.keys(args).some((key) => key === 'client-only')
@@ -109,7 +79,6 @@ export default function (args) {
   }
   return [
     ...(c === 'index' ? [createDeclarationConfig(`src/${c}.ts`, 'dist')] : []),
-    createCommonJSConfig(`src/${c}.ts`, `dist/${c}`, clientOnly),
-    createESMConfig(`src/${c}.ts`, `dist/esm/${c}.js`, clientOnly),
+    createESMConfig(`src/${c}.ts`, `dist/${c}.js`, clientOnly),
   ]
 }
